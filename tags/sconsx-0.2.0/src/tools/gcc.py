b07a60fbd2750af49b7d3e83f278dfaad7aa9357
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2006-11-24 16:08:29 +0000

    Tagging the 0.2.0 release of the 'SConsX' package.
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/sconsx@133 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ tags/sconsx-0.2.0/src/tools/gcc.py
@@ -0,0 +1,63 @@
+# -*-python-*-
+#--------------------------------------------------------------------------------
+#
+#       OpenAlea.SConsX: SCons extension package for building platform
+#                        independant packages.
+#
+#       Copyright or © or Copr. 2006 INRIA - CIRAD - INRA  
+#
+#       File author(s): Christophe Pradal <christophe.prada@cirad.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+#--------------------------------------------------------------------------------
+
+__doc__=""" Gcc configure environment. """
+__license__= "Cecill-C"
+__revision__="$Id: $"
+
+import os, sys
+from openalea.sconsx.config import *
+
+
+class Gcc:
+
+   def __init__( self, config ):
+      self.name= 'gcc'
+      self.config= config
+
+
+   def option(  self, opts ):
+      pass
+
+   def update( self, env ):
+      """ Update the environment with specific flags """
+
+      t= Tool( 'gcc' )
+      t( env )
+
+      CXXFLAGS= []
+      if env["warnings"]:
+         CXXFLAGS += [ '-W', '-Wall' ]
+
+      if env["debug"]:
+         CXXFLAGS.extend(['-g'])
+      else:
+         CXXFLAGS.extend(['-DNDEBUG', '-O2'])
+
+      env.AppendUnique(CXXFLAGS = CXXFLAGS)
+
+
+   def configure( self, config ):
+      pass
+
+def create( config ):
+   " Create gcc tool "
+   gcc= Gcc( config )
+
+   return gcc
+
