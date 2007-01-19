2d5f03a93e0623731b2647274076ecaa8566d975
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-01-19 12:34:15 +0000

    Modify __init__.py files to import openalea locally in an os independent way for developers.
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@261 04ea39c6-a500-0410-8508-e77fe2a861cf

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 # This file allow to use openalea packages without installing them.
 
-
 import os
 import openalea
+from distutils.sysconfig import get_python_lib
+
+openalea_dir= os.path.join(get_python_lib(),'openalea')
 
 pkg_dirs = [ "core/src",
              "visualea/src",
              "library/src",
-             "/usr/lib/python2.4/site-packages/openalea/"
+             openalea_dir
              ]
 
  
@@ -19,3 +21,6 @@ for subdir in  pkg_dirs:
         if(os.path.isdir(newpath)):
             __path__.append( newpath )
             break
+
+del get_python_lib
+del openalea_dir
