10f001eea727af7c810f0af627bd2263d213ef83
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-01-19 10:24:57 +0000

    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@246 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/external.py
+++ core/src/core/external.py
@@ -3,7 +3,7 @@ This module defines all the import used in wralea declarations
 """
 
 __license__= "Cecill-C"
-__revision__=" $Id:$"
+__revision__=" $Id$"
 
 
 from core import Package, Factory, Node
