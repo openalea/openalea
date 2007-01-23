3aee3e9f3883ef92a5aab31164040581ef569100
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-01-23 18:29:17 +0000

    Library Add __call__ in plot2D
    Core
    add session class (manage workspace and user package)
    add persistance to session
    
    
    Visualea
    Modify file menu 
    Link file menu to session functions
    improve tab manipulation
    tab are opened when creating a new network
    Correct bug provocated by new inhetitance model
    
    
    
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@284 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ core/test/test_session.py
@@ -0,0 +1,89 @@
+# -*- python -*-
+#
+#       OpenAlea.Core: OpenAlea Core
+#
+#       Copyright or (C) or Copr. 2006 INRIA - CIRAD - INRA  
+#
+#       File author(s): Christophe Pradal <christophe.prada@cirad.fr>
+#                       Samuel Dufour-Kowalski <samuel.dufour@sophia.inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+
+
+
+__doc__= """
+Test the session
+"""
+
+
+from openalea.core.session import Session
+from openalea.core.pkgmanager import PackageManager
+from openalea.core.subgraph import SubGraphFactory
+
+import os
+import openalea
+
+
+def test_session():
+
+    pkgman = PackageManager()
+
+    session = Session(pkgman)
+
+    # user package is created
+    assert pkgman.pkgs.has_key(Session.USR_PKG_NAME)
+    
+    # file is created
+    session.save('savesession.xml')
+    assert os.path.exists("savesession.xml")
+
+    pkgman.clear()
+    assert not pkgman.pkgs.has_key(Session.USR_PKG_NAME)
+    assert len(pkgman.pkgs.keys())==0
+
+    session.load('savesession.xml')
+    assert pkgman.pkgs.has_key(Session.USR_PKG_NAME)
+    assert session.user_pkg.has_key('Workspace')
+    
+def test_load():
+
+    
+    pkgman = PackageManager()
+    session = Session(pkgman)
+    
+    session.load('testsave.xml')
+
+    assert len(session.user_pkg.keys())==2
+
+
+def test_save():
+    pkgman = PackageManager()
+    session = Session(pkgman)
+    
+    sgfactory = SubGraphFactory(pkgman, name="SubGraphExample",
+                                description= "Examples",
+                                category = "Examples",
+                                )
+
+    # build the subgraph factory
+
+    addid = sgfactory.add_nodefactory ("Library", "add", (200,200))
+    session.user_pkg.add_factory(sgfactory)
+
+    session.save('testsave2.xml')
+
+    session.clear()
+    session.load('testsave2.xml')
+    
+    assert len(session.user_pkg.keys())==2
+
+    
+
+test_session()
+test_load()
+test_save()
