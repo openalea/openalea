720233e53fdc593a0083b2df8a943e43dcd3caa1
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-01-31 08:59:00 +0000

    Declare OPENALEADIR environ variable Modify README.txt
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/config@316 04ea39c6-a500-0410-8508-e77fe2a861cf

--- oac_postinstall.py
+++ oac_postinstall.py
@@ -133,8 +133,10 @@ def main():
 
     print "Setting environment variables"
 
-    set_lsb_env('openalea', ['LD_LIBRARY_PATH=%s'%(config.lib_dir,)])
-    set_win_env(['PATH=%s'%(config.lib_dir,)])
+    set_lsb_env('openalea', ['LD_LIBRARY_PATH=%s'%(config.prefix_dir,),
+                             'OPENALEADIR=%s'%(config.prefix_dir,)])
+    set_win_env(['PATH=%s'%(config.prefix_dir,),
+                 'OPENALEADIR=%s'%(config.prefix_dir,)])
 
 if (__name__ == '__main__'):
     main()
