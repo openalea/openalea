352190b5241c14847064e4e236fa868f300f2f71
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-02-06 10:46:43 +0000

    Add singleton metaclass Add icon ccmime Add Drag and Drap between data pool and node inputs
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/visualea/src/visualea@337 04ea39c6-a500-0410-8508-e77fe2a861cf

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created: Mon Feb  5 19:20:51 2007
+# Created: Tue Feb  6 11:44:31 2007
 #      by: PyQt4 UI code generator 4.1
 #
 # WARNING! All changes made in this file will be lost!
