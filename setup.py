5ae6e59f9a59c66549353b3691b3ec0cef4b5ac8
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-02-08 17:13:09 +0000

    Oops. Change the version number...
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/tags/sconsx-0.3.0@358 04ea39c6-a500-0410-8508-e77fe2a861cf

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ name= 'sconsx'
 namespace= 'openalea'
 pkg_name= namespace + '.' + name
 
-version= '0.2.0'
+version= '0.3.0'
 
 description= 'Scons Extension to build multi-platform packages for OpenAlea and others.' 
 long_description= \
