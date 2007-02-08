d1a1bc2f80f06601a0572c03214bf0b1da57c970
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-02-08 18:20:30 +0000

    remove tools subpackage in core
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@360 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/doc/gendoc.py
+++ core/doc/gendoc.py
@@ -2,7 +2,7 @@ import os, sys
 sys.path.append("..")
 import version
 
-filepattern = "../src/core/*"
+filepattern = "../src/core"
 outdir = "%s"%(version.version)
 
-os.system('epydoc --html -u "http://openalea.gforge.inria.fr" -n "openalea.core" -o %s %s'%(outdir, filepattern))
+os.system('epydoc --html  -o %s %s'%(outdir, filepattern))
