0c18abbe7e2f6400ad52186eaba046c59b8ca2f7
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-02-09 12:11:53 +0000

    gendoc script transfert doc on the webserver and create a symbolic link
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/core@364 04ea39c6-a500-0410-8508-e77fe2a861cf

--- doc/gendoc.py
+++ doc/gendoc.py
@@ -3,6 +3,8 @@ sys.path.append("..")
 import version
 
 filepattern = "../src/core"
-outdir = "%s"%(version.version)
+outdir = "core-%s"%(version.version)
 
-os.system('epydoc --html  -o %s %s'%(outdir, filepattern))
+os.system('epydoc --html  -o %s %s'%(outdir, 'openalea.core'))
+os.system('scp -r %s dufourko@scm.gforge.inria.fr:/var/lib/gforge/chroot/home/groups/openalea/htdocs/doc/openalea'%(outdir,))
+os.system('ssh dufourko@scm.gforge.inria.fr "cd /var/lib/gforge/chroot/home/groups/openalea/htdocs/doc/openalea; ln -s %s core"'%(outdir))
