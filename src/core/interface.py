5ee9456b1a9dbf4e7cdcce2e243211e65d5b3153
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-03-22 16:09:32 +0000

    Allow declaration of new interface and widget trough the InterfaceMapper singleton object
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/core@423 04ea39c6-a500-0410-8508-e77fe2a861cf

--- src/core/interface.py
+++ src/core/interface.py
@@ -22,21 +22,21 @@ __license__= "Cecill-C"
 __revision__=" $Id$ "
     
 
-class Interface(object):
+class IInterface(object):
     """ Abstract base class for all interfaces """
     pass
 
-class IFileStr(Interface):
+class IFileStr(IInterface):
     """ File Path interface """
     pass
 
 
-class IStr(Interface):
+class IStr(IInterface):
     """ String interface """
     pass
 
 
-class IFloat(Interface):
+class IFloat(IInterface):
     """ Float interface """
     
     def __init__(self, min = -2.**24, max = 2.**24):
@@ -46,7 +46,7 @@ class IFloat(Interface):
     
 
 
-class IInt(Interface):
+class IInt(IInterface):
     """ Int interface """
     
     def __init__(self, min = -2**24, max = 2**24):
@@ -55,35 +55,87 @@ class IInt(Interface):
         self.max = max
 
 
-class IBool(Interface):
+class IBool(IInterface):
     """ Bool interface """
     pass
 
 
-class IEnumStr(Interface):
+class IEnumStr(IInterface):
     """ String enumeration """
 
     def __init__(self, enum = []):
         self.enum = enum
 
 
-class IRGBColor(Interface):
+class IRGBColor(IInterface):
     """ RGB Color """
     pass
 
-class ITuple3(Interface):
+class ITuple3(IInterface):
     """ Tuple3 """
     def __init__(self):
         pass
 
-class IFunction(Interface):
+class IFunction(IInterface):
     """ Function interface """
     pass
 
-class ISequence(Interface):
+class ISequence(IInterface):
     """ Sequence interface (list, tuple, ...) """
     pass
 
-class IDict(Interface):
+class IDict(IInterface):
     """ Dictionary interface """
     pass
+
+
+
+# Dictionary to map Interface with corresponding widget
+
+from openalea.core.singleton import Singleton
+
+class InterfaceMapper(dict):
+    __metaclass__ = Singleton
+
+    def __init__(self, *args):
+        dict.__init__(self, *args)
+
+    def declare_interface(self, interface, widget=None):
+        """
+        Declare an interface and its optional widget
+        @param interface : IInterface class object
+        @param widget : IInterfaceWidget class object
+        """
+
+        self[inteface] = widget
+
+
+
+
+class IInterfaceWidget(object):
+    """ Base class for widget associated to an interface """
+
+    def __init__(self, node, parent, parameter_str, interface):
+        """
+        @param parameter_str : the parameter key the widget is associated to
+        @param interface : instance of interface object
+        """
+  
+        self.node = node
+        self.param_str = parameter_str
+
+
+    def update_state(self):
+        """ Enable or disable widget depending of connection status """
+
+        i = self.node.get_input_index(self.param_str)
+        state = self.node.get_input_state(i)
+
+        # By default, disable the entire widget
+        try:
+            if(state == "connected"):
+                self.setEnabled(False)
+            else:
+                self.setEnabled(True)
+        except:
+            pass
