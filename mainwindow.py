0146f931756a1e3bbb1110ad100e84182de58097
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-03-28 16:39:49 +0000

    Visualea :   Create NodeFactoryView to factorise drag and drop function for all node   factory views   Finalise search integration
    Core:
      Create a search function in pkgmanager
    
    
    
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/visualea_new_shell@430 04ea39c6-a500-0410-8508-e77fe2a861cf

--- mainwindow.py
+++ mainwindow.py
@@ -32,7 +32,7 @@ from pycutext import PyCutExt
 from openalea.core import cli
 from code import InteractiveInterpreter as Interpreter
 
-from node_treeview import PackageTreeView, PkgModel, CategoryModel
+from node_treeview import NodeFactoryTreeView, PkgModel, CategoryModel
 from node_treeview import DataPoolListView, DataPoolModel
 from node_treeview import SearchListView, SearchModel
 
@@ -74,13 +74,13 @@ class MainWindow(QtGui.QMainWindow,
 
         # package tree view
         self.pkg_model = PkgModel(pkgman)
-        self.packageTreeView = PackageTreeView(self, self.packageview)
+        self.packageTreeView = NodeFactoryTreeView(self, self.packageview)
         self.packageTreeView.setModel(self.pkg_model)
         self.vboxlayout.addWidget(self.packageTreeView)
 
         # category tree view
         self.cat_model = CategoryModel(pkgman)
-        self.categoryTreeView = PackageTreeView(self, self.categoryview)
+        self.categoryTreeView = NodeFactoryTreeView(self, self.categoryview)
         self.categoryTreeView.setModel(self.cat_model)
         self.vboxlayout1.addWidget(self.categoryTreeView)
 
@@ -114,6 +114,8 @@ class MainWindow(QtGui.QMainWindow,
                      self.exec_python_script)
         self.connect(self.action_New_Network, SIGNAL("activated()"),
                      self.new_graph)
+        self.connect(self.actionFind_Node, SIGNAL("activated()"),
+                     self.find_node)
 
         self.connect(self.action_New_Session, SIGNAL("activated()"), self.new_session)
         self.connect(self.action_Open_Session, SIGNAL("activated()"), self.open_session)
@@ -482,9 +484,19 @@ class MainWindow(QtGui.QMainWindow,
     def search_node(self):
         """ Activated when search line edit is validated """
 
-        self.search_model.set_results([str(self.search_lineEdit.text())])
+        results = self.pkgmanager.search_node(str(self.search_lineEdit.text()))
+        self.search_model.set_results(results)
+
+    def find_node(self):
+        """ Find node Command """
+
+        i = self.tabPackager.indexOf(self.searchview)
+        self.tabPackager.setCurrentIndex(i)
+        self.search_lineEdit.setFocus()
        
 
+
+################################################################################
 import ui_newgraph
 
 class NewGraph(  QtGui.QDialog, ui_newgraph.Ui_NewGraphDialog) :
