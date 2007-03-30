7240400bcd65f5ba1c5fbb0fa430d4fc9114755f
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-03-30 09:07:16 +0000

    Core : Add user wralea path in home directory for linux : /home/user/.openalea/wralea for windows : $USERPROGILE/.openalea/wralea
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@442 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/pkgmanager.py
+++ core/src/core/pkgmanager.py
@@ -44,9 +44,10 @@ class PackageManager(object):
     """
 
     def __init__ (self):
+        
 
         # list of path to search wralea file
-        self.wraleapath = list(openalea.__path__)
+        self.wraleapath = self.get_default_wraleapath()
         
         # save system path
         self.old_syspath = sys.path[:]
@@ -57,6 +58,11 @@ class PackageManager(object):
 
         # dictionnay of category
         self.category = {}
+        
+
+    def get_default_wraleapath(self):
+        """ Return a list wralea path """
+        return list(openalea.__path__) + [get_wralea_home_dir()]
 
 
     def init(self, filename=None):
@@ -77,7 +83,8 @@ class PackageManager(object):
         self.recover_syspath()
         self.category = {}
 
-        self.wraleapath = openalea.__path__ 
+        self.wraleapath = self.get_default_wraleapath()
+        
 
     # Path Functions
     def add_wraleapath(self, new_path):
@@ -164,8 +171,8 @@ class PackageManager(object):
             p= path(wp).abspath()
 
             # search for wralea.py
-            wralea_files.update( p.walkfiles("wralea.py") )
-            wralea_files.update( p.walkfiles("wralea.xml") )
+            wralea_files.update( p.walkfiles("*wralea.py") )
+            wralea_files.update( p.walkfiles("*wralea.xml") )
 
         for f in wralea_files:
             print "Package Manager : found %s" % f
@@ -256,3 +263,65 @@ class Category(set):
         return ""
 
 
+################################################################################
+# Directories functions
+################################################################################
+
+def get_default_home_dir() :
+    
+    """ Return the home directory (valid on linux and windows) """
+    if sys.platform != 'win32' :
+        return os.path.expanduser( '~' )
+
+    def valid(path) :
+        if path and os.path.isdir(path) :
+            return True
+        return False
+    
+    def env(name) :
+        return os.environ.get( name, '' )
+
+    homeDir = env( 'USERPROFILE' )
+    if not valid(homeDir) :
+        homeDir = env( 'HOME' )
+    elif not valid(homeDir) :
+        homeDir = '%s%s' % (env('HOMEDRIVE'),env('HOMEPATH'))
+    elif not valid(homeDir) :
+        homeDir = env( 'SYSTEMDRIVE' )
+    elif not valid(homeDir) :
+        homeDir = 'C:\\'
+
+    if homeDir and (not homeDir.endswith('\\')) :
+        homeDir += '\\'
+        
+    return homeDir
+
+
+def get_openalea_home_dir(name='.openalea'):
+    """
+    Return the openalea home dirextory
+    If it doesn't exist, create it
+    """
+
+    home = get_default_home_dir()
+
+    aleahome = os.path.join(home, name)
+    if(not os.path.exists(aleahome)):
+        os.mkdir(aleahome)
+        
+    return aleahome
+
+
+
+def get_wralea_home_dir(name='wralea' ):
+    """
+    Return the openalea wralea home dirextory
+    If it doesn't exist, create it
+    """
+
+    aleahome = get_openalea_home_dir()
+    wraleahome = os.path.join(aleahome, name)
+    if(not os.path.exists(wraleahome)):
+        os.mkdir(wraleahome)
+
+    return wraleahome
