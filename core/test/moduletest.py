dbb90687853084408ed78deb7a9450ff571a76af
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-04-11 08:42:19 +0000

    Change some menu in main window Change tab order in new graph dialog Fix bug 'getText' in code editor Adapt subgraph.to_factory function to allow graph selection export Fix bug in get_input_desc function
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@463 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/test/moduletest.py
+++ core/test/moduletest.py
@@ -1,6 +1,6 @@
 def userfunc(a, b):
     return pow(a,b)
 
-def userclass(object):
-    def __call__(self, a, b):
-        return a - b
+class userclass(object): pass
+#     def __call__(self, a, b):
+#         return a - b
