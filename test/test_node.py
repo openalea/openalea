1c548b262003cdc7605d0c75bcc35f12139aabd7
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-04-19 12:17:06 +0000

    Separate FuncNode from Node Add tests
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/core@488 04ea39c6-a500-0410-8508-e77fe2a861cf

--- test/test_node.py
+++ test/test_node.py
@@ -5,7 +5,7 @@
 from openalea.core.node import *
 
 
-def test_node():
+def test_funcnode():
 
     # Test Node creation
     inputs = ( dict(name='x', interface=None, value=None),)
@@ -13,7 +13,7 @@ def test_node():
     def func():
         raise RuntimeError()
     
-    n = Node( inputs, outputs, func)
+    n = FuncNode( inputs, outputs, func)
 
     try:
         n()
@@ -40,9 +40,95 @@ def test_node():
 
 
 
+class MyNode(Node):
+
+    def __call__(self, inputs):
+        return sum(inputs)
+
+def MyFunc(a, b):
+    return a + b
+
+def test_node():
+
+    # Test Node creation
+    inputs = ( dict(name='x', interface=None, value=None),)
+    outputs = ( dict(name='y', interface=None),)
+    def func():
+        raise RuntimeError()
+    
+    n = Node( inputs, outputs)
+
+    try:
+        n()
+        assert False
+    except NotImplementedError:
+        assert True
+
+
+    n = MyNode(inputs, outputs)
+
+
+    assert n.get_nb_input() == 1
+    assert n.get_nb_output() == 1
+
+
+    # Test IO and acess by key or index
+    n.set_input(0, 1)
+    assert n.get_input('x') == 1
+
+    n.eval()
+    assert n.get_output('y') == 1
+
+
 def test_factory():
 
-    # Test Factory Creation
+    f1 = Factory( name = "MyFactory",
 
-    # See Test Wralea
-    pass
+                 nodemodule = "test_node",
+                 nodeclass = "MyNode",
+                 
+                 )
+                
+    n = f1.instantiate()
+    assert n.get_nb_input() == 0
+    
+    f2 = Factory( name = "MyFactory2",
+
+                  nodemodule = "test_node",
+                  nodeclass = "MyFunc",
+                 )
+
+    n = f2.instantiate()
+    assert n.get_nb_input() == 2
+    
+
+    f1IO = Factory( name = "MyFactory",
+
+                    nodemodule = "test_node",
+                    nodeclass = "MyNode",
+                    
+                    inputs = (dict(name="x", interface=None),
+                              dict(name="y", interface=None),
+                              dict(name="z", interface=None)),
+                    outputs = (dict(name="a", interface=None),),
+                 
+                 )
+
+    n = f1IO.instantiate()
+    assert n.get_nb_input() == 3
+                
+
+    f2IO = Factory( name = "MyFactory2",
+
+                    nodemodule = "test_node",
+                    nodeclass = "MyFunc",
+                    
+                    inputs = (dict(name="x", interface=None), dict(name="y", interface=None)),
+                    outputs = (dict(name="z", interface=None),),
+                 )
+
+    n = f2IO.instantiate()
+    assert n.get_nb_input() == 2
+
+
+
