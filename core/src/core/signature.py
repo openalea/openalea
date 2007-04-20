85e9123b066fa3dacc80b32ce68b8569c2f1117f
dasilva <dasilva@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-04-20 08:19:25 +0000

    creation of signature object with its tests
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@494 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/signature.py
+++ core/src/core/signature.py
@@ -25,57 +25,106 @@ __revision__=" $Id$ "
 
 import inspect
 import types
-from interface import TypeInterfaceMap
+from openalea.core.interface import TypeInterfaceMap
 
 
 class Signature(object):
-    """ Instrospect function objects """
-
-    def __init__(self, func):
-        """ func is a function object or a functor class """
-
-
-
-
-def get_parameters(f):
-    """ Return the list of
-    dict(name='name', interface=None, value=None) corresponding
-    to the function parameters """
-
-    try:
-        # if f is a class
-        if(not isinstance(f, types.FunctionType)):
-            specs = inspect.getargspec(f.__call__)
-            varnames = specs[0]
-            varnames = varnames[1:]
-            defaults = specs[3]
-            
-        # f is a function
-        else:
-            specs = inspect.getargspec(f)
-            varnames = specs[0]
-            defaults = f.func_defaults
-            defaults = specs[3]
-
-    except Exception, e:
-        print e
-        return ()
-
-    if(defaults == None): defaults = []
-    if(varnames == None): varnames = []
+    """Object to represent the signature of a function/method.
     
-    args= []
-    nv = len(varnames)
-    nd = len(defaults)
-    # parse args without default value
-    for i,name in enumerate(varnames[:nv - nd]):
-        args.append(dict(name=name, interface=None, value=None))
+        Attributes:
+        * name
+            Name of the function/method.
+        * parameters
+            List of dict keyed on the variable name, interface and value for default value
+        """
 
-    # parse args with default value
-    for i,name in enumerate(varnames[nv - nd:]):
-        v = defaults[i]
-        interface = TypeInterfaceMap().get(type(v),None)
-        args.append(dict(name=name, interface=interface, value=v))
+    def __init__(self, f):
+        """ f is a function object or instance method, functor class managed but neeed to be tested more carefully"""
+        
+        self.name = f.__name__
+        self.parameters = []
+        
+        try:
+            #if f is an instance method
+            if( isinstance(f,types.MethodType)):
+                func = f.im_func
+                spec = inspect.getargspec(func)
+                varnames=spec[0][1:]
+            #if f is a function
+            elif( isinstance(f,types.FunctionType)):
+                func = f
+                spec = inspect.getargspec(func)
+                varnames=spec[0]
+            elif( hasattr(f, '__call__')):
+                func = f.__call__
+                spec = inspect.getargspec(func) #modules have __call__ method but getargspec won't work !
+                varnames=spec[0][1:]
 
-    return args
+            default_values = spec[3] or []
+
+            parameters = []
+            nv = len(varnames)
+            nd = len(default_values)
+            # parse args without default value
+            for i,name in enumerate(varnames[:nv - nd]):
+                self.parameters.append(dict(name=name, interface=None, value=None))
+
+            # parse args with default value
+            for i,name in enumerate(varnames[nv - nd:]):
+                df = default_values[i]
+                interface = TypeInterfaceMap().get(type(df),None)
+                self.parameters.append(dict(name=name, interface=interface, value=df))
+
+        except Exception, e:
+            print e
+        
+    def get_name(self):
+        return self.name
+
+    def get_parameters(self):
+        print self.parameters
+        return self.parameters
+
+
+#def get_parameters(f):
+#    """ Return the list of
+#    dict(name='name', interface=None, value=None) corresponding
+#    to the function parameters """
+#
+#    try:
+#        # if f is a class
+#        if(not isinstance(f, types.FunctionType)):
+#            specs = inspect.getargspec(f.__call__)
+#            varnames = specs[0]
+#            varnames = varnames[1:]
+#            defaults = specs[3]
+#            
+#        # f is a function
+#        else:
+#            specs = inspect.getargspec(f)
+#            varnames = specs[0]
+#            defaults = f.func_defaults
+#            defaults = specs[3]
+#
+#    except Exception, e:
+#        print e
+#        return ()
+#
+#    if(defaults == None): defaults = []
+#    if(varnames == None): varnames = []
+#    
+#    args= []
+#    nv = len(varnames)
+#    nd = len(defaults)
+#    # parse args without default value
+#    for i,name in enumerate(varnames[:nv - nd]):
+#        args.append(dict(name=name, interface=None, value=None))
+#
+#    # parse args with default value
+#    for i,name in enumerate(varnames[nv - nd:]):
+#        v = defaults[i]
+#        interface = TypeInterfaceMap().get(type(v),None)
+#        args.append(dict(name=name, interface=interface, value=v))
+#
+#    return args
 
