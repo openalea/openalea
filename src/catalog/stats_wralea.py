06a351fb327dfc730aacb9da2e3e7e19301c8eeb
dasilva <dasilva@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-04-25 23:45:47 +0000

    linear regression nodes added
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/catalog@521 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ src/catalog/stats_wralea.py
@@ -0,0 +1,86 @@
+# -*- python -*-
+#
+#       OpenAlea.Core.Library: OpenAlea Core Library module
+#
+#       Copyright or (C) or Copr. 2006 INRIA - CIRAD - INRA  
+#
+#       File author(s): Da SILVA David <david.da_silva@cirad.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+
+
+__doc__="""
+Wralea for Catalog.stats 
+"""
+
+__license__= "Cecill-C"
+__revision__=" $Id$ "
+
+
+
+from openalea.core import *
+
+def register_packages(pkgmanager):
+    """ Initialisation function
+    Return a list of package to include in the package manager.
+    This function is called by the package manager when it is updated
+    """
+
+
+    metainfo={ 'version' : '0.0.1',
+               'license' : 'CECILL-C',
+               'authors' : 'OpenAlea Consortium',
+               'institutes' : 'INRIA/CIRAD',
+               'description' : 'Catalog library.',
+               'url' : 'http://openalea.gforge.inria.fr'
+               }
+
+
+    package = Package("Catalog.Stats", metainfo)
+
+###### begin nodes definitions #############
+
+    nf = Factory( name="LinearRegression",
+                  description="compute the linear regression",
+                  category="Stat",
+                  nodemodule="stats",
+                  nodeclass="LinearRegression",
+                  inputs= ( dict( name = "X", interface=ISequence, value=[ 0.69314718,  1.79175949,  2.48490667,  3.17805386,  3.87120104] ),
+                            dict( name = "Y", interface=ISequence, value=[ 2.07944154,  2.7080502,   4.41884061,  5.58724866,  6.92264389] ),
+                            dict( name = "alpha", interface=IFloat, value=5. ),
+                            dict( name = "origin", interface=IBool, value=False ),
+                          ),
+                  outputs=(dict(name="reg", interface = IDict),
+                          ),
+                  )
+
+    package.add_factory( nf )
+
+    nf = Factory( name="LRtoPlot",
+                  description="generate plotable object from linear regression",
+                  category="Stat",
+                  nodemodule="stats",
+                  nodeclass="LR2Plot",
+                  inputs= ( dict( name='reg', interface=IDict ),
+                            dict( name='pointLegend', interface=IStr, value='Data' ),
+                            dict( name='pointMarker', interface=IStr, value='^' ),
+                            dict( name='pointColor', interface=IStr, value='dodgerblue' ),
+                            dict( name='regLineStyle', interface=IStr, value='-' ),
+                          ),
+                  outputs=( dict(name='plotObjList', interface=ISequence),
+                          ),
+                  )
+
+    package.add_factory( nf )
+
+
+###### end nodes definitions ###############
+
+    pkgmanager.add_package(package)
+
+
