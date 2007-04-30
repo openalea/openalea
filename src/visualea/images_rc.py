b19a95bb8f62591721a048e28a4dfe12ffbede8b
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-04-30 16:28:39 +0000

    Add reload graph function Improve export to model
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/visualea@534 04ea39c6-a500-0410-8508-e77fe2a861cf

--- src/visualea/images_rc.py
+++ src/visualea/images_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: lun. avr. 30 15:27:21 2007
+# Created: lun. avr. 30 17:55:16 2007
 #      by: The Resource Compiler for PyQt (Qt v4.2.3)
 #
 # WARNING! All changes made in this file will be lost!
--- src/visualea/ui_newpackage.py
+++ src/visualea/ui_newpackage.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'newpackage.ui'
 #
-# Created: Mon Apr 30 15:27:21 2007
+# Created: Mon Apr 30 17:55:16 2007
 #      by: PyQt4 UI code generator 4.2
 #
 # WARNING! All changes made in this file will be lost!
