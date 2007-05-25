8963d4daa53430b81510b979550fddb30fe0f45a
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-05-25 12:25:30 +0000

    Replace Copyright
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@559 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/visualeagui.py
+++ visualea/src/visualea/visualeagui.py
@@ -4,7 +4,7 @@
 #
 #       OpenAlea.Visualea: OpenAlea graphical user interface
 #
-#       Copyright or (C) or Copr. 2006 INRIA - CIRAD - INRA  
+#       Copyright 2006 INRIA - CIRAD - INRA  
 #
 #       File author(s): Samuel Dufour-Kowalski <samuel.dufour@sophia.inria.fr>
 #                       Christophe Pradal <christophe.prada@cirad.fr>
