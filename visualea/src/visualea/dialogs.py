bee166c8b0956624ca766e542c5ef62a7bd244ee
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-05-30 16:06:22 +0000

    Change reference from setting to settings
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@563 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/dialogs.py
+++ visualea/src/visualea/dialogs.py
@@ -23,7 +23,7 @@ __revision__=" $Id$ "
 
 
 from PyQt4 import QtCore, QtGui
-from openalea.core.setting import get_userpkg_dir
+from openalea.core.settings import get_userpkg_dir
 from openalea.core.compositenode import CompositeNodeFactory
 from openalea.core.pkgmanager import PackageManager
 from openalea.core.settings import Settings
