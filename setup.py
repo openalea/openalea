a6d8586db4a6c2b9684cf7b649131922f036bcda
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-06-13 11:54:43 +0000

    Update version and changelog.txt of catalog
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/catalog@580 04ea39c6-a500-0410-8508-e77fe2a861cf

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ name= 'catalog'
 
 namespace=config.namespace 
 pkg_name= namespace + '.' + name
-version= '0.1.0' 
+version= '0.2.0a' 
 description= 'OpenAlea Logical Component Catalog.' 
 
 long_description= ''
