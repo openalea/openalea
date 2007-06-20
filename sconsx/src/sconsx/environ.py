aecea995590818800c695919a24f939b6797e5f2
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-06-20 07:46:10 +0000

    * Modifiy python extension suffix only on Windows: dll -> pyd
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@592 04ea39c6-a500-0410-8508-e77fe2a861cf

--- sconsx/src/sconsx/environ.py
+++ sconsx/src/sconsx/environ.py
@@ -111,7 +111,15 @@ def ALEAWrapper( env, python_dir, target, source, *args, **kwds ):
   if (env['compiler'] == 'msvc') and ('8.0' in env['MSVS_VERSION']):
     kwds['SHLINKCOM']= [env['SHLINKCOM'], 
       'mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:$TARGET;2']
-  wrap = env.SharedLibrary( real_target, source, SHLIBPREFIX='',SHLIBSUFFIX='.pyd', *args, **kwds )
+  if os.name == 'win32':
+    wrap = env.SharedLibrary( real_target, source, 
+                              SHLIBPREFIX='',SHLIBSUFFIX='.pyd', 
+                              *args, **kwds )
+  else:
+    wrap = env.SharedLibrary( real_target, source, 
+                              SHLIBPREFIX='', 
+                              *args, **kwds )
+
   Alias( "build", wrap )
   return wrap
 
