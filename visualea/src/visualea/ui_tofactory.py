9ae4b674fcd512291eefd108c6e9f399583c5b4c
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-06-20 08:51:37 +0000

    Change Icon in Visualea
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@595 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/ui_tofactory.py
+++ visualea/src/visualea/ui_tofactory.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'tofactory.ui'
 #
-# Created: Mon May 21 17:43:15 2007
+# Created: Wed Jun 20 10:50:19 2007
 #      by: PyQt4 UI code generator 4.2
 #
 # WARNING! All changes made in this file will be lost!
