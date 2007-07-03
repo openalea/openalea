d9512ae00c5cc267badd5809832df259192e5c8e
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-07-03 09:32:43 +0000

    Add in preference dialog evaluation algorithm selection
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/visualea@619 04ea39c6-a500-0410-8508-e77fe2a861cf

--- src/visualea/images_rc.py
+++ src/visualea/images_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: ven. juin 29 10:54:05 2007
+# Created: mar. juil. 3 11:19:09 2007
 #      by: The Resource Compiler for PyQt (Qt v4.2.3)
 #
 # WARNING! All changes made in this file will be lost!
