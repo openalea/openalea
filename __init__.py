ad9d7217c95202c081f6c607e39fc86dd2a986d0
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-07-17 17:21:06 +0000

    Change __path__ initialisation
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@645 04ea39c6-a500-0410-8508-e77fe2a861cf

--- __init__.py
+++ __init__.py
@@ -1,34 +1,14 @@
  
 
-# This file allow to use openalea packages without installing them.
-import os
-import openalea
-from distutils.sysconfig import get_python_lib
-
-openalea_dir= os.path.join(get_python_lib(),'openalea')
-
-pkg_dirs = [ "core/src",
-             "visualea/src",
-             "catalog/src",
-             "distx",
-             "sconsx/src",
-             openalea_dir
-             ]
-
- 
-for subdir in  pkg_dirs:
-    for p in __path__:
-        newpath = os.path.abspath(os.path.join(p, os.path.normpath(subdir)))
-        if(os.path.isdir(newpath)):
-            __path__.append(newpath)
-            break
 
 try:
-    import vplants
-    __path__ += vplants.__path__
-except:
-    pass
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+    
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
 
 
-del get_python_lib
-del openalea_dir
+import __init_path__
+
---
+++ __init_path__.py
@@ -0,0 +1,34 @@
+# This file allow to use openalea packages without installing them.
+import os
+from distutils.sysconfig import get_python_lib
+import openalea
+
+openalea_dir = os.path.join(get_python_lib(),'openalea')
+
+pkg_dirs = [ "core/src",
+             "visualea/src",
+             "catalog/src",
+             "distx",
+             "sconsx/src",
+             openalea_dir
+             ]
+
+ 
+for subdir in  pkg_dirs:
+    for p in openalea.__path__:
+        
+        newpath = os.path.abspath(os.path.join(p, os.path.normpath(subdir)))
+        if(os.path.isdir(newpath)):
+            openalea.__path__.append(newpath)
+            break
+
+
+try:
+    import vplants
+    openalea.__path__ += vplants.__path__
+except:
+    pass
+
+
+del get_python_lib
+del openalea_dir
