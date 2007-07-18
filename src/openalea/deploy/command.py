b45b5954eff20696d4b36a6e58f8a025c753d2b0
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-07-18 09:11:31 +0000

    Add test for egglib sconsx tool Change namespace __init__
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/deploy@647 04ea39c6-a500-0410-8508-e77fe2a861cf

--- src/openalea/deploy/command.py
+++ src/openalea/deploy/command.py
@@ -302,6 +302,14 @@ try:
 except ImportError:
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
+
+# Local setup for openalea subversion
+try:
+    from __init_path__ import set_path
+    set_path()
+except:
+    pass
+
 """
 
     user_options = []
