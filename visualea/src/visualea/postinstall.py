c105488e577c69a6ac99b1220068a4b5cb01ea6a
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-07-30 13:42:21 +0000

    Fix bug in windows shortcut creation Fix visualea setup.py for windows
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@674 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/postinstall.py
+++ visualea/src/visualea/postinstall.py
@@ -3,22 +3,25 @@
 
 def install():
     
-    from openalea.deploy import create_win_shortcut, set_win_reg, create_fd_shortcut
+    from openalea.deploy.shortcut import create_win_shortcut, set_win_reg, create_fd_shortcut
     import sys
-    
+    from os.path import join as pj
+
+    winexe = sys.executable
+    winexe = winexe.replace('python', 'pythonw')
     create_win_shortcut(name = 'Visualea',
-                        target = sys.executable,
-                        arguments = pj(sys.prefix, 'Scripts', 'visualea.exe'),
-                        startIn = "", 
+                        target = winexe,
+                        arguments = '"'+pj(sys.prefix, 'Scripts', 'visualea-script.pyw')+'"',
+                        startin = "", 
                         icon = "",
                         description = "Visual programming",
-                        menuGroup = "OpenAlea")
+                        menugroup = "OpenAlea")
         
 
     create_fd_shortcut(name='Visualea',
                        target=sys.executable, 
                        arguments=pj(sys.prefix, 'bin', 'visualea'), 
-                       group='OpenAlea', 
+                       menugroup='OpenAlea', 
                        icon='' )
    
 
