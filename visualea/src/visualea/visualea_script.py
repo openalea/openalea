60fd178cdabafa36db1cc18829803367c2486106
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-07-30 15:38:06 +0000

    adapt visulea_script for linux and windows
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@676 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/visualea_script.py
+++ visualea/src/visualea/visualea_script.py
@@ -113,9 +113,16 @@ def start_gui():
     except ImportError:
         envdict = check_system()
 
-    os.execle(sys.executable, sys.executable, '-c',
-              '"import sys; from openalea.visualea import visualeagui; visualeagui.main(sys.argv)"',
-              envdict)
+    if('win' in sys.platform.lower()):
+        os.execle(sys.executable, sys.executable, '-c',
+                  '"import sys; from openalea.visualea import visualeagui; visualeagui.main(sys.argv)"',
+                  envdict)
+    else:
+        os.execle(sys.executable, sys.executable, '-c',
+                  'import sys; from openalea.visualea import visualeagui; visualeagui.main(sys.argv)',
+                  envdict)
+
+        
 
 if( __name__ == "__main__"):
     start_gui()
