e57a7b54012a7fd76a2c3657091bbf8573ddb461
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-09-26 13:01:37 +0000

    Fix bug with copy annotation
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk/deploy@779 04ea39c6-a500-0410-8508-e77fe2a861cf

--- src/openalea/deploy/alea_config.py
+++ src/openalea/deploy/alea_config.py
@@ -2,12 +2,21 @@
 
 
 from openalea.deploy import check_system
+import os, sys
 
 def main():
 
     env = check_system()
-    for k, v in env.items():
-        print "export %s=%s\n"%(k, v)
+
+    if('posix' in os.name):
+        for k, v in env.items():
+            print "export %s=%s\n"%(k, v)
+            
+    elif('win' in sys.platform):
+        for k, v in env.items():
+            print "set %s=%s\n"%(k, v)
+
+    
 
 
 if(__name__ == '__main__'):
