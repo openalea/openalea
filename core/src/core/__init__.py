fde16ebe7961cf4a645d998af3638b94acbe6b5c
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-10-02 15:59:46 +0000

    add global_module convenience function
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@813 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/__init__.py
+++ core/src/core/__init__.py
@@ -1,3 +1,8 @@
 from external import *
 
 
+def global_module(module):
+    """ Declare a module accessible everywhere """
+    import __builtin__
+    __builtin__.__dict__[module.__name__] = module
+
