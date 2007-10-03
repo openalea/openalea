c000606e97e3964b7b5ab16f4b928ccd28eada07
dasilva <dasilva@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-10-03 18:12:11 +0000

    LR legend upgrade
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@842 04ea39c6-a500-0410-8508-e77fe2a861cf

--- rpywrap/src/rpywrap/rstat.py
+++ rpywrap/src/rpywrap/rstat.py
@@ -132,7 +132,7 @@ class LR2Plot( Node ):
         reg_legend = "y = "+str( round( reg[ 'pente' ],3 ) )+ \
                      "x + "+str( round(reg[ 'intercept' ],3 ))+ \
                      " $\pm$ "+str( round( reg[ 'ic' ],3 ) )+ \
-                     "    r2 = "+str( round( reg[ 'r2' ],3 ) )
+                     r"    $r^2$ = "+str( round( reg[ 'r2' ],3 ) )
         reg_color='red'
         points = plotable.VisualSequence2D( x=reg[ 'x' ], y=reg[ 'y' ], legend= 'Data', linestyle='None', marker = '^', color='dodgerblue' )
         line = plotable.VisualSequence2D( x=reg_x, y=reg_y, legend=reg_legend, linestyle='-', marker='None', color=reg_color )
