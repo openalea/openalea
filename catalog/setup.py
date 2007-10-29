887cb4f2ac255695bee72b4d99f39d9431c1f559
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2007-10-29 16:51:49 +0000

    Change version to 0.3.2 Add packagedir node Adapt core to support packagedir node
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/openalea-0.3@964 04ea39c6-a500-0410-8508-e77fe2a861cf

--- catalog/setup.py
+++ catalog/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 name = 'OpenAlea.Catalog'
 namespace = 'openalea'
 pkg_name = 'openalea.catalog'
-version = '0.3.1' 
+version = '0.3.2' 
 description = 'OpenAlea Logical Component Catalog.' 
 long_description = ''
 
