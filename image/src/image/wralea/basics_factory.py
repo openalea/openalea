d5b62a50b874244b3919e2e5375ffde05df7b313
dasilva <dasilva@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-01-16 17:02:18 +0000

    basic wralea
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1045 04ea39c6-a500-0410-8508-e77fe2a861cf

--- image/src/image/wralea/basics_factory.py
+++ image/src/image/wralea/basics_factory.py
@@ -26,4 +26,41 @@ __revision__=" $Id: graph.py 116 2007-02-07 17:44:59Z tyvokka $ "
 from openalea.core import Factory
 
 def define_factory (package) :
-	pass
+
+    nf = Factory( name= "load image", 
+                  description= "load an image file", 
+                  category = "Image", 
+                  nodemodule = "utils_nodes",
+                  nodeclass = "load_image",
+                  inputs=(dict(name="Filename", interface=IFileStr,),),
+                  outputs=(dict(name="Image", interface=IPix,),),
+                  )
+
+    package.add_factory( nf )
+
+    nf = Factory( name= "save image", 
+                  description= "save an image file", 
+                  category = "Image", 
+                  nodemodule = "utils_nodes",
+                  nodeclass = "save_image",
+                  inputs=(dict(name="Image", interface=IPix,),
+                          dict(name="Filename", interface=IFileStr,),
+                          ),
+                  outputs=(dict(name="Image", interface=IPix,),),
+                  )
+
+    package.add_factory( nf )
+
+    nf = Factory( name= "convert mode", 
+                  description= "change the color mode of the image", 
+                  category = "Image", 
+                  nodemodule = "utils_nodes",
+                  nodeclass = "convert",
+                  inputs=(dict(name="Image", interface=IPix,),
+                          dict(name="Mode", interface=IStr,),
+                          ),
+                  outputs=(dict(name="Image", interface=IPix,),),
+                  )
+
+    package.add_factory( nf )
+
