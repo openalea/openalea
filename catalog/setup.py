89146eeca60639786bbac99117563a3773cfaeb1
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-01-16 12:31:06 +0000

    Change versions
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1030 04ea39c6-a500-0410-8508-e77fe2a861cf

--- catalog/setup.py
+++ catalog/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 name = 'OpenAlea.Catalog'
 namespace = 'openalea'
 pkg_name = 'openalea.catalog'
-version = '0.3.0a' 
+version = '0.4.0' 
 description = 'OpenAlea Logical Component Catalog.' 
 long_description = ''
 
