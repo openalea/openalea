b6845a6659357b2b76c8a7ff51c0318d284e08c2
dufourko <dufourko@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-07-31 14:11:45 +0000

    implemente feature #6010 (block node) Rewrite some wralea
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1332 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/__init__.py
+++ core/src/core/__init__.py
@@ -1,8 +1,8 @@
 from openalea.core.external import *
 
 
-# def global_module(module):
-#     """ Declare a module accessible everywhere """
-#     import __builtin__
-#     __builtin__.__dict__[module.__name__] = module
+def global_module(module):
+    """ Declare a module accessible everywhere """
+    import __builtin__
+    __builtin__.__dict__[module.__name__] = module
 
