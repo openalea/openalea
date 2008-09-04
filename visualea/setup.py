9fe891c78ae9481187fa6196618fde0df9953f0d
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2008-09-04 09:18:58 +0000

    mv rgb node to openalea.color
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@1373 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/setup.py
+++ visualea/setup.py
@@ -11,7 +11,7 @@ pkg_name = 'openalea.visualea'
 sys.path.append("src")
 import visualea.metainfo as metainfo
 
-version = 0.4
+version = 0.4.1
 
 description = 'OpenAlea visual programming environment.' 
 long_description = ''
