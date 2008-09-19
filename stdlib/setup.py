def690ce055651d73dfba3294d0172c930b1637f
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-09-19 12:41:15 +0000

    * Fix packaging bug: openalea package was not installed properly.
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1387 04ea39c6-a500-0410-8508-e77fe2a861cf

--- stdlib/setup.py
+++ stdlib/setup.py
@@ -1,12 +1,12 @@
 import os, sys
 pj = os.path.join
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name = 'OpenAlea.StdLib',
-    version = '0.5.0' ,
+    version = '0.5.1' ,
     description = 'OpenAlea standard logical component library.', 
     long_description = '',
     author = 'OpenAlea consortium',
@@ -15,22 +15,10 @@ setup(
     license = 'Cecill-C',
 
 
-    namespace_packages=['openalea'],
     create_namespaces=False,
     zip_safe=False,
 
-    packages=['openalea.color', 'openalea.data',
-              'openalea.csv', 'openalea.file',
-              'openalea.functional',
-              'openalea.math', 'openalea.model',
-              'openalea.pickling', 'openalea.python',
-              'openalea.string',
-              'openalea.plotools',
-              'openalea.image',
-              'openalea.stat',
-              'openalea.spatial',
-              'openalea.stand',
-              ],
+    packages=find_packages('src'),
 
     package_dir={"":"src" },
 
@@ -42,21 +30,20 @@ setup(
     # entry_points
     entry_points = {
         "wralea": ['openalea.color = openalea.color', 
-                   'openalea.data = openalea.data',
                    'openalea.csv = openalea.csv', 
+                   'openalea.data = openalea.data',
                    'openalea.file = openalea.file',
                    'openalea.functional = openalea.functional',
+                   'openalea.image = openalea.image',
                    'openalea.math = openalea.math', 
                    'openalea.model = openalea.model',
                    'openalea.pickling = openalea.pickling', 
-                   'openalea.python = openalea.python',
-                   'openalea.string = openalea.string',
                    'openalea.plotools = openalea.plotools',
-                   'openalea.image = openalea.image',
-                   'openalea.stat = openalea.stat',
+                   'openalea.python = openalea.python',
                    'openalea.spatial = openalea.spatial',
                    'openalea.stand = openalea.stand',
-                   
+                   'openalea.stat = openalea.stat',
+                   'openalea.string = openalea.string',
 
                    # Deprecated
                    'catalog.color = deprecated', 
