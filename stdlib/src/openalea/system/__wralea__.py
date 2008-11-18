39199654e84cae4caa8b88a48a351b30180ba206
boudon <boudon@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-11-18 16:09:42 +0000

    add first draft of vprint
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1430 04ea39c6-a500-0410-8508-e77fe2a861cf

--- stdlib/src/openalea/system/__wralea__.py
+++ stdlib/src/openalea/system/__wralea__.py
@@ -56,5 +56,20 @@ cmd = Factory(name="command",
 __all__.append('cmd')
 
 
+vprint = Factory(name="vprint", 
+             description="Visual Print", 
+             category="System", 
+             nodemodule="vprint",
+             nodeclass="VPrint",
+             inputs = (dict(name="obj", interface=None, desc='The object to display'), 
+                       dict(name="caption", interface=IStr, desc='The caption of the display',value='Value is'), #,hide=True
+                       dict(name="blocking", interface=IBool, desc='The caption of the display',value=False),    #,hide=True
+                       dict(name="strfunc", interface=None, desc='The function to convert the object to a string',value=str), #,hide=True
+                       ),
+             outputs = ( dict(name="returned_obj", interface=None, desc='The object'), ),
+                     )
+
+__all__.append('vprint')
+
 
 
