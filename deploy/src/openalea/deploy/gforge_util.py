4803eebb22ce0467a330401c9df8ad3849e131b9
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-12-09 15:05:04 +0000

    change docstring to avoid sphinx warnings
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1453 04ea39c6-a500-0410-8508-e77fe2a861cf

--- deploy/src/openalea/deploy/gforge_util.py
+++ deploy/src/openalea/deploy/gforge_util.py
@@ -22,26 +22,25 @@
 # Lesser General Public License for more details.
 #
 """
-Usage:
-  Enables the use of multipart/form-data for posting forms
+:Usage: Enables the use of multipart/form-data for posting forms
 
-Inspirations:
+:Inspirations:
   Upload files in python:
     http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/146306
   urllib2_file:
     Fabien Seisen: <fabien@seisen.org>
 
-Example:
-  import MultipartPostHandler, urllib2, cookielib
+:Example:
 
-  cookies = cookielib.CookieJar()
-  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies),
-                                MultipartPostHandler.MultipartPostHandler)
-  params = { "username" : "bob", "password" : "riviera",
-             "file" : open("filename", "rb") }
-  opener.open("http://wwww.bobsite.com/upload/", params)
+>>> import MultipartPostHandler, urllib2, cookielib
+    cookies = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies),
+        MultipartPostHandler.MultipartPostHandler)
+    params = { "username" : "bob", "password" : "riviera",
+        "file" : open("filename", "rb") }
+    opener.open("http://wwww.bobsite.com/upload/", params)
 
-Further Example:
+:Further Example:
   The main function of this file is a sample which downloads a page and
   then uploads it to the W3C validator.
 """
