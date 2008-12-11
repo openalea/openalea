41704a993b7927e964a8e79be21a7aeb70c7e852
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-12-11 15:13:36 +0000

    update the docstring (restructured text, sphinx) and clean up the code (pep8)
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1472 04ea39c6-a500-0410-8508-e77fe2a861cf

--- stdlib/src/openalea/string/strings.py
+++ stdlib/src/openalea/string/strings.py
@@ -2,18 +2,18 @@
 #
 #       OpenAlea.StdLib
 #
-#       Copyright 2006 - 2008 INRIA - CIRAD - INRA  
+#       Copyright 2006 - 2008 INRIA - CIRAD - INRA
 #
 #       Distributed under the Cecill-C License.
 #       See accompanying file LICENSE.txt or copy at
 #           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
-# 
+#
 #       OpenAlea WebSite : http://openalea.gforge.inria.fr
 #
-################################################################################
+#############################################################################
 
 
-__doc__=""" String manipulation """
+__doc__="""String manipulation"""
 __license__= "Cecill-C"
 __revision__=" $Id$ "
 
@@ -21,49 +21,49 @@ from openalea.core import *
 
 # File name manipulation
 
-def str_split(string_, char):
-    """
-Split a String
-In 0 : a String
-In 1 : The split character 
-Out :  The splitted string
-    """
 
-    return (string_.split(char),)
+def str_split(string_, char):
+    """Split a String
+
+    :param string_ : a String
+    :param char : a split character
+    :returns: the splitted string
+    """
+    return (string_.split(char), )
 
 
 def str_join(list_, char):
-    """
-Join a list
-In 0 : A list of String
-In 1 : The join character 
-Out :  The joinned string
-    """
+    """Join a list
 
-    return (char.join(list_),)
+    :param list_: A list of String
+    :param char: the join character
+    :returns:  the joinned string
+    """
+    return (char.join(list_), )
+
 
 def str_strip(string_, chars):
     """
     Return a copy of the string with leading and trailing whitespace removed.
     """
-    return (string_.strip(chars),)
+    return (string_.strip(chars), )
 
 
 class String(Node):
     """
-String Variable
-Input 0 : The stored value
-Ouput 0 : Transmit the stored value
-    """
+    String Variable
 
+    :param 0: the stored value
+    :returns 0: transmit the stored value
+    """
 
     def __call__(self, inputs):
         """ inputs is the list of input values """
         s = str(inputs[0])
         self.set_caption(repr(s))
-        return ( s, )
+        return (s, )
 
 
-def text( long_str ):
+def text(long_str):
     """ Return a copy of the input string. """
-    return (str(long_str),)
+    return (str(long_str), )
