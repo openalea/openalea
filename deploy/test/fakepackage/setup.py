8570d3089d6604950463b4880c187567758d0860
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2008-12-11 13:36:23 +0000

    correction of a bug in setup.py that prevents the test to build the fakepackage. Although the test can go through a few sub tests, it is still failing during the distribution process.
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1471 04ea39c6-a500-0410-8508-e77fe2a861cf

--- deploy/test/fakepackage/setup.py
+++ deploy/test/fakepackage/setup.py
@@ -37,8 +37,8 @@ setup(
     zip_safe= False,
 
     # Specific options of openalea.deploy
-    lib_dirs = {'lib' : pj(build_prefix, 'lib') ,
-    inc_dirs = { 'include' : pj(build_prefix, 'include') },
+    lib_dirs = {'lib': pj(build_prefix, 'lib') },
+    inc_dirs = {'include': pj(build_prefix, 'include') } ,
     share_dirs = { 'share' : 'examples'},
     postinstall_scripts = ['openalea.fakepackage.postinstall',],
     
