9433e3bc475e23f667a0e773c5414d3f89065da8
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-12 08:02:34 +0000

    add a few comments and category keywords
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@1474 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/category.py
+++ core/src/core/category.py
@@ -23,14 +23,10 @@ __license__= "Cecill-C"
 __revision__=" $Id: pkgmanager.py 1433 2008-11-27 08:52:19Z cokelaer $ "
 
 import openalea
-
 import sys
 import os
 import glob
 
-
-
-###############################################################################
 
 class PackageManagerCategory():
     """
@@ -41,41 +37,49 @@ class PackageManagerCategory():
     def __init__ (self):
         """ Constructor """
 
+        # note that keywords here below have small caps except PGL
+        # the keywords in the wralea files may have big or small caps though
         self.keywords = { 
-            'image':        'proposal.Image Processing.standard', 
-            'svg':          'proposal.Image Processing.svg', 
-            'datatype':     'proposal.Python Functionalities.Datatype', 
-            'type':         'proposal.Python Functionalities.Datatype', 
-            'functional':   'proposal.Python Functionalities.functional', 
-            'math':         'proposal.Python Functionalities.math', 
-            'python':       'proposal.Python Functionalities.standard', 
-            'data':         'proposal.Raw Data (txt,geom,png,etc)', 
-            'tutorial':     'proposal.Tutorial', 
-            'workflow':     'proposal.Workflow', 
-            'flow control': 'proposal.Workflow', 
-            'system':       'proposal.System Interaction', 
-            'io':           'proposal.Data I/O', 
-            'file':         'proposal.Data I/O', 
-            'test':         'proposal.Test', 
-            'misc':         'proposal.Miscellaneous', 
-            'plot':         'proposal.Visualisation', 
-            'graphics':     'proposal.Visualisation', 
-            'graphic':      'proposal.Visualisation', 
-            'visualization':'proposal.Visualisation', 
-            'visualisation':'proposal.Visualisation', 
-            'spatial':      'proposal.Scene Design', 
-            'scene':        'proposal.Scene Design', 
-            'light':        'proposal.Scene Design', 
-            'PGL Object Generator':'proposal.Scene Design', 
-            'fractal analysis':'proposal.Fractal Analysis', 
-            'demo':         'proposal.Demo', 
-            'composite':    'proposal.Composite', 
-            'descriptive':  'proposal.Data Processing.statistics', 
-            'stat':         'proposal.Data Processing.statistics', 
-            'regression':   'proposal.Data Processing.regression', 
-            'fitting':      'proposal.Data Processing.fitting', 
-            'distribution': 'proposal.Data Processing.distribution', 
-            'test':         'proposal.Data Processing.statistical tests', 
+            'image':        'proposal.Image Processing.standard',
+            'svg':          'proposal.Image Processing.svg',
+            'datatype':     'proposal.Python Functionalities.Datatype',
+            'type':         'proposal.Python Functionalities.Datatype',
+            'functional':   'proposal.Python Functionalities.functional',
+            'math':         'proposal.Python Functionalities.math',
+            'python':       'proposal.Python Functionalities.standard',
+            'data':         'proposal.Raw Data (txt,geom,png,etc)',
+            'tutorial':     'proposal.Tutorial',
+            'workflow':     'proposal.Workflow',
+            'flow control': 'proposal.Workflow',
+            'system':       'proposal.System Interaction',
+            'io':           'proposal.Data I/O',
+            'file':         'proposal.Data I/O',
+            'test':         'proposal.Test',
+            'misc':         'proposal.Miscellaneous',
+            'spatial':      'proposal.Scene Design.spatial',
+            'scene':        'proposal.Scene Design',
+            'light':        'proposal.Scene Design.light',
+            'PGL Object Generator':'proposal.Scene Design.PGL',
+            'demo':         'proposal.Demo',
+            'composite':    'proposal.Composite',
+            'descriptive':  'proposal.Data Processing.statistics',
+            'stat':         'proposal.Data Processing.statistics',
+            'statistics':   'proposal.Data Processing.statistics',
+            'regression':   'proposal.Data Processing.regression',
+            'fitting':      'proposal.Data Processing.fitting',
+            'distribution': 'proposal.Data Processing.distribution',
+            'test':         'proposal.Data Processing.statistical tests',
             'datamine':     'proposal.Data processing.Data Manipulation and Transformation',
-            'codec':        'proposal.Data processing.Data Manipulation and Transformation'}
+            'string':       'proposal.Data processing.Data Manipulation and Transformation',
+            'codec':        'proposal.Data processing.Data Manipulation and Transformation',
+            'fractal analysis':'proposal.Fractal Analysis',
+            'plot':         'proposal.Visualisation',
+            'graphics':     'proposal.Visualisation',
+            'graphic':      'proposal.Visualisation',
+            'visualization':'proposal.Visualisation',
+            'visualisation':'proposal.Visualisation',
+            'web':          'proposal.Web Services',
+            'unclassified':'proposal.to Classify'}
+            # here, I keep "to Classify with a lower case so that it appears 
+            # at the end of the category list (all others have a big cap) 
         
