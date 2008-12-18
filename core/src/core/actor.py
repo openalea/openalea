5b9fc216c57efde786380b818bc3bee6bb6dc3cb
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-18 16:11:29 +0000

    convert to pep8 convention
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@1498 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/actor.py
+++ core/src/core/actor.py
@@ -2,60 +2,57 @@
 #
 #       OpenAlea.Core
 #
-#       Copyright 2006-2007 INRIA - CIRAD - INRA  
+#       Copyright 2006-2007 INRIA - CIRAD - INRA
 #
 #       File author(s): Jerome Chopard <jerome.chopard@sophia.inria.fr>
 #
 #       Distributed under the Cecill-C License.
 #       See accompanying file LICENSE.txt or copy at
 #           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
-# 
-#       OpenAlea WebSite : http://openalea.gforge.inria.fr
 #
-################################################################################
+#       OpenAlea WebSite: http://openalea.gforge.inria.fr
+#
+###############################################################################
+
+__doc__ = """This module provide an actor interface"""
+__license__ =  "Cecill-C"
+__revision__ = " $Id$ "
 
 
-__doc__="""
-This module provide an actor interface
-"""
+class IActor(object):
+    """
+    interface to emulate a function
+    """
+    def inputs(self):
+        """
+        iterate on all input description
+        return iter of (input key,input interface)
+        """
+        raise NotImplementedError
 
-__license__= "Cecill-C"
-__revision__=" $Id$ "
+    def outputs(self):
+        """
+        iterate on all output descriptions
+        return iter of (otuput key,output interface)
+        """
+        raise NotImplementedError
 
-class IActor (object) :
-	"""
-	interface to emulate a function
-	"""
-	def inputs (self) :
-		"""
-		iterate on all input description
-		return iter of (input key,input interface)
-		"""
-		raise NotImplementedError
-	
-	def outputs (self) :
-		"""
-		iterate on all output descriptions
-		return iter of (otuput key,output interface)
-		"""
-		raise NotImplementedError
-	
-	def eval (self) :
-		"""
-		function called after setting the input
-		to compute output values
-		"""
-		raise NotImplementedError
-	
-	def set_input (self, key, value) :
-		"""
-		set input specified by a key to value
-		"""
-		raise NotImplementedError
-	
-	def output (self, key) :
-		"""
-		get value computed for output
-		"""
-		raise NotImplementedError
+    def eval(self):
+        """
+        function called after setting the input
+        to compute output values
+        """
+        raise NotImplementedError
+
+    def set_input(self, key, value):
+        """
+        set input specified by a key to value
+        """
+        raise NotImplementedError
+
+    def output(self, key):
+        """
+        get value computed for output
+        """
+        raise NotImplementedError
 
