ba07b076cedf4e42eb95c248da7330e1522e794b
Thomas Cokelaer <cokelaer@gmail.com>
2009-01-06 16:47:01 +0000

    update epydoc script to automatically generate the doculentation
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@1519 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/doc/gendoc.py
+++ core/doc/gendoc.py
@@ -1,3 +1,7 @@
+"""script to automatically generate epydoc documentation
+
+
+"""
 import os, sys
 sys.path.append(os.path.abspath("../src/core"))
 import version
@@ -5,14 +9,28 @@ import version
 filepattern = "../src/core"
 outdir = "core-%s"%(version.version)
 admin_name = 'cokelaer'
+
+
+epydoc_options= """
+--html
+--name epydoc_test
+--docformat restructuredtext
+--graph all
+--url http://openalea.gforge.inria.fr
+--include-log
+--show-sourcecode
+"""
+
 def run(cmd, verbose=True, test=True):
-    if verbose: print cmd
-    if not test : os.system(cmd)
+    if verbose:
+        print cmd
+    if not test: 
+        os.system(cmd)
 
 
+# this is the epydoc command line arguments
+opt= ' '.join( epydoc_options.split() )
+run(' epydoc %s -o %s %s  '%(opt, outdir, '../src/core/*.py'), verbose=True, test=True)
 
-run('epydoc --html  -o %s %s --docformat restructuredText --include-log --show-sourcecode '%(outdir, '../src/core/*.py'), verbose=True, test=True)
-
-
-run('scp -r %s %s@scm.gforge.inria.fr:/var/lib/gforge/chroot/home/groups/openalea/htdocs/doc/'%(outdir,admin_name), verbose=True, test=True)
-#os.system('ssh %s@scm.gforge.inria.fr "cd /var/lib/gforge/chroot/home/groups/openalea/htdocs/doc; ln -s %s core"'%(outdir, admin_name))
+# once done, we can scp the documentation
+#run('scp -r %s %s@scm.gforge.inria.fr:/var/lib/gforge/chroot/home/groups/openalea/htdocs/doc/'%(outdir,admin_name), verbose=True, test=True)
