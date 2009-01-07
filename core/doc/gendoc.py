c24c201ec1084e6af1f88152f18854b501e4b1c7
Thomas Cokelaer <cokelaer@gmail.com>
2009-01-07 11:11:04 +0000

    finalize automatic documentation for epydoc
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@1520 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/doc/gendoc.py
+++ core/doc/gendoc.py
@@ -2,14 +2,13 @@
 
 
 """
-import os, sys
+import os
+import sys
 sys.path.append(os.path.abspath("../src/core"))
 import version
 
 filepattern = "../src/core"
 outdir = "core-%s"%(version.version)
-admin_name = 'cokelaer'
-
 
 epydoc_options= """
 --html
@@ -21,16 +20,19 @@ epydoc_options= """
 --show-sourcecode
 """
 
+
 def run(cmd, verbose=True, test=True):
     if verbose:
         print cmd
-    if not test: 
+    if not test:
         os.system(cmd)
 
 
 # this is the epydoc command line arguments
-opt= ' '.join( epydoc_options.split() )
-run(' epydoc %s -o %s %s  '%(opt, outdir, '../src/core/*.py'), verbose=True, test=True)
+opt = ' '.join(epydoc_options.split())
+run('epydoc %s -o %s %s  ' % (opt, outdir, '../src/core/*.py'),
+    verbose=True, test=True)
 
 # once done, we can scp the documentation
-#run('scp -r %s %s@scm.gforge.inria.fr:/var/lib/gforge/chroot/home/groups/openalea/htdocs/doc/'%(outdir,admin_name), verbose=True, test=True)
+run('scp -r %s scm.gforge.inria.fr:/home/groups/openalea/htdocs/doc/'
+    % (outdir), verbose=True, test=True)
