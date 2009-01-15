677c3534eb6d153d0a584f27166eca94f69f8c3e
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2009-01-15 08:47:03 +0000

    add doc string and clean the code (pep8)
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1543 04ea39c6-a500-0410-8508-e77fe2a861cf

--- stdlib/test/test_math_nodes.py
+++ stdlib/test/test_math_nodes.py
@@ -1,97 +1,109 @@
 from openalea.core.alea import run
 from openalea.core.pkgmanager import PackageManager
-from random import random,randint
+from random import random, randint
 import unittest
 import math
 
 """ A unique PackageManager is created for all test of dataflow """
 pm = PackageManager()
 pm.init(verbose=False)
- 
+
+
 def test_add():
     """ Test of node + """
-    res = run(('openalea.math','+'),inputs={'a':1,'b':2},pm=pm)  
+    res = run(('openalea.math', '+'), inputs={'a': 1, 'b': 2}, pm=pm)
     assert res[0] == 3
 
 
 def function_tst(function_name, val1, res=True, arg1name='a'):
     """ Function test """
     epsilon = 1e-15
-    rres = run(('openalea.math',function_name),inputs={arg1name:val1},pm=pm)  
-    if (rres[0] - res) > epsilon :
-        raise ValueError("Bad result for operator '"+function_name+"' with values "+str((val1))+'. Expected '+str(res)+', Got '+str(rres[0])+'.')
-    
-def binary_tst(opname, val1, val2, res = True, arg1name = 'a',arg2name = 'b'):
+    rres = run(('openalea.math', function_name), inputs={arg1name: val1}, \
+        pm=pm)
+    if (rres[0] - res) > epsilon:
+        raise ValueError("Bad result for operator '"\
+            +function_name+"' with values "+str((val1))\
+            +'. Expected '+str(res)+', Got '+str(rres[0])+'.')
+
+
+def binary_tst(opname, val1, val2, res = True, arg1name = 'a', arg2name = 'b'):
     """ Binary Test """
-    rres = run(('openalea.math',opname),inputs={arg1name:val1,arg2name:val2},pm=pm)  
-    if rres[0] != res :
-        raise ValueError("Bad result for operator '"+opname+"' with values "+str((val1,val2))+'. Expected '+str(res)+', Got '+str(rres[0])+'.')
-    
+    rres = run(('openalea.math', opname), \
+        inputs={arg1name: val1, arg2name: val2}, pm=pm)
+    if rres[0]!=res:
+        raise ValueError("Bad result for operator '"\
+            +opname+"' with values "+str((val1, val2))\
+            +'. Expected '+str(res)+', Got '+str(rres[0])+'.')
+
+
 def test_eq():
     """ Test of equal operators """
-    val1,val2 = random()*10000, random()*10000
-    binary_tst('!=',val1,val2,val1!=val2)
-    binary_tst('!=',val1,val1,False)
-    binary_tst('==',val1,val2,val1==val2)
-    binary_tst('==',val1,val1,True)
-    val1,val2 = randint(0,10000), randint(0,10000)
-    binary_tst('!=',val1,val2,val1!=val2)
-    binary_tst('!=',val1,val1,False)
-    binary_tst('==',val1,val2,val1==val2)
-    binary_tst('==',val1,val1,True)
+    val1, val2 = random()*10000, random()*10000
+    binary_tst('!=', val1, val2, val1!=val2)
+    binary_tst('!=', val1, val1, False)
+    binary_tst('==', val1, val2, val1==val2)
+    binary_tst('==', val1, val1, True)
+    val1, val2 = randint(0, 10000), randint(0, 10000)
+    binary_tst('!=', val1, val2, val1!=val2)
+    binary_tst('!=', val1, val1, False)
+    binary_tst('==', val1, val2, val1==val2)
+    binary_tst('==', val1, val1, True)
+
 
 def test_sup():
     """ Test of sup operators """
     val1 = random()*10000
     val2 = val1 + (random()*10000)+1
-    binary_tst('>',val1,val2,False)
-    binary_tst('>',val2,val1,True)
-    binary_tst('>',val1,val1,False)
-    binary_tst('>=',val1,val2,False)
-    binary_tst('>=',val2,val1,True)
-    binary_tst('>=',val1,val1,True)
-    val1 = randint(0,10000)
-    val2 = val1 + randint(0,10000)+1
-    binary_tst('>',val1,val2,False)
-    binary_tst('>',val2,val1,True)
-    binary_tst('>',val1,val1,False)
-    binary_tst('>=',val1,val2,False)
-    binary_tst('>=',val2,val1,True)
-    binary_tst('>=',val1,val1,True)
-    
+    binary_tst('>', val1, val2, False)
+    binary_tst('>', val2, val1, True)
+    binary_tst('>', val1, val1, False)
+    binary_tst('>=', val1, val2, False)
+    binary_tst('>=', val2, val1, True)
+    binary_tst('>=', val1, val1, True)
+    val1 = randint(0, 10000)
+    val2 = val1 + randint(0, 10000)+1
+    binary_tst('>', val1, val2, False)
+    binary_tst('>', val2, val1, True)
+    binary_tst('>', val1, val1, False)
+    binary_tst('>=', val1, val2, False)
+    binary_tst('>=', val2, val1, True)
+    binary_tst('>=', val1, val1, True)
+
+
 def test_inf():
     """ Test of inf operators """
     val1 = random()*10000
     val2 = val1 + (random()*10000)+1
-    binary_tst('<',val1,val2,True)
-    binary_tst('<',val2,val1,False)
-    binary_tst('<',val1,val1,False)
-    binary_tst('<=',val1,val2,True)
-    binary_tst('<=',val2,val1,False)
-    binary_tst('<=',val1,val1,True)
-    val1 = randint(0,10000)
-    val2 = val1 + randint(0,10000)+1
-    binary_tst('<',val1,val2,True)
-    binary_tst('<',val2,val1,False)
-    binary_tst('<',val1,val1,False)
-    binary_tst('<=',val1,val2,True)
-    binary_tst('<=',val2,val1,False)
-    binary_tst('<=',val1,val1,True)
-   
+    binary_tst('<', val1, val2, True)
+    binary_tst('<', val2, val1, False)
+    binary_tst('<', val1, val1, False)
+    binary_tst('<=', val1, val2, True)
+    binary_tst('<=', val2, val1, False)
+    binary_tst('<=', val1, val1, True)
+    val1 = randint(0, 10000)
+    val2 = val1 + randint(0, 10000)+1
+    binary_tst('<', val1, val2, True)
+    binary_tst('<', val2, val1, False)
+    binary_tst('<', val1, val1, False)
+    binary_tst('<=', val1, val2, True)
+    binary_tst('<=', val2, val1, False)
+    binary_tst('<=', val1, val1, True)
+
 
 def test_binaries():
-    """ Test of %""" 
-    val1 = randint(0,10000)
-    val2 = randint(1,5) 
+    """ Test of %"""
+    val1 = randint(0, 10000)
+    val2 = randint(1, 5)
     val1 *= val2
-    # val1 is guaranteed to be a multiple of val2, so the following test must 
+    # val1 is guaranteed to be a multiple of val2, so the following test must
     # always return 0.
     binary_tst('%', val1, val2, 0)
     binary_tst('*', 2, 3, 6)
     binary_tst('+', 2, 3, 5)
     binary_tst('-', 2, 3, -1)
     binary_tst('/', 4, 2, 2)
-    
+
+
 def test_trigo():
     """ Test trigonometric functions """
     function_tst('cos', math.pi, -1)
@@ -101,10 +113,11 @@ def test_trigo():
     function_tst('acos', -1, math.pi)
     function_tst('asin', -1, math.pi)
 
-def test_others(): 
-    binary_tst('cmp', 0, 1,-1)
-    binary_tst('cmp', 1, 0,1)
-    binary_tst('cmp', 1, 1,0)
+
+def test_others():
+    binary_tst('cmp', 0, 1, -1)
+    binary_tst('cmp', 1, 0, 1)
+    binary_tst('cmp', 1, 1, 0)
 
 
 if __name__=='__main__':
--- stdlib/test/test_pybase_node.py
+++ stdlib/test/test_pybase_node.py
@@ -1,6 +1,6 @@
 from openalea.core.alea import run
 from openalea.core.pkgmanager import PackageManager
-from random import random,randint
+from random import random, randint
 
 """ A unique PackageManager is created for all test of dataflow """
 pm = PackageManager()
@@ -9,119 +9,165 @@ pm.init(verbose=False)
 
 def test_ifelse():
     """ Test of node ifelse """
-    res = run(('openalea.python method','ifelse'),inputs={'c':True,'a':'a','b':'b'},pm=pm)  
+    res = run(('openalea.python method', 'ifelse'),\
+        inputs={'c': True, 'a': 'a', 'b': 'b'}, pm=pm)
     assert res[0] == 'a'
-    res = run(('openalea.python method','ifelse'),inputs={'c':False,'a':'a','b':'b'},pm=pm)  
+    res = run(('openalea.python method', 'ifelse'),\
+        inputs={'c': False, 'a': 'a', 'b': 'b'}, pm=pm)
     assert res[0] == 'b'
-    
+
+
 def test_getitem():
     """ Test of node getitem """
-    res = run(('openalea.python method','getitem'),inputs={'obj':{'a':1,'b':2},'key':'a'},pm=pm)  
+    res = run(('openalea.python method', 'getitem'),\
+        inputs={'obj': {'a': 1, 'b': 2}, 'key': 'a'}, pm=pm)
     assert res[0] == 1
-    res = run(('openalea.python method','getitem'),inputs={'obj':range(10),'key':2},pm=pm)  
+    res = run(('openalea.python method', 'getitem'),\
+        inputs={'obj': range(10), 'key': 2}, pm=pm)
     assert res[0] == 2
+
 
 def test_setitem():
     """ Test of node setitem """
-    res = run(('openalea.python method','setitem'),inputs={'obj':{'a':1,'b':2},'key':'c','value':3},pm=pm)  
+    res = run(('openalea.python method', 'setitem'),
+        inputs={'obj': {'a': 1, 'b': 2}, 'key': 'c', 'value': 3}, pm=pm)
     assert len(res[0]) == 3
-    res = run(('openalea.python method','setitem'),inputs={'obj':range(10),'key':2,'value':3},pm=pm)  
+    res = run(('openalea.python method', 'setitem'),
+        inputs={'obj': range(10), 'key': 2, 'value': 3}, pm=pm)
     assert len(res[0]) == 10
     assert res[0][2] == 3
-    
+
+
 def test_delitem():
     """ Test of node delitem """
-    res = run(('openalea.python method','delitem'),inputs={'obj':{'a':1,'b':2},'key':'a'},pm=pm)  
+    res = run(('openalea.python method', 'delitem'),
+        inputs={'obj': {'a': 1, 'b': 2}, 'key': 'a'}, pm=pm)
     assert len(res[0]) == 1
-    res = run(('openalea.python method','delitem'),inputs={'obj':range(10),'key':2},pm=pm)  
+    res = run(('openalea.python method', 'delitem'),
+        inputs={'obj': range(10), 'key': 2}, pm=pm)
     assert len(res[0]) == 9
+
 
 def test_len():
     """ Test of node len """
-    res = run(('openalea.python method','len'),inputs={'obj':{'a':1,'b':2}},pm=pm)  
+    res = run(('openalea.python method', 'len'),
+        inputs={'obj': {'a': 1, 'b': 2}}, pm=pm)
     assert res[0] == 2
-    res = run(('openalea.python method','len'),inputs={'obj':range(10)},pm=pm)  
+    res = run(('openalea.python method', 'len'),
+        inputs={'obj': range(10)}, pm=pm)
     assert res[0] == 10
+
 
 def test_keys():
     """ Test of node keys """
-    res = run(('openalea.python method','keys'),inputs={'obj':{'a':1,'b':2}},pm=pm)  
-    assert res[0] == ['a','b']
+    res = run(('openalea.python method', 'keys'),
+        inputs={'obj': {'a': 1, 'b': 2}}, pm=pm)
+    assert res[0] == ['a', 'b']
+
 
 def test_values():
     """ Test of node values """
-    res = run(('openalea.python method','values'),inputs={'obj':{'a':1,'b':2}},pm=pm)  
-    assert res[0] == [1,2]
+    res = run(('openalea.python method', 'values'),
+        inputs={'obj': {'a': 1, 'b': 2}}, pm=pm)
+    assert res[0] == [1, 2]
+
 
 def test_items():
     """ Test of node items """
-    res = run(('openalea.python method','items'),inputs={'obj':{'a':1,'b':2}},pm=pm)  
-    assert res[0] == [('a',1),('b',2)]
+    res = run(('openalea.python method', 'items'),
+        inputs={'obj': {'a': 1, 'b': 2}}, pm=pm)
+    assert res[0] == [('a', 1), ('b', 2)]
+
 
 def test_range():
     """ Test of node range """
-    res = run(('openalea.python method','range'),inputs={'start':0, 'stop':10, 'step':2},pm=pm)  
-    assert res[0] == range(0,10,2)
+    res = run(('openalea.python method', 'range'),
+        inputs={'start': 0, 'stop': 10, 'step': 2}, pm=pm)
+    assert res[0] == range(0, 10, 2)
+
 
 def test_enumerate():
     """ Test of node enumerate """
-    res = run(('openalea.python method','enumerate'),inputs={'obj':range(2,10)},pm=pm)  
-    assert res[0] == [(i-2,i) for i in range(2,10)]
+    res = run(('openalea.python method', 'enumerate'),
+        inputs={'obj': range(2, 10)}, pm=pm)
+    assert res[0] == [(i-2, i) for i in range(2, 10)]
+
 
 def test_print():
     """ Test of node print """
     val = 'test'
-    res = run(('openalea.python method','print'),inputs={'x':val},pm=pm)  
+    res = run(('openalea.python method', 'print'),
+        inputs={'x': val}, pm=pm)
     assert res[0] == val
+
 
 def test_method():
     """ Test of node method """
+
     class Dummy:
+
         def __init__(self):
             self.tvalue = False
-        def test(self,value=True):
+
+        def test(self, value=True):
             self.tvalue = value
     val = Dummy()
-    res = run(('openalea.python method','method'),inputs={'obj':val, 'member_name':'test'},pm=pm)  
+    res = run(('openalea.python method', 'method'),
+        inputs={'obj': val, 'member_name': 'test'}, pm=pm)
     assert res[0] == val
     assert val.tvalue == True
     val.tvalue = False
-    res = run(('openalea.python method','method'),inputs={'obj':val, 'member_name':'test','args':(True,)},pm=pm)  
+    res = run(('openalea.python method', 'method'),
+        inputs={'obj': val, 'member_name': 'test', 'args': (True, )}, pm=pm)
     assert res[0] == val
     assert val.tvalue == True
 
+
 def test_getattr():
     """ Test of node getattr """
+
     class Dummy:
+
         def __init__(self):
             self.tvalue = False
     val = Dummy()
-    res = run(('openalea.python method','getattr'),inputs={'obj':val, 'member_name':'tvalue'},pm=pm)  
+    res = run(('openalea.python method', 'getattr'),
+        inputs={'obj': val, 'member_name': 'tvalue'}, pm=pm)
     assert res[0] == val.tvalue
+
 
 def test_eval():
     """ Test of node eval """
     val = 'True'
-    res = run(('openalea.python method','eval'),inputs={'expression':val},pm=pm)  
+    res = run(('openalea.python method', 'eval'),
+        inputs={'expression': val}, pm=pm)
     assert res[0] == True
+
 
 def test_zip():
     """ Test of node zip """
-    res = run(('openalea.python method','zip'),inputs={'s1':range(10),'s2':range(10)},pm=pm)  
-    assert res[0] == [(i,i) for i in range(10)]
+    res = run(('openalea.python method', 'zip'),
+        inputs={'s1': range(10), 's2': range(10)}, pm=pm)
+    assert res[0] == [(i, i) for i in range(10)]
+
 
 def test_flatten():
     """ Test of node flatten """
-    res = run(('openalea.python method','flatten'),inputs={'obj':[(3*i,3*i+1,3*i+2) for i in range(5)]},pm=pm)  
+    res = run(('openalea.python method', 'flatten'),
+        inputs={'obj': [(3*i, 3*i+1, 3*i+2) for i in range(5)]}, pm=pm)
     assert len(res[0]) == 15
     assert res[0] == range(15)
 
+
 def test_sum():
     """ Test of node sum """
-    res = run(('openalea.python method','sum'),inputs={'sequence':[i for i in range(5)]},pm=pm)  
+    res = run(('openalea.python method', 'sum'),
+        inputs={'sequence': [i for i in range(5)]}, pm=pm)
     assert res[0] == sum(range(5))
+
 
 def test_mean():
     """ Test of node mean """
-    res = run(('openalea.python method','mean'),inputs={'sequence':[i for i in range(5)]},pm=pm)  
+    res = run(('openalea.python method', 'mean'),
+        inputs={'sequence': [i for i in range(5)]}, pm=pm)
     assert res[0] == sum(range(5))/5.
