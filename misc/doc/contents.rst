fcb63a884f3668f14739d5302e1cdf2ffd93eafb
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2009-02-12 12:54:18 +0000

    add sphinx configuration file and reST files
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1608 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ misc/doc/contents.rst
@@ -0,0 +1,53 @@
+.. _misc:
+
+OpenAlea Misc documentation
+###########################
+
+Description
+==================
+
+.. sidebar:: Summary
+
+    :Version: |version|
+    :Release: |release| 
+    :Date: |today|
+    :Author: `See AUTHORS.txt <../../AUTHORS.txt>`_
+    :Changelog: `See ChangeLog.txt <../../ChangeLog.txt>`_
+    
+.. topic:: General 
+
+    |misc| contains a set of tools to help developpers to 
+
+    - generate documentation :mod:`sphinx_tools`, :mod:`gendoc`, :mod:`create_rest_files`
+    - create eggs and upload them to the gforge :mod:`openalea_distrib`, :mod:`upload_dist`
+    - install all packages from a project :mod:`make_develop`
+    
+Documentation
+============= 
+
+.. toctree::
+    :maxdepth: 1
+
+    misc/user_index.rst
+    misc/reference_index.rst
+    
+- A `PDF <../latex/misc.pdf>`_ version of |misc| documentation is available.
+
+.. seealso::
+
+   More documentation can be found on the
+   `openalea <http://openalea.gforge.inria.fr>`__ wiki.
+
+
+
+License
+=======
+
+|misc| is released under a Cecill-C License. See `LICENSE.txt <../../LICENSE.txt>`_    
+
+.. note:: Cecill-C license is a LGPL compatible license.
+
+
+
+
+.. |misc| replace:: OpenAlea.Misc
---
+++ misc/doc/postprocess.py
@@ -0,0 +1,54 @@
+""" clean up tool
+
+The reST files are automatically generated using sphinx_tools.
+
+However, there are known issus which require cleaning.
+
+This code is intended at cleaning these issues until a neat 
+solution is found.
+
+:known problem:
+
+- the misc package is not distributed under openalea namespace for now.
+  Yet, project is openalea and therefore, openalea. namespace are included
+  in the automodule string. They should be removed.
+- Removing the "openalea." string , tha name of the automodule is identical to
+  the module directive name so .. module:: and .. automodule :: are in conflict
+
+:solution: remove "openalea." in the automodule and remove the module directive
+
+"""
+import os
+
+print 'Fixing the module and automodule conflict.'
+
+files = ['./misc/create_rest_files_ref.rst',
+    './misc/path_ref.rst',
+    './misc/download_ref.rst',
+    './misc/sphinx_tools_ref.rst',
+    './misc/gendoc_ref.rst',
+    './misc/upload_dist_ref.rst',
+    './misc/make_develop_ref.rst',
+    './misc/openalea_distrib_ref.rst',]
+
+for file in files: 
+    text = open(file,'r').read()
+    
+    if text.find('.. automodule::')==-1:
+        print 'skip %s which do not contains any automodule directive' % file
+        continue
+    else:
+        lines = text.split('\n')
+        foutput = open(file, 'w')
+        for line in lines:
+            if '.. module::' not in line:
+                if '.. automodule::' in line:
+                    line = line.replace('openalea.','')
+                foutput.write(line+'\n')
+    foutput.close()
+    print file + ' done'
+
+
+    
+print 'Try python setup.py build_sphinx now.'
+
