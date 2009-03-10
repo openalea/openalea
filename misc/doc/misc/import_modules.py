8f03db32ca5cea24480137036aff451f662a9b62
Thomas Cokelaer <cokelaer@gmail.com>
2009-03-10 16:09:11 +0000

    - remove rst files that are now uatomtically generated - update ini and conf.py  to handle intersphinx - update overview.txt (create_rest_file.py removed) - update other rst files (index and contents)
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@1673 04ea39c6-a500-0410-8508-e77fe2a861cf

--- misc/doc/misc/import_modules.py
+++
@@ -1,8 +0,0 @@
-import gendoc
-import sphinx_tools
-import openalea_distrib
-import path
-import create_rest_files
-import download
-import upload_dist
-import make_develop
--- misc/doc/postprocess.py
+++ misc/doc/postprocess.py
@@ -26,6 +26,7 @@ import sphinx_tools
 print 'Fixing the module and automodule conflict.'
 
 files = [
+    './misc/upload_ref.rst',
     './misc/path_ref.rst',
     './misc/download_ref.rst',
     './misc/sphinx_tools_ref.rst',
