ad0f2aede8b1fc4a59d3da87b260a88fe6c8ca03
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2009-09-07 13:51:49 +0000

    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1809 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ deploy/example/setup_boost.py
@@ -0,0 +1,48 @@
+# -*- coding: iso-8859-15 -*-
+
+# Header
+import os, sys
+pj= os.path.join
+
+from setuptools import setup
+
+#Check OS
+if not sys.platform.startswith('win'):
+    print 'This package contain only Boost binary for MS Windows'
+    print 'Please use your system installer to install Boost library'
+    exit(1);
+
+
+# Package name
+name= 'BoostPython'
+version= '1.40'
+
+
+# Description of the package
+
+# Short description
+description= 'Boost.Python binary and header for win32' 
+
+
+license= 'Boost Software License V 1.0' 
+
+
+# For other meta-information, please read the Python distutils documentation.
+
+# Main setup
+setup(
+    # Meta data
+    name=name,
+    version=version,
+    description=description,
+    license=license,
+
+    lib_dirs = { 'lib' : 'lib' },
+    inc_dirs = { 'include' : 'include' },
+
+    setup_requires = ['openalea.deploy'],
+    dependency_links = ['http://openalea.gforge.inria.fr/pi'],
+ 
+    )
+
+
