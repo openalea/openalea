4c93f7204e661a90e58b1b7575118ca525fe9297
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2009-09-18 09:27:02 +0000

    Script to build an egg
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1826 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ deploy/example/build_zip.py
@@ -0,0 +1,49 @@
+import zipfile
+from openalea.core.path import path
+import os
+
+def qt4(directory = path('qt4'), name='qt4'):
+
+    zip_name = path('..')/name + '.zip'
+
+    patterns = ['*2.5.dylib', '*.py', '*.api', '*.so',  '*.txt', '*safe', '*INFO',  '*.4.dylib'] 
+
+    curdir = path(os.curdir).abspath()
+    os.chdir(directory)
+
+    zf=zipfile.ZipFile(zip_name,compression=zipfile.ZIP_DEFLATED,mode='w')
+
+    d = path('.')
+    for pattern in patterns:
+        for fn in d.walkfiles(pattern):
+	    print 'add ', fn
+	    zf.write(fn)
+
+    zf.close()
+    os.chdir(curdir)
+
+def qt4_dev (directory=path('qt4-dev'), name='qt4_dev'):
+
+    zip_name = path('..')/name + '.zip'
+    curdir = path(os.curdir).abspath()
+    os.chdir(directory)
+
+    zf=zipfile.ZipFile(zip_name,compression=zipfile.ZIP_DEFLATED,mode='w')
+
+    dirs = {}
+    dirs['EGG-INFO'] = ['*']
+    dirs['include'] = ['*']
+    dirs['bin'] = ['*']
+    dirs['sip'] = ['*']
+    dirs['lib'] = ['*.4.dylib', '*2.5.dylib']
+
+    for dir, patterns in dirs.iteritems():
+        d = path(dir)
+	for pattern in patterns:
+            for fn in d.walkfiles(pattern):
+	        print 'add ', fn
+	        zf.write(fn)
+
+    zf.close()
+    os.chdir(curdir)
+
