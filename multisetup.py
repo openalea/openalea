86bd3f02891c3aa0591aa19878708ed02854ff29
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2009-10-20 09:28:42 +0000

    add mutisetup rev1898 from trunk (not a merge !!)
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/release_0.7@1901 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ multisetup.py
@@ -0,0 +1,71 @@
+#!/usr/env/python
+# -*- python -*-
+#
+#       Multisetup
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+
+"""
+Multisetup allows to build and install all the packages of OpenAlea
+found in this directory.
+
+:Examples: 
+
+    # Developer mode : Installation of the packages
+    python multisetup.py develop
+    python multisetup.py nosetests -w test
+
+    # User mode: Installation of the packages on the system as root
+    python multisetup.py install
+
+    # Administrator mode: Create distribution of the packages
+    python multisetup.py nosetests -w test install bdist_egg -d ../dist sdist -d ../dist
+
+.. todo::
+    - multisetup -h 
+        * list of the options
+"""
+import os, sys
+
+try:
+    from openalea.misc.multisetup import Multisetup
+except ImportError:
+    print 'Install OpenAlea.Misc first'
+    try:
+        sys.path.append(os.path.join('misc', 'src', 'openalea', 'misc'))
+        from multisetup import MultiSetup
+    except ImportError,e:
+        print e
+
+
+dirs = """
+deploy 
+deploygui 
+core 
+scheduler 
+visualea 
+stdlib 
+sconsx
+misc
+""".split()
+
+
+
+def main():
+
+    args = sys.argv[1:]
+    if  len(args) == 1 and args[0] in ['-h', '--help']:
+        Multisetup.help()
+    else:
+        mysetup = Multisetup(curdir='.', commands=args, packages=dirs)
+        mysetup.run()
+
+
+if __name__ == '__main__':
+    main()
+
