ae7996e41dd4cc2b7c07bf53d8b4c05ad907c08d
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2009-11-24 15:47:17 +0000

    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1978 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ stdlib/src/openalea/multiprocessing/__wralea__.py
@@ -0,0 +1,45 @@
+# -*- python -*-
+#
+#       OpenAlea.StdLib
+#
+#       Copyright 2006-2009 INRIA - CIRAD - INRA  
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+################################################################################
+
+""" openalea.multiprocessing operator """
+__revision__=" $Id: __wralea__.py 1894 2009-10-19 15:04:08Z pradal $ "
+
+
+from openalea.core import *
+
+__name__ = "openalea.multiprocessing"
+
+__alias__ = []
+
+__version__ = '0.0.2'
+__license__ = 'CECILL-C'
+__authors__ = 'OpenAlea Consortium'
+__institutes__ = 'INRIA/CIRAD'
+__description__ = 'Functional Node library.'
+__url__ = 'http://openalea.gforge.inria.fr'
+
+__all__ = ['pmap_',]    
+
+
+pmap_ = Factory( name="pmap",
+               description="Apply a function on a sequence",
+               category="Functional",
+               inputs=(dict(name='func', interface=IFunction), 
+                       dict(name='seq', interface=ISequence), 
+                       dict(name='N', interface=IInt(min=1))),
+               nodemodule="parallel",
+               nodeclass="pymap",
+               )
+
+
