ef4829c3ad3a73cc4057e716b227855c95b44dfa
chopard <chopard@04ea39c6-a500-0410-8508-e77fe2a861cf>
2009-11-26 14:28:52 +0000

    added tools to align nodes accessible from the Workspace menu
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@1983 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/mainwindow.py
+++ visualea/src/visualea/mainwindow.py
@@ -163,6 +163,21 @@ class MainWindow(QtGui.QMainWindow,
         # daniel was here:
         action = self.menu_Workspace.addAction("Change Color")
         self.connect(action, QtCore.SIGNAL("triggered()"), self.color_selection)
+        #jerome too
+        menu = self.menu_Workspace.addMenu("Align")
+        action = menu.addAction("horizontaly")
+        self.connect(action,QtCore.SIGNAL("triggered()"), self.align_selection_horizontal)
+        action = menu.addAction("left")
+        self.connect(action,QtCore.SIGNAL("triggered()"), self.align_selection_left)
+        action = menu.addAction("right")
+        self.connect(action,QtCore.SIGNAL("triggered()"), self.align_selection_right)
+        action = menu.addAction("mean")
+        self.connect(action,QtCore.SIGNAL("triggered()"), self.align_selection_mean)
+        
+        action = menu.addAction("distribute horizontaly")
+        self.connect(action,QtCore.SIGNAL("triggered()"), self.distribute_selection_horizontaly)
+        action = menu.addAction("distribute verticaly")
+        self.connect(action,QtCore.SIGNAL("triggered()"), self.distribute_selection_verticaly)
 
         self.connect(self.action_Run, SIGNAL("triggered()"), self.run)
         self.connect(self.actionReset, SIGNAL("triggered()"), self.reset)
@@ -696,6 +711,178 @@ class MainWindow(QtGui.QMainWindow,
             return
 
         widget.set_selection_color(color)
+        return
+    
+    def align_selection_horizontal (self):
+        """Align all items on a median ligne.
+        """
+        index = self.tabWorkspace.currentIndex()
+        widget = self.index_nodewidget[index]
+        
+        if widget is None :
+            return
+        
+        items = widget.get_selected_item(True)
+        if len(items) > 1 :
+            #find median base #TODO beware of relative to parent coordinates
+            ymean = sum(item.pos().y() for item in items) / len(items)
+            
+            #move all items
+            for item in items :
+                item.setPos(item.pos().x(),ymean)
+        
+            #notify
+            widget.notify(None,("graph_modified",) )
+        
+        return
+
+    def align_selection_left (self):
+        """Align all items on their left side.
+        """
+        index = self.tabWorkspace.currentIndex()
+        widget = self.index_nodewidget[index]
+        
+        if widget is None :
+            return
+        
+        items = widget.get_selected_item(True)
+        if len(items) > 1 :
+            #find left ligne #TODO beware of relative to parent coordinates
+            xmean = sum(item.pos().x() for item in items) / len(items)
+            
+            #move all items
+            for item in items :
+                item.setPos(xmean,item.pos().y() )
+        
+            #notify
+            widget.notify(None,("graph_modified",) )
+        
+        return
+
+    def align_selection_right (self):
+        """Align all items on their right side.
+        """
+        index = self.tabWorkspace.currentIndex()
+        widget = self.index_nodewidget[index]
+        
+        if widget is None :
+            return
+        
+        items = widget.get_selected_item(True)
+        if len(items) > 1 :
+            #find left ligne #TODO beware of relative to parent coordinates
+            xmean = sum(item.pos().x() + \
+                        item.boundingRect().width() \
+                        for item in items) / len(items)
+            
+            #move all items
+            for item in items :
+                item.setPos(xmean - item.boundingRect().width(),item.pos().y() )
+        
+            #notify
+            widget.notify(None,("graph_modified",) )
+        
+        return
+
+    def align_selection_mean (self):
+        """Align all items vertically around a mean ligne.
+        """
+        index = self.tabWorkspace.currentIndex()
+        widget = self.index_nodewidget[index]
+        
+        if widget is None :
+            return
+        
+        items = widget.get_selected_item(True)
+        if len(items) > 1 :
+            #find left ligne #TODO beware of relative to parent coordinates
+            xmean = sum(item.pos().x() + \
+                        item.boundingRect().width() / 2. \
+                        for item in items) / len(items)
+            
+            #move all items
+            for item in items :
+                item.setPos(xmean - item.boundingRect().width() / 2.,item.pos().y() )
+        
+            #notify
+            widget.notify(None,("graph_modified",) )
+        
+        return
+
+    def distribute_selection_horizontaly (self):
+        """distribute the horizontal distances between items.
+        """
+        index = self.tabWorkspace.currentIndex()
+        widget = self.index_nodewidget[index]
+        
+        if widget is None :
+            return
+        
+        items = widget.get_selected_item(True)
+        if len(items) > 2 :
+            #find xmin,xmax of selected items #TODO beware of relative to parent coordinates
+            xmin = min(item.pos().x() for item in items)
+            xmax = max(item.pos().x() + item.boundingRect().width() \
+                       for item in items)
+            
+            #find mean distance between items
+            dist = ( (xmax - xmin) - \
+                   sum(item.boundingRect().width() for item in items) )\
+                   / (len(items) - 1)
+            
+            #sort all items by their mean position
+            item_centers = [(item.pos().x() + item.boundingRect().width() / 2.,item) for item in items]
+            item_centers.sort()
+            
+            #move all items
+            first_item = item_centers[0][1]
+            current_x = first_item.pos().x() + first_item.boundingRect().width()
+            
+            for x,item in item_centers[1:-1] :
+                item.setPos(current_x + dist,item.pos().y() )
+                current_x += dist + item.boundingRect().width()
+        
+            #notify
+            widget.notify(None,("graph_modified",) )
+        
+        return
+
+    def distribute_selection_verticaly (self):
+        """distribute the vertical distances between items.
+        """
+        index = self.tabWorkspace.currentIndex()
+        widget = self.index_nodewidget[index]
+        print "widget",type(widget)
+        if widget is None :
+            return
+        
+        items = widget.get_selected_item(True)
+        if len(items) > 1 :
+            #find ymin,ymax of selected items #TODO beware of relative to parent coordinates
+            ymin = min(item.pos().y() for item in items)
+            ymax = max(item.pos().y() + item.boundingRect().height() \
+                       for item in items)
+            
+            #find mean distance between items
+            dist = ( (ymax - ymin) - \
+                   sum(item.boundingRect().height() for item in items) )\
+                   / (len(items) - 1)
+            
+            #sort all items by their mean position
+            item_centers = [(item.pos().y() + item.boundingRect().height() / 2.,item) for item in items]
+            item_centers.sort()
+            
+            #move all items
+            first_item = item_centers[0][1]
+            current_y = first_item.pos().y() + first_item.boundingRect().height()
+            
+            for y,item in item_centers[1:-1] :
+                item.setPos(item.pos().x(),current_y + dist)
+                current_y += dist + item.boundingRect().height()
+            
+            #notify
+            widget.notify(None,("graph_modified",) )
+        
         return
 
     def delete_selection(self):
