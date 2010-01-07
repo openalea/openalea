32182ef0ddf273b9a6767e2866166bde311fe575
Christophe Pradal <christophe.pradal@inria.fr>
2010-01-07 17:46:07 +0000

    Changing the way metadatadict behaves internally. Previously, a field pointed to an iterable containing both the type of the value AND the value. In the end there was too much that was serialised. Now we seperated the type into another mapping. The interface remains the same. However their could have been some implicits that are not true anymore so some bugs might appear.
    This solves part of the saving problem too by the way.
    
    
    
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@2066 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/metadatadict.py
+++ core/src/core/metadatadict.py
@@ -27,69 +27,82 @@ class MetaDataDict(observer.Observed):
     used to customize the appearance of the node."""
     def __init__(self, slots=None):
         observer.Observed.__init__(self)
-        self._metas = {}
+        self._metaValues = {}
+        self._metaTypes = {}
 
         if(slots):
             if( isinstance(slots, self.__class__) ):
-                self._metas = slots._metas.copy()
+                self._metaValues = slots._metaValues.copy()
+                self._metaTypes = slots._metaTypes.copy()
             else:
-                for name, v in slots.iteritems():
-                    _type, _value = v
-                    self.add_metadata(name, _type )
-                    self.set_metadata(name, _value)
+                for name, value in slots.iteritems():
+                    if (isinstance(value, tuple) or isinstance(value, list)) and \
+                       len(value) == 2  and isinstance(value[0],type):
+
+                        typ, val = value
+                    else:
+                        typ =  type(value) if value is not None else None
+                        val = value
+                    self.add_metadata(name, typ )
+                    self.set_metadata(name, val)
 
     def __repr__(self):
-        if(not len(self._metas)): return "{}"
-        stri = "{"
-        for key, val in self._metas.iteritems():
-            stri = stri+ "\"" + key + "\" : [" + val[0].__name__ + ", " + repr(val[1]) + "],"
-        stri = stri[:-1] + "}"
-        return stri
+        if(not len(self._metaValues)): return "{}"
+        keys = set(self._metaTypes)-set(self._metaValues)
+        d = self._metaValues.copy()
+        for k in keys:
+            d[k] = None
+        return repr(d)
+#         stri = "{"
+#         for key, val in self._metaValues.iteritems():
+#             stri = stri+ "\"" + key + "\" : [" + val[0].__name__ + ", " + repr(val[1]) + "],"
+#         stri = stri[:-1] + "}"
+#         return stri
 
     def __len__(self):
-        return len(self._metas)
+        return len(self._metaTypes)
 
     def add_metadata(self, key, valType, notify=True):
         """Creates a new entry in the meta data registry.
         The data to set will be of the given 'type' type."""
-        assert type(valType) == types.TypeType
 
-        if key in self._metas :
+        if key == 'userColor':
+            print key, valType
+        if key in self._metaTypes :
             raise Exception("This key already exists : " + key)
 
-        self._metas[key] = [valType, None]
+        self._metaTypes[key] = valType
         if(notify):
             self.notify_listeners(("metadata_added", key, valType))
         return
 
     def set_metadata(self, key, value, notify=True):
         """Sets the value of a meta data."""
-        if( value == None ): return 
-        if key not in self._metas :
+        if value is None : return 
+        if key not in self._metaTypes :
             raise Exception("This key does not exist : " + key)
 
-        if( type(value) != self._metas[key][0] ) :
+        if key in self._metaTypes and type(value) != self._metaTypes[key] :
             print(self.__class__, "set_metadata : Unexpected value type", key, 
                   " : ", type(value),
                   " assuming duck-typing")
 
-        self._metas[key][1] = value
-        valType = self._metas[key][0]
+        self._metaValues[key] = value
+        valType = self._metaTypes[key]
         if(notify): 
             self.notify_listeners(("metadata_changed", key, value, valType))
         return
     
     def get_metadata(self, key):
         """Gets the value of a meta data."""
-        if key not in self._metas :
+        if key not in self._metaTypes :
             raise Exception("This key does not exist : " + key)
-        
-        return self._metas[key][1]
+        return  self._metaValues.get(key)
 
     def simulate_full_data_change(self):
-        for k in self._metas.keys():
-            valType = self._metas[k][0]
-            value = self._metas[k][1]
+        for k in self._metaValues.keys():
+            valType = self._metaTypes[k]
+            value = self._metaValues[k]
             self.notify_listeners(("metadata_changed", k, value, valType))
 
     def __str__(self):
