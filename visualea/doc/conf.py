16aeae1e9980af841aa29becfd0e6df220ecd5dd
Thomas Cokelaer <cokelaer@gmail.com>
2010-01-08 18:04:10 +0000

    fix conf.py in openalea
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@2077 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/doc/conf.py
+++ visualea/doc/conf.py
@@ -7,10 +7,10 @@ from openalea.deploy.metainfo import read_metainfo
 
 sphinx_check_version()                      # check that sphinx version is recent
 metadata = read_metainfo('../metainfo.ini') # read metainfo from common file with setup.py
-for key in ['version','project','release','authors']:
+for key in ['version','project','release','authors', 'name']:
     exec("%s = '%s'" % (key, metadata[key]))
 
 # by product that need to be updated:
 latex_documents = [('contents', 'main.tex', project + ' documentation', authors, 'manual')]
 
-
+project = name
