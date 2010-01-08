5fc6a70bbfbf49caedd30a09d9eaffe46d96452f
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-01-08 15:00:29 +0000

    sphinx refactoring
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2070 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ starter/doc/user/index.rst
@@ -0,0 +1,24 @@
+.. _core_user:
+
+Core User Guide
+###############
+
+:Version: |version|
+:Release: |release|
+:Date: |today|
+
+This manual details functions, modules, and objects included in 
+OpenAlea.Starter, describing what they are and what they do. For a complete 
+reference guide, see :ref:`starter_reference`.
+
+.. warning::
+
+   This "User Guide" is still very much in progress. 
+   Many aspects of OpenAlea.Starter are not covered.
+
+   More documentation can be found on the
+   `openalea <http://openalea.gforge.inria.fr>`__ wiki.
+
+.. toctree::
+
+    manual.rst
--- starter/setup.py
+++ starter/setup.py
@@ -1,26 +1,43 @@
 # This Setup script has been commented to ease the writing of your own file. 
 
-# A setup script mainly consist of a call to the setup function of setuptool, that allows to create a distribution archive of a set of python modules grouped in packages (ie in directories with an __init__.py file).
-# In the context of OpenAlea, this function has been extended by the openalea.deploy module to ease the simultaneaous distribution of binaries and libraries.
+# A setup script mainly consist of a call to the setup function of setuptool, 
+# that allows to create a distribution archive of a set of python modules 
+# grouped in packages (i.e. in directories with an __init__.py file).
+
+# In the context of OpenAlea, this function has been extended by the 
+# openalea.deploy module to ease the simultaneaous distribution of binaries
+# and libraries.
 
 
-
-# (To adapt this script for your package, you mainly have to change the content of the variable defined before the call to setup function, and comment out unused options in the call of the function)
+# In order adapt this script for your package, you have to change the content 
+# of metainfo.ini file that is read in the setup.py and to adapt the variables
+# defined before the call to setup function, and comment out unused options 
+# in the call of the function
 
 import sys
 import os
 
 from setuptools import setup, find_packages
 
+# Reads the metainfo file
+from openalea.deploy.metainfo import read_metainfo
+metadata = read_metainfo('metainfo.ini', verbose=True)
+for key,value in zip(metadata.keys(), metadata.values()):
+    exec("%s = '%s'" % (key, value))
 
-# Name and version of for your 'distribution archive'
+#The metainfo files must contains
+# version, release, project, name, namespace, pkg_name, description, long_description
+# authors, authors_email, url and license
+# * note that authors and authors_email have an s even though there is only one author
+# * version is 0.7.0 and release 0.7
+# * project must be in [openalea, vplants, alinea]
+# * name is the full name (e.g., OpenAlea.Starter) whereas pkg_name is only 'starter'
 
-# (This will determine the name of the egg, as well as the name of the pakage directory under Python/lib/site-packages)
-# (This name is also the one to use in setup script of other packages to declare a dependency to this package)
+# name will determine the name of the egg, as well as the name of 
+# the pakage directory under Python/lib/site-packages). It is also 
+# the one to use in setup script of other packages to declare a dependency to this package)
 # (The version number is used by deploy to detect UPDATES)
 
-name = 'OpenAlea.Starter'
-version= '0.7.0' 
 
 # Packages list, namespace and root directory of packages
 
@@ -31,7 +48,6 @@ version= '0.7.0'
 # (if you want more control on what to put in your distribution, you can manually edit the' packages' list 
 # (the 'package_dir' dictionary must content the pkg_rootdir and all top-level pakages under it)
 
-namespace = 'openalea'
 pkg_root_dir = 'src'
 pkgs = [ pkg for pkg in find_packages(pkg_root_dir) if namespace not in pkg]
 top_pkgs = [pkg for pkg in pkgs if  len(pkg.split('.')) < 2]
@@ -41,22 +57,6 @@ package_dir = dict( [('',pkg_root_dir)] + [(namespace + "." + pkg, pkg_root_dir 
 # List of top level wralea packages (directories with __wralea__.py) 
 # (to be kept only if you have visual components)
 #wralea_entry_points = ['%s = %s'%(pkg,namespace + '.' + pkg) for pkg in top_pkgs]
-
-# Meta information
-# (used to construct egg infos)
-description= 'Starter package for OpenAlea.' 
-long_description= '''
-The Starter package is a typical package example to help developper
-to create their own package, compatible with OpenAlea standards.
-It defines best practices for build, installation, coding rules,
-and license information.
-It contains code in C++, Python, as well as Boost.Python wrappers.
-The package can be installed on various platform.
-'''
-author= 'Samuel Dufour-Kowalski, Christophe Pradal'
-author_email= 'samuel.dufour@sophia.inria.fr, christophe.pradal@cirad.fr'
-url= 'http://openalea.gforge.inria.fr'
-license= 'Cecill-C' 
 
 # dependencies to other eggs
 # (This is used by deploy to automatically downloads eggs during the installation of your package)
@@ -87,8 +87,8 @@ setup(
     version=version,
     description=description,
     long_description=long_description,
-    author=author,
-    author_email=author_email,
+    author=authors,
+    author_email=authors_email,
     url=url,
     license=license,
     keywords = '',	
