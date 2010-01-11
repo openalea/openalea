4d9f6f9e799efe287cfbd38a4a2bb7e70e47b704
Thomas Cokelaer <cokelaer@gmail.com>
2010-01-11 19:16:51 +0000

    update metainfoa nd conf.py files
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@2096 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/doc/conf.py
+++ visualea/doc/conf.py
@@ -3,14 +3,14 @@ import os,sys
 # read sphinx conf.py file
 from openalea.misc.sphinx_configuration import *
 from openalea.misc.sphinx_tools import sphinx_check_version
-from openalea.deploy.metainfo import read_metainfo
+from openalea.deploy.metainfo import read_metainfo, compulsary_words
 
 sphinx_check_version()                      # check that sphinx version is recent
 metadata = read_metainfo('../metainfo.ini') # read metainfo from common file with setup.py
-for key in ['version','project','release','authors', 'name']:
+for key in compulsary_words:
     exec("%s = '%s'" % (key, metadata[key]))
 
 # by product that need to be updated:
 latex_documents = [('contents', 'main.tex', project + ' documentation', authors, 'manual')]
 
-project = name
+project = project + '.' + package
