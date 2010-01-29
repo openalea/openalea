90dad5aff9e54da235748c735e02e754226bdd40
dbarbeau <dbarbeau@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-01-29 15:41:59 +0000

    Indentation fixes, matplotlib dependency fix for windows.
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/release_0_8@2194 04ea39c6-a500-0410-8508-e77fe2a861cf

--- deploy/example/build_zip.py
+++ deploy/example/build_zip.py
@@ -16,8 +16,8 @@ def qt4(directory = path('qt4'), name='qt4'):
     d = path('.')
     for pattern in patterns:
         for fn in d.walkfiles(pattern):
-	    print 'add ', fn
-	    zf.write(fn)
+	      print 'add ', fn
+	      zf.write(fn)
 
     zf.close()
     os.chdir(curdir)
@@ -41,8 +41,8 @@ def qt4_dev (directory=path('qt4-dev'), name='qt4_dev'):
         d = path(dir)
 	for pattern in patterns:
             for fn in d.walkfiles(pattern):
-	        print 'add ', fn
-	        zf.write(fn)
+                print 'add ', fn
+                zf.write(fn)
 
     zf.close()
     os.chdir(curdir)
