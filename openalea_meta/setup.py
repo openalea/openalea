d53153f8f3db95f40b75c87f7f082f844cd93cc2
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-02-08 17:27:50 +0000

    merged release 0.8.0 changes r:2101:2241 into the trunk (openalea_meta package)
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2250 04ea39c6-a500-0410-8508-e77fe2a861cf

--- openalea_meta/setup.py
+++ openalea_meta/setup.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+__revision__ = "$Id$"
+
 import os, sys
 pj = os.path.join
 
@@ -5,31 +8,35 @@ from setuptools import setup, find_packages
 
 from openalea.deploy.metainfo import read_metainfo
 metadata = read_metainfo('metainfo.ini', verbose=True)
-for key,value in zip(metadata.keys(), metadata.values()):
+for key,value in metadata.iteritems():
     exec("%s = '%s'" % (key, value))
 
 
 platform = sys.platform
-external_dependencies = [
-'numpy',
-'scipy',
-'matplotlib>=0.99',
-]
+if platform.startswith('win'):
+    external_dependencies = ['matplotlib<=0.99.1',
+                             'scipy>=0.7.1',
+                             'numpy>=1.4.0']
+else:
+    external_dependencies = ['matplotlib',
+                             'scipy',
+                             'numpy']
 
 if platform != 'darwin':
     external_dependencies.append('PIL<=1.1.6')
 
 alea_dependencies = [
-'openalea.deploy >= 0.7.0.dev',
-'openalea.deploygui >= 0.7.0.dev',
-'openalea.core >= 0.7.0.dev',
-'openalea.visualea >= 0.7.0.dev',
-'openalea.stdlib >= 0.7.0.dev',
-'openalea.sconsx >=0.7.0.dev',
-'openalea.misc >=0.7.0.dev',
-'openalea.scheduler >=0.7.0.dev',
-#'openalea.container >=2.0.1.dev',
-#'openalea.mtg >=0.7.0.dev',
+'openalea.core >= 0.8.0.dev',
+'openalea.deploy >= 0.8.0.dev',
+'openalea.deploygui >= 0.8.0.dev',
+'openalea.grapheditor >=0.8.0.dev',
+'openalea.misc >=0.8.0.dev',
+'openalea.visualea >= 0.8.0.dev',
+'openalea.stdlib >= 0.8.0.dev',
+'openalea.sconsx >=0.8.0.dev',
+'openalea.scheduler >=0.8.0.dev',
+#'openalea.container >=2.0.1.dev', part of vplants 
+#'openalea.mtg >=0.7.0.dev', part of vplants
 ]
 
 install_requires = alea_dependencies
@@ -37,6 +44,8 @@ install_requires = alea_dependencies
 # Add dependencies on Windows and Mac OS X platforms
 if 'win' in platform:
     install_requires += external_dependencies 
+
+build_prefix = "build-scons"
 
 setup(
     name = name,
@@ -55,6 +64,8 @@ setup(
     packages=find_packages('src'),
 
     package_dir={"":"src" },
+
+    scons_scripts=['SConstruct'],
 
     # Add package platform libraries if any
     include_package_data=True,
