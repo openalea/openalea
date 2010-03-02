ecead04a4bc6b5a9dfb2ea9c4058fb8351a9d6b5
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-03-02 12:09:41 +0000

    add transformations into __init__
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2346 04ea39c6-a500-0410-8508-e77fe2a861cf

--- stdlib/src/openalea/image/__init__.py
+++ stdlib/src/openalea/image/__init__.py
@@ -2,5 +2,6 @@ try:
     from data_image import from_image
     from data_algo import *
     from image_morpho import *
+    import transformations
 except ImportError:
     print "Please install PIL to use Image module"
---
+++ stdlib/src/openalea/image/transformations/__init__.py
@@ -0,0 +1,5 @@
+try:
+    import image_transfo
+except ImportError:
+    print "could not import image_transfor"
+
