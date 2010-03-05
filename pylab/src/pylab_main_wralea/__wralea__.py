c6543c9f9e9b453ab9ad25c7f614f5b705bd4c93
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-03-05 13:52:40 +0000

    add directories for wralea fiels
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2352 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ pylab/src/pylab_main_wralea/__wralea__.py
@@ -0,0 +1,22 @@
+
+# This file has been generated at Fri Mar  5 11:20:24 2010
+
+from openalea.core import *
+
+
+__name__ = 'openalea.pylab'
+
+__editable__ = True
+__description__ = 'pylab nodes.'
+__license__ = 'CECILL-C'
+__url__ = 'http://openalea.gforge.inria.fr/doc/openalea/pylab/doc/_build/html/contents.html'
+__alias__ = []
+__version__ = '0.8.0'
+__authors__ = 'Thomas Cokelaer'
+__institutes__ = 'INRIA/CIRAD'
+__icon__ = 'icon.png'
+
+
+
+
+
