8aef30433c120c49966f7770bd87b0279c82cefe
moscardi <moscardi@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-03-29 12:54:27 +0000

    add packages for numpy
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2384 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ numpy/src/numpy_test_wralea/test_math_wralea/__wralea__.py
@@ -0,0 +1,15 @@
+from openalea.core import *
+
+
+__name__ = 'openalea.numpy.test.math'
+
+__editable__ = True
+__description__ = 'numpy test nodes.'
+__license__ = 'CECILL-C'
+__url__ = 'http://openalea.gforge.inria.fr/doc/openalea/numpy/doc/_build/html/contents.html'
+__alias__ = []
+__version__ = '0.8.0'
+__authors__ = 'Eric MOSCARDI'
+__institutes__ = 'INRIA/CIRAD'
+__icon__ = 'icon.png'
+
