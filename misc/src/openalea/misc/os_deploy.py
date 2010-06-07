c1e918e4e7b4e7c1c6b8b7201af9cee098803b25
dbarbeau <dbarbeau@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-06-07 15:25:46 +0000

    merging with dependency modules with parallel development that happened accidentally in vplants
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2496 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ misc/src/openalea/misc/os_deploy.py
@@ -0,0 +1,30 @@
+# -*- python -*-
+#
+#       VirtualPlant's buildbot continuous integration scripts.
+#
+#       Copyright 2006-2009 INRIA - CIRAD - INRA
+#
+#       File author(s): Daniel Barbeau <daniel.barbeau@sophia.inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+###############################################################################
+
+from os_factory import Factory
+import dependency
+
+
+def deploy_runtime_dependencies(software, osname):
+    theOs = Factory().create(osname)
+    dependencies = dependency.Dependency(software, osname)
+    theOs.distributionInstallation(dependencies.runtime_distribution_packages())
+
+def deploy_development_dependencies(software, osname):
+    theOs = Factory().create(osname)
+    dependencies = dependency.Dependency(software, osname)
+    theOs.distributionInstallation(dependencies.development_distribution_packages())
+
+
+
