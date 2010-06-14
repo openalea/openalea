3bd190d2d1efc3c24c0f082dc6b49332575b18d3
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-06-14 14:50:56 +0000

    Fix wrong datafile info. PyQGLViewer has to be in lib with libQGLViewer.so
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2517 04ea39c6-a500-0410-8508-e77fe2a861cf

--- deploy/example/setup_pyqglviewer.py
+++ deploy/example/setup_pyqglviewer.py
@@ -19,7 +19,7 @@ long_description= 'This egg was created from the source file. libQGLViewer was p
 
 license= 'GPL' 
 
-data_files = 'PyQGLViewer.pyd' if distutils.util.get_platform() == "win32" else 'PyQGLViewer.so'
+#data_files = 'PyQGLViewer.pyd' if distutils.util.get_platform() == "win32" else 'PyQGLViewer.so'
 
 # Main setup
 setup(
@@ -35,7 +35,7 @@ setup(
     package_data = {'sip':['*.sip'], 'examples':['*.py']},
     lib_dirs = { 'lib' : 'lib'  },
     inc_dirs = { 'include' : 'include' },
-    data_files = [('',[data_files])],
+    #data_files = [('',[data_files])],
 
     zip_safe = False,
     setup_requires = ['openalea.deploy'],
