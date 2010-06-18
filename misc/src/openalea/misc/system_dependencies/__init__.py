840e083807124b97442f5e1c8dc8197ad8faabee
dbarbeau <dbarbeau@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-06-18 12:03:55 +0000

    refactoring system_dependencies
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2535 04ea39c6-a500-0410-8508-e77fe2a861cf

--- misc/src/openalea/misc/system_dependencies/__init__.py
+++ misc/src/openalea/misc/system_dependencies/__init__.py
@@ -1,4 +1,2 @@
-from os_deploy import *
-from os_factory import *
 from dependency import *
-from dependencies import *
+
--- misc/src/openalea/misc/system_dependencies/dependency.py
+++ misc/src/openalea/misc/system_dependencies/dependency.py
@@ -12,14 +12,172 @@
 #
 ###############################################################################
 
-import platform, types, warnings, collections
-import dependencies, os_factory
+import platform, types, warnings, collections, subprocess, abc
 
 
-__all__ = ["Dependency", "EggDependency", "InstallerDependency"]
+__all__ =  ["get_platform"]
+__all__ += ["DependencySolver", "BaseDependency", "EggDependency", "InstallerDependency"]
+__all__ += ["deploy_runtime_dependencies", "deploy_development_dependencies"]
 
 
-class DistributionPackageFactory(os_factory.BaseOsFactory):
+
+# --------------------
+# -- High level api --
+# --------------------
+def deploy_runtime_dependencies(software, osname, fake):
+    theOs = OsInterfaceFactory().create(osname)
+    dependencies = DependencySolver(software, osname)
+    theOs.install_packages(dependencies.runtime_distribution_packages(), fake)
+
+
+def deploy_development_dependencies(software, osname, fake):
+    theOs = OsInterfaceFactory().create(osname)
+    dependencies = DependencySolver(software, osname)
+    theOs.install_packages(dependencies.development_distribution_packages(), fake)
+
+def get_platform():
+    return BaseOsFactory.get_platform()
+
+# --------------------------------
+# -- Operating system Factories --
+# --------------------------------
+class MSingleton(type):
+    """Metaclass that makes the class that uses it a singleton"""
+    instances = {}
+    def __call__(cls, *args, **kwargs):
+        return MSingleton.instances.setdefault(cls, type.__call__(cls, *args, **kwargs))
+
+
+
+class BaseOsFactory(object):
+    """Base class for foactories that create objects depending on the operating system"""
+    __metaclass__ = MSingleton
+
+    @classmethod
+    def get_platform(cls):
+        """Creates a string out of the current platform with names seperated by whitespaces:
+        ex: "fedora 13 goddard" or "ubuntu 9.10 karmic"."""
+
+        _platform = None
+        system = platform.system().lower()
+        if system == "linux":
+            dist, number, name = platform.linux_distribution()
+            _platform = dist.lower() + " " + number.lower() + " " + name.lower()
+        elif system == "windows":
+            dist, host, name, number, proc, procinfo = platform.uname()
+            _platform = dist.lower() + " " + number.lower() + " " + name.lower() + " "+ proc.lower()
+        else:
+            warnings.warn("Currently unhandled system : " + system + ". Implement me please.")
+        return _platform
+
+
+    @classmethod
+    def intersect_platform_names(cls, requestedPlatformNames, availablePlatformNames):
+       """Find intersection between platformName and packageList subdictionnary keys.
+       ex : ["fedora", "13", "goddard"] and ["fedora", "ubuntu"]
+       returns (["fedora"]), [("13", "goddard"])"""
+
+       requestedPlatformNames = set(requestedPlatformNames)
+       availablePlatformNames = set(availablePlatformNames)
+       return requestedPlatformNames&availablePlatformNames, requestedPlatformNames-availablePlatformNames
+
+
+    @classmethod
+    def intersect_and_solve(cls, platform, candidates, conflictSolve=lambda x: x[0]):
+        """ Given a platform name from BaseOsFactory.get_platform() or similar, and
+        candidate names (ex: ["fedora 13 goddard", "ubuntu 9.10 karmic"]), this method
+        will return the largest intersection of both lists and use conflictSolve to
+        resolve conflicts when there are several intersections with the same number
+        of items."""
+
+        assert platform is not None
+        #We find the right distribution class by intersecting
+        #the platform description with the X_X_PackageNames classes
+        #whose names are mangled.
+        #the correct disctribution class is the one with which the
+        #intersection is the largest. If there's equality between two
+        #the conflict is solved using the conflictSolve function.
+        platform = platform.split(" ")
+        maxIntersectionAmount  = 0
+        maxIntersectionDistrib = None
+        for dist in candidates:
+            intersections = cls.intersect_platform_names(platform, dist.split(" "))[0]
+            numInters = len(intersections)
+            if numInters > maxIntersectionAmount:
+                maxIntersectionAmount = numInters
+                maxIntersectionDistrib = dist
+            elif numInters == maxIntersectionAmount and numInters > 0:
+                if isinstance(maxIntersectionDistrib, list):
+                    maxIntersectionDistrib.append(dist)
+                elif maxIntersectionDistrib is not None:
+                    maxIntersectionDistrib = [maxIntersectionDistrib, dist]
+                else:
+                    maxIntersectionDistrib = dist
+        if isinstance(maxIntersectionDistrib, list):
+            maxIntersectionDistrib = conflictSolve(maxIntersectionDistrib)
+        return maxIntersectionDistrib
+
+
+
+class OsInterfaceFactory(BaseOsFactory):
+    """ Factory class that manages OsInterfaces. """
+    def __init__(self):
+        self.__oses = {}
+
+    def create(self, osname):
+        osname = self.intersect_and_solve(osname, self.get_platforms())
+        return self.__oses.get(osname, None)
+
+    def register(self, osname, osinterface):
+        self.__oses[osname] = osinterface
+
+    def get_platforms(self):
+        return self.__oses.keys()
+
+
+
+class OsInterface(object):
+    """ Class whose instances define various system operations
+    such as svn, yum/apt/..., etc..."""
+    def __init__(self, distIntallerCmd, svnCommand):
+        self.__distInstaller = distIntallerCmd
+        self.__svnCommand = svnCommand
+
+    def install_packages(self, packages, fake, batch=True):
+        """ Install a list of packages that are instances of
+        BaseDependency or subclasses. If batch is true
+        will try to issue one single system installer command for
+        all packages, instead of one per package. """
+        if not issubclass(packages.__class__, collections.Iterable):
+            packages = [packages]
+        if batch:
+            pkgnames = ""
+            for p in packages:
+                if p.is_base():
+                    pkgnames += p.distrib_name() + " "
+            self.package_install(pkgnames, fake)
+        else:
+            for p in packages:
+                p.install(self, fake)
+
+    def package_install(self, package, fake):
+        """ Install package using the system's installer """
+        command = self.__distInstaller[:]
+        command += " "+ package
+
+        if fake:
+            print command
+        else:
+            subprocess.Popen(command.split(" "))
+
+
+
+# ---------------------------
+# -- Dependency management --
+# ---------------------------
+class DistributionPackageFactory(BaseOsFactory):
+    """ A factory that creates DistributionPackageNames
+    properly according to the platform """
     def __init__(self):
         self.__distPkgs = {}
 
@@ -38,7 +196,6 @@ class DistributionPackageFactory(os_factory.BaseOsFactory):
 
 
 
-
 # -- Decanonification and handling special package types like eggs --
 class DistributionPackageNames(dict):
     """Base class for per-OS decanonification"""
@@ -50,6 +207,8 @@ class DistributionPackageNames(dict):
         other = dict(zip(ks,map(lambda x: BaseDependency(x) if isinstance(x,str) else x,
                                 vs)))
         dict.update(self, other)
+
+
 
 class BaseDependency(object):
     def __init__(self, name):
@@ -64,6 +223,8 @@ class BaseDependency(object):
     def distrib_name(self):
         return self.name
 
+
+
 class EggDependency(BaseDependency):
     def __init__(self, name):
         BaseDependency.__init__(self, name)
@@ -72,6 +233,8 @@ class EggDependency(BaseDependency):
     def is_base(self):return False
     def distrib_name(self):
         return ""
+
+
 
 class InstallerDependency(BaseDependency):
     def __init__(self, name, path):
@@ -87,9 +250,13 @@ class InstallerDependency(BaseDependency):
 
 
 
+class DependencySolver(object):
+    __dep_tree__ = {}
 
+    @classmethod
+    def set_dependency_tree(cls, tree):
+        cls.__dep_tree__ = tree
 
-class Dependency(object):
     def __init__(self, package, _platform=False):
         self.__canonical_deps = []
         self.__distribution_deps = []
@@ -118,7 +285,7 @@ class Dependency(object):
     ############################################################
     def __solve_dependencies(self, package, _platform=False):
         assert _platform is not None
-        package_deps = dependencies.get_canonincal_dependencies().get(package, None)
+        package_deps = self.__dep_tree__.get(package, None)
         if not package_deps:
             raise Exception("No such package : " + package)
 
@@ -140,7 +307,7 @@ class Dependency(object):
                 ancestors.append(currentPkg)
                 childs.append(currentPkgChilds)
                 currentPkg = child
-                currentPkgChildsList = dependencies.get_canonincal_dependencies().get(currentPkg, None)
+                currentPkgChildsList = self.__dep_tree__.get(currentPkg, None)
                 if currentPkgChildsList:
                     currentPkgChilds = currentPkgChildsList.__iter__()
                 else:
@@ -175,11 +342,11 @@ class Dependency(object):
 
 # -- shortcuts for Openalea, VPlants and Alinea --
 def Openalea(_platform=False):
-    return Dependency("openalea", _platform)
+    return DependencySolver("openalea", _platform)
 
 def VPlants(_platform=False):
-    return Dependency("vplants", _platform)
+    return DependencySolver("vplants", _platform)
 
 def Alinea(_platform=False):
-    return Dependency("alinea", _platform)
+    return DependencySolver("alinea", _platform)
 
