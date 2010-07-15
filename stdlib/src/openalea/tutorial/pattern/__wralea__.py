fad164e103814bd0be1fb20a773ef05b41a21db1
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-07-15 12:28:53 +0000

    add a new directory to demonstrate design patterns in OpenAlea
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2614 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ stdlib/src/openalea/tutorial/pattern/__wralea__.py
@@ -0,0 +1,22 @@
+
+# This file has been generated at Tue Jul 13 16:00:18 2010
+
+from openalea.core import *
+
+
+__name__ = 'openalea.tutorial.design pattern'
+
+__editable__ = True
+__description__ = ''
+__license__ = ''
+__url__ = ''
+__alias__ = []
+__version__ = ''
+__authors__ = ''
+__institutes__ = ''
+__icon__ = ''
+
+
+__all__ = []
+
+
