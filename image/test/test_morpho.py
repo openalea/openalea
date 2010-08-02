372fc17aa91a029df3012199aa466238297e7a38
moscardi <moscardi@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-08-02 12:45:53 +0000

    add tests for image.morpho
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2678 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ image/test/test_morpho.py
@@ -0,0 +1,87 @@
+# -*- python -*-
+#
+#       image: image morpho
+#
+#       Copyright 2006 INRIA - CIRAD - INRA  
+#
+#       File author(s): Eric Moscardi <eric.moscardi@sophia.inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from openalea.image import component_labeling
+
+def test_component_labeling_with_threshold_image():
+    """
+    Test component_labeling : 
+
+    :Parameters : 
+    - `threshold image`
+    """
+    img = np.load('labels.npy')
+    assert img.shape == (1024, 1344)
+
+    thresh_im = np.where(img < 127, False, True)
+    out,nlabels = component_labeling(thresh_im)    
+    assert nlabels == 18
+
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(img, cmap=plt.cm.gray)
+    plt.title('reference image')
+    plt.subplot(122)
+    plt.imshow(out, cmap=plt.cm.gray)
+    plt.title('labeled image')
+    plt.show()
+
+def test_component_labeling_with_threshold():
+    """
+    Test component_labeling : 
+
+    :Parameters : 
+    - `threshold`
+    """
+
+    img = np.load('labels.npy')
+    assert img.shape == (1024, 1344)
+
+    out,nlabels = component_labeling(img, threshold=127)    
+    assert nlabels == 18
+
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(img, cmap=plt.cm.gray)
+    plt.title('reference image')
+    plt.subplot(122)
+    plt.imshow(out, cmap=plt.cm.gray)
+    plt.title('labeled image')
+    plt.show()
+
+def test_component_labeling_with_number_labels():
+    """
+    Test component_labeling : 
+
+    :Parameters : 
+    - `number_labels`
+    """
+    img = np.load('labels.npy')
+    assert img.shape == (1024, 1344)
+
+    out,nlabels = component_labeling(img, threshold=127, number_labels=3)    
+    assert nlabels == 3
+
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(img, cmap=plt.cm.gray)
+    plt.title('reference image')
+    plt.subplot(122)
+    plt.imshow(out, cmap=plt.cm.gray)
+    plt.title('labeled image')
+    plt.show()
