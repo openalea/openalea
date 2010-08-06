7887d601dcf7a3dac0b4bd114cf10cb67af7f428
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-08-06 09:42:43 +0000

    add add_doctring decorator to ease the automatic import of existing docstring into wralea functions
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2701 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/external.py
+++ core/src/core/external.py
@@ -8,3 +8,34 @@ from openalea.core.node import Factory, Node, NodeFactory, Alias
 from openalea.core.data import DataFactory
 from openalea.core.compositenode import CompositeNode, CompositeNodeFactory
 from openalea.core.interface import *
+
+
+
+def add_docstring(obj):
+    """Decorator that replace a function's docstring by another from `obj`
+
+    This function should be used to decorate your wralea functions when docstring
+    is equivalent to an already existing function or class.
+
+    :param obj: a function or class that contains a docstring
+
+    :Example:
+
+    ::
+
+        from math import abs
+
+        @add_docstring(abs)
+        def int_abs_function(x):
+            if type(x) == int:
+                return abs(x)
+            else:
+                raise TypeError("expect int as parameter")
+
+
+    """
+    def wrap(f):
+        f.__doc__ = obj.__doc__
+        return f
+    return wrap
+
