2deb877597c244f5f2f8b0214900770c1730188d
Daniel Barbeau <daniel.barbeau@sophia.inria.fr>
2010-08-10 11:12:47 +0000

    some more cleanups
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@2725 04ea39c6-a500-0410-8508-e77fe2a861cf

--- grapheditor/src/grapheditor/qtutils.py
+++ grapheditor/src/grapheditor/qtutils.py
@@ -90,6 +90,122 @@ class AleaSignal(object):
             c(*args)
 
 ###############################################
+# A QGraphicsWidget that looks like a post it #
+###############################################
+class MemoRects(QtGui.QGraphicsRectItem):
+    __handleSize    = 7.5
+    __defaultColor  = QtGui.QColor(250, 250, 100)
+
+    def __init__(self, rect, parent=None):
+        QtGui.QGraphicsRectItem.__init__(self, rect, parent)
+        self.__resizing   = False
+        self.__handlePoly = QtGui.QPolygonF([QtCore.QPointF(0, -self.__handleSize),
+                                             QtCore.QPointF(0, 0),
+                                             QtCore.QPointF(-self.__handleSize,0)])
+        self.setFlag(QtGui.QGraphicsItem.ItemStacksBehindParent)
+        self.setFlag(QtGui.QGraphicsItem.ItemStacksBehindParent)
+        # -- handle --
+        self.__handlePos  = QtCore.QPointF(0,0)
+        # -- header --
+        self.__headerContentRect = None
+        self.__headerRect = None
+        # -- color --
+        self.__color       = None
+        self.__darkerColor = None
+        self.__shadowColor = None
+        self.setColor(self.__defaultColor.darker(110))
+        # -- optionnal cosmetics --
+        if safeEffects:
+            fx = QtGui.QGraphicsDropShadowEffect()
+            fx.setOffset(2,2)
+            fx.setBlurRadius(5)
+            self.setGraphicsEffect(fx)
+
+    def setColor(self, color):
+        self.__color       = color
+        self.__darkerColor = color.darker(140)
+        self.__shadowColor = color.darker(200)
+        self.update()
+
+    def __moveHandleBottomRightTo(self, point):
+        delta = point - self.__handlePos
+        self.__handlePoly.translate(delta.x(), delta.y())
+        self.__handlePos = point
+
+    def setHeaderRect(self, rect):
+        myRect = self.boundingRect()
+        self.__headerContentRect = rect.adjusted(0,0,0,0)
+
+        rect.setX(0); rect.setY(0)
+        myRect.setX(0); myRect.setY(0)
+
+        if rect.bottom() >= (myRect.bottom()-self.__handleSize):
+            myRect.setBottom(rect.bottom() + self.__handleSize)
+        if rect.right() >= myRect.right():
+            myRect.setRight(rect.right())
+        else:
+            rect.setRight(myRect.right())
+        self.__headerRect = rect
+        self.__moveHandleBottomRightTo(myRect.bottomRight())
+        self.setRect(myRect)
+        self.update()
+
+    def mousePressEvent(self, event):
+        pos         = event.pos()
+        bottomRight = self.boundingRect().bottomRight()
+        x, y        = bottomRight.x(), bottomRight.y()
+        rect = QtCore.QRectF(x-5, y-5, x+5, y+5)
+        if self.__handlePoly.containsPoint(pos, QtCore.Qt.OddEvenFill):
+            self.__resizing = True
+        else:
+            QtGui.QGraphicsRectItem.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event):
+        if self.__resizing:
+            delta = event.pos() - event.lastPos()
+            bottomRight = self.boundingRect().bottomRight() + delta
+            newRect = QtCore.QRectF(0.,0., bottomRight.x(), bottomRight.y())
+            if newRect.contains(self.__headerContentRect.adjusted(0,0,0,self.__handleSize)):
+                self.setRect(newRect)
+                self.__headerRect = QtCore.QRectF(0.,0., bottomRight.x(), self.__headerContentRect.height())
+                self.__moveHandleBottomRightTo(newRect.bottomRight())
+        else:
+            QtGui.QGraphicsRectItem.mouseMoveEvent(self, event)
+
+    def mouseReleaseEvent(self, event):
+        if self.__resizing:
+            self.__resizing = False
+        else:
+            QtGui.QGraphicsRectItem.mouseReleaseEvent(self, event)
+
+    def paint(self, painter, paintOptions, widget):
+        myRect = self.boundingRect()
+
+        painter.fillRect(self.__headerRect, self.__darkerColor)
+        gradTop = self.__headerRect.bottomLeft()
+        gradBot = gradTop + QtCore.QPointF(0,4)
+        gradient = QtGui.QLinearGradient(gradTop,gradBot)
+        gradient.setColorAt(0, self.__shadowColor)
+        gradient.setColorAt(1, self.__color)
+        brush = QtGui.QBrush(gradient)
+
+        bottomRect = myRect.adjusted(0,self.__headerRect.bottom(),0,0)
+        painter.fillRect(bottomRect, brush)
+
+        if not safeEffects:
+            oldPen = painter.pen()
+            pen = QtGui.QPen()
+            pen.setColor(QtGui.QColor(10,10,10,100))
+            pen.setWidth(1)
+            painter.setPen(pen)
+            painter.drawRect(myRect.adjusted(0.5,0.5,-0.5,-0.5))
+            painter.setPen(oldPen)
+
+        painter.setBrush(QtGui.QBrush(self.__darkerColor))
+        painter.drawConvexPolygon(self.__handlePoly)
+
+
+###############################################
 # A Vanishing GraphicsItem mixing. Appears on #
 # hover in and vanishes on hover out          #
 ###############################################
