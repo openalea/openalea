3db0ff239a1c8212499cf1faaa59de281e0d2812
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-08-27 20:52:23 +0000

    update get_shared_data to accept optional path
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@2803 04ea39c6-a500-0410-8508-e77fe2a861cf

--- deploy/src/openalea/deploy/shared_data.py
+++ deploy/src/openalea/deploy/shared_data.py
@@ -16,16 +16,17 @@
 """INRIA GForge SOAP python API wrappers (based on SOAPpy)
 
 """
+from os.path import join as pj
 
 __license__ = "Cecill-C"
 __revision__ = " $Id: gforge.py 2243 2010-02-08 17:08:47Z cokelaer $ "
 
-def get_shared_data_path(path, filename=None):
+def get_shared_data_path(package_path, filename=None, share_path=pj('share','data')):
     """Return a valid pathname pointing to shared-data directory
 
     In a package, the shared data are expected to be found in `./share/data`.
 
-    :param path: a list of paths as provided within an __init__ file
+    :param package_path: a list of paths as provided within an __init__ file
         by the variable __path__
     :param filename: an optional valid filename without any path that is expected
         to be found in the directory __path__[0]/share/data
@@ -38,23 +39,26 @@ def get_shared_data_path(path, filename=None):
 
        >>> get_shared_data_path(['/home/user/mypackage'])
        '/home/user/mypackage/share/data'
+       >>> get_shared_data_path(['/home/user/mypackage'], mypath='share/databases')
+       '/home/user/mypackage/share/databases'
     """
     from os.path import join, realpath, isdir
     # in develop mode, we need to move in the tree directory one 
     # more time than in install mode
-    ff = join( path[0], '..', '..', 'share', 'data')
+
+    ff = pj( package_path[0], '..', '..', share_path)
     ff = realpath(ff)
     if isdir(ff) is False:
-        ff = join(path[0], '..', '..', '..', 'share', 'data')
-        ff = realpath(ff)
-        if isdir(ff) is False:
-            raise IOError('Could not find share/data directory make sure first argument is a valid path.')
+       ff = pj(package_path[0], '..', '..', '..', share_path)
+       ff = realpath(ff)
+       if isdir(ff) is False:
+           raise IOError('Could not find share/data directory make sure first argument is a valid path.')
 
     if filename==None:
         return ff
     else:
         from os.path import isfile
-        ff = join(ff, filename)
+        ff = pj(ff, filename)
         if isfile(ff):
             return realpath(ff)
         else:
