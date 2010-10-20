08a1e327f5e4d8a9670678fb19e41e5625f3800a
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-10-20 10:04:04 +0000

    add tests for wralea_test
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/release_0_9@2983 04ea39c6-a500-0410-8508-e77fe2a861cf

--- pylab/test/test_pylab_nodes.py
+++ pylab/test/test_pylab_nodes.py
@@ -3,10 +3,12 @@ from openalea.core.alea import *
 pm = PackageManager()
 pm.init(verbose=False)
 
+names = ['acorr', 'boxplot', 'cohere', 'contour', 'csd', 'errorbar', 'fill', 'hexbin', 'hist', 'loglog', 'pcolor', 'pie', 'plot', 'polar', 'psd', 'quiver', 'scatter', 'specgram', 'stem', 'step']
 
-def test_pylab_nodes():
-    res = run(('openalea.pylab.nodes', 'PyLabRandom'),{},pm=pm)
-    assert len(res[0]) == 100
+def test_all():
+    for i in range(0, len(names)):
+        yield check,names[i]
 
-res = test_pylab_nodes()
+def check(name):
+    res = run(('openalea.pylab.test', name),{},pm=pm)
 
