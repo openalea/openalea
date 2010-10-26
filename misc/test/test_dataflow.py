f85f32b8837efc796c1d65d9b10d6ddcf27e8ecf
cokelaer <cokelaer@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-10-26 15:57:11 +0000

    update doc remove most of sphinx_tools module content that is deprecated
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3002 04ea39c6-a500-0410-8508-e77fe2a861cf

--- misc/test/test_dataflow.py
+++ misc/test/test_dataflow.py
@@ -7,9 +7,9 @@ def test_shared_data_browser():
     res = run(('openalea.misc.test','shared_data_browser'),{},pm=pm)
     assert res == []
 
-if __name__ == "__main__":
-    test_demo_corsican()
-    test_demo_dycorinia()
-    test_stat_tool_tutorial_compound()
-    test_stat_tool_tutorial_convolution()
+#if __name__ == "__main__":
+#    test_demo_corsican()
+#    test_demo_dycorinia()
+#    test_stat_tool_tutorial_compound()
+#    test_stat_tool_tutorial_convolution()
 
