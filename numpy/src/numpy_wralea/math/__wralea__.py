c33a2b4ea7f338c7069ac3be7bd3357adb837ca7
dbarbeau <dbarbeau@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-10-29 10:00:54 +0000

    Fixes for numpy: openalea.numpy.numpy_wralea.math.math was conflicting with python's builtin math module.
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/release_0_9@3020 04ea39c6-a500-0410-8508-e77fe2a861cf

--- numpy/src/numpy_wralea/math/__wralea__.py
+++ numpy/src/numpy_wralea/math/__wralea__.py
@@ -52,7 +52,7 @@ std =  Factory(name = "std",
         dict(name='ddof', interface=IInt, value=0),
         ),
     outputs = None,
-    nodemodule="math",
+    nodemodule="numpy_math",
     nodeclass = "std",
     )
 __all__.append("std")
@@ -188,7 +188,7 @@ inv = Factory(name = "inv",
 		category = "numpy",
 		inputs = (dict(name='array', interface=ISequence),),
 		outputs = (dict(name='array', interface= ISequence),),
-                nodemodule = "math",
+                nodemodule = "numpy_math",
 		nodeclass = "inv",
 		)
 
@@ -606,7 +606,7 @@ mean = Factory(name = "mean",
         dict(name='dtype', interface=None, value=None),
         ),
     outputs = None,
-    nodemodule="math",
+    nodemodule="numpy",
     nodeclass = "mean",
     )
 __all__.append("mean")
@@ -620,7 +620,7 @@ sum = Factory(name = "sum",
         dict(name='axis', interface=IInt,  value=None),
         dict(name='dtype', interface=IEnumStr(list_type), value='float64')),
     outputs = None,
-    nodemodule="math",
+    nodemodule="numpy_math",
     nodeclass = "wra_sum",
     )
 __all__.append("sum")
@@ -633,7 +633,7 @@ min = Factory(name = "min",
         dict(name='array', interface=None),
         dict(name='axis', interface=IInt,  value=None),),
     outputs = None,
-    nodemodule="math",
+    nodemodule="numpy_math",
     nodeclass = "wra_min",
     )
 __all__.append("min")
@@ -645,7 +645,7 @@ max = Factory(name = "max",
                 inputs = (dict(name='array', interface=None),
                           dict(name='axis', interface=IInt, value=None),),
                 outputs = (dict(name='out', interface= None),),
-                nodemodule = "math",
+                nodemodule = "numpy_math",
                 nodeclass = "wra_max",
               )
 
