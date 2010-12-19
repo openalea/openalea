2de30336dd6234288fbc0bee8b0b47e5d5e35755
moscardi <moscardi@04ea39c6-a500-0410-8508-e77fe2a861cf>
2010-12-19 17:54:24 +0000

    add test for read sequence function
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3089 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ image/test/test_read_sequence.py
@@ -0,0 +1,64 @@
+# -*- python -*-
+#
+#       image: image reading
+#
+#       Copyright 2006 INRIA - CIRAD - INRA  
+#
+#       File author(s): Eric Moscardi <eric.moscardi@sophia.inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+"""
+Test image : reading
+"""
+
+__license__= "Cecill-C"
+__revision__ = " $Id:  $ "
+
+from openalea.image import read_sequence
+
+def test_read_sequence():
+    """
+    Test of read_sequence function
+    """
+    directory = "../share/data/p60-tiff/"
+    verbose = False
+
+    # Test of read_sequence with "directory path"
+    res = read_sequence(directory, verbose=verbose)
+    assert res.shape == (460, 460, 59)
+
+    # Test of read_sequence with "number_images" parameter
+    res = read_sequence(directory, number_images=10, verbose=verbose)
+    assert res.shape == (460, 460, 10)
+
+    # Test of read_sequence with "start" parameter
+    res = read_sequence ( directory, start=10, verbose=verbose)
+    assert res.shape == (460, 460, 49)
+
+    # Test of read_sequence with "increment" parameter
+    res = read_sequence ( directory, increment=2, verbose=verbose)
+    assert res.shape == (460, 460, 30)
+
+    res = read_sequence ( directory, increment=3, verbose=verbose)
+    assert res.shape == (460, 460, 20)
+
+    # Test of read_sequence with "filename_contains" parameter
+    res = read_sequence ( directory, filename_contains="zzzrrr...", verbose=verbose)
+    assert res == -1
+
+    # Test of read_sequence with "number_images" and "start" parameters
+    res = read_sequence(directory, number_images=10, start=5, verbose=verbose)
+    assert res.shape == (460, 460, 10)
+
+    # Test of read_sequence with "number_images" and "increment" parameters
+    res = read_sequence(directory, number_images=10, increment=2, verbose=verbose)
+    assert res.shape == (460, 460, 10)
+
+    # Test of read_sequence with "number_images", "start" and "increment" parameters
+    res = read_sequence(directory, number_images=10, start=5, increment=2, verbose=verbose)
+    assert res.shape == (460, 460, 10)
