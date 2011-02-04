77e19a20f7ad63147b908a646e6792678882c637
dbarbeau <dbarbeau@04ea39c6-a500-0410-8508-e77fe2a861cf>
2011-02-04 11:39:01 +0000

    make this more Qtesque (create a Openalea(QApplication)) + thread the session initisalition.
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3119 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/visualeagui.py
+++ visualea/src/visualea/visualeagui.py
@@ -22,7 +22,7 @@ __license__ = "CeCILL v2"
 __revision__ = "$Id$"
 
 
-import sys, os
+import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
@@ -30,11 +30,70 @@ from openalea.core import logger
 from openalea.visualea.mainwindow import MainWindow
 from openalea.core.session import Session
 
+
+
+class Openalea(QtGui.QApplication):
+    """Materialisation of the Openalea application.
+    Does the basic inits. The session is initialised
+    in a thread. It is safe to use once the sessionStarted
+    signal has been emitted."""
+
+    sessionStarted = QtCore.pyqtSignal(object)
+
+    def __init__(self, args):
+        QtGui.QApplication.__init__(self, args)
+        # -- redirect stdout to null if pythonw --
+        set_stdout()
+        # -- reconfigure LoggerOffice to use Qt log handler and a file handler --
+        logger.default_init(level=logger.DEBUG, handlers=["qt"])
+        logger.connect_loggers_to_handlers(logger.get_logger_names(), logger.get_handler_names())
+        if __debug__:
+            logger.set_global_logger_level(logger.DEBUG)
+        else:
+            logger.set_global_logger_level(logger.WARNING)
+        # -- show the splash screen --
+        self.splash = show_splash_screen()
+        # -- main window --
+        self.win = MainWindow(None)
+        self.win.show()
+        self.sessionStarted.connect(self.win.on_session_started)
+        # -- start session in a thread --
+        self.sessionth = threadit(timeit, self, self.__cb_session_thread_end, Session)
+
+    def __cb_session_thread_end(self):
+        self.splash.finish(self.win)
+        self.sessionStarted.emit(self.sessionth.retVal)
+
+    @staticmethod
+    def check_qt_version():
+        """Ensure we are running a minimal version of Qt"""
+        version = QtCore.QT_VERSION_STR
+        # QT_VERSION_STR implement __le__ operator
+        if(version < '4.5.2'):
+            mess = QtGui.QMessageBox.warning(None,
+                                             "Error",
+                                             "Visualea needs Qt library >= 4.5.2")
+            sys.exit(-1)
+
+
+def main(args):
+    # Restore default signal handler for CTRL+C
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    Openalea.check_qt_version()
+    app = Openalea(args)
+    return app.exec_()
+
+
+
+
+###########################
+# A few utility functions #
+###########################
+import os
 import time
 
-# Restore default signal handler for CTRL+C
-import signal
-signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def set_stdout():
     """Disable stdout if using pythonw"""
@@ -43,66 +102,78 @@ def set_stdout():
         sys.stdout = nullfd
         sys.stderr = nullfd
 
-
-def main(args):
-    set_stdout()
-    app = QtGui.QApplication(args)
-    logger.default_init(level=logger.DEBUG, handlers=["qt", "file"])
-    # Check Version
-    version = QtCore.QT_VERSION_STR
-    # QT_VERSION_STR implement __le__ operator
-    if(version < '4.5.2'):
-
-        mess = QtGui.QMessageBox.warning(None, "Error",
-                                         "Visualea need QT library >=4.5.2")
-        return
-
-    #splash screen
+def show_splash_screen():
+    """Show a small splash screen to make people wait for OpenAlea to startup"""
     import metainfo
-
     pix = QtGui.QPixmap(":/icons/splash.png")
-    splash = QtGui.QSplashScreen(pix)
-
+    splash = QtGui.QSplashScreen(pix, QtCore.Qt.WindowStaysOnTopHint)
     splash.show()
-
-    message = QtCore.QString(
-        metainfo.get_copyright() +
-        "Version : %s\n"%(metainfo.get_version(),) +
-        "Loading modules...")
-
+    message = QtCore.QString( metainfo.get_copyright() +
+                              "Version : %s\n"%(metainfo.get_version(),) +
+                              "Loading modules...")
     splash.showMessage(message, QtCore.Qt.AlignCenter|QtCore.Qt.AlignBottom)
-
-    app.processEvents() #make sure qt really display the message before importing the modules.
-
-    time.clock()
-
-    session = Session()
-
-    t1 = time.clock()
-
-    print 'session build in %f seconds'%t1
-
-
-    #parse command line
-    """
-    if(len(args)==2):
-        filename = args[1]
-        try:
-            session.load(filename)
-        except Exception, e:
-            print e
-    """
-
+    # -- make sure qt really display the message before importing the modules.--
     QtGui.QApplication.processEvents()
+    return splash
 
-    win = MainWindow(session)
-    win.show()
-    splash.finish(win);
-    return app.exec_()
+def timeit(f, *args, **kwargs):
+    t1 = time.time()
+    ret = f(*args, **kwargs)
+    t2 = time.time()
+    logger.debug(f.__name__+" took "+str(t2-t1)+" seconds")
+    if __debug__:
+        print
+    return ret
+
+def threadit(f, parent=None, endCb=None, *args, **kwargs):
+    """Utility function that takes a function f and runs it under
+    a new thread. The threadit function immediately returns
+    a CustomThread. If `endCb` is provided, it must be a function with
+    no arguments (or an instance method). It will be called on
+    thread end.
+
+    The returned Custom thread contains the return value of the `f`::
+
+    def func():
+        return "apricots"
+
+    def onFuncEnd():
+        print th.retVal
+
+    th = threadit(func, endCb=onFuncEnd)
+
+
+    It probably requires a QApp to be started somewhere.
+    """
+    class CustomThread(QtCore.QThread):
+        def __init__(self, target, parent=parent, args=[], kwargs={}):
+            QtCore.QThread.__init__(self, parent=parent)
+            self.target = target
+            self.args   = args
+            self.kwargs = kwargs
+            self.retVal = None
+
+        def run(self):
+            self.retVal = self.target(*args,**kwargs)
+
+    th = CustomThread( target = f,
+                       args = args,
+                       kwargs = kwargs)
+    if endCb:
+        th.finished.connect(endCb)
+    th.start()
+    return th
 
 
 
 
+
+
+
+
+############################
+# Ok, Let's start for real #
+############################
 if __name__ == "__main__":
     main(sys.argv)
 
