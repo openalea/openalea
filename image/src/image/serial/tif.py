8b5e116a3179e420e2b7d470129e6c441e57acc2
dbarbeau <dbarbeau@04ea39c6-a500-0410-8508-e77fe2a861cf>
2011-07-13 15:21:58 +0000

    improve tif image reading
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3317 04ea39c6-a500-0410-8508-e77fe2a861cf

--- image/src/image/serial/tif.py
+++ image/src/image/serial/tif.py
@@ -2,14 +2,15 @@
 #
 #       image.serial: read lsm
 #
-#       Copyright 2011 INRIA - CIRAD - INRA  
+#       Copyright 2011 INRIA - CIRAD - INRA
 #
 #       File author(s): Christophe Pradal <christophe.pradal@cirad.fr>
+#                       Daniel Barbeau    <daniel.barbeau@cirad.fr>
 #
 #       Distributed under the Cecill-C License.
 #       See accompanying file LICENSE.txt or copy at
 #           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
-# 
+#
 #       OpenAlea WebSite : http://openalea.gforge.inria.fr
 #
 """
@@ -45,14 +46,49 @@ def read_tif(filename,channel=0):
 
     nx, ny, nz = _data.shape
 
+    # -- prepare metadata dictionnary
     info_str = tif.get_info()
-    bbox = [ l.split('BoundingBox')[-1] for l in info_str.split('\n') if 'BoundingBox' in l][0]
-    xm, xM, ym, yM, zm, zM = map(float,bbox.split())
-    _vx = (xM-xm)/nx
-    _vy = (yM-ym)/ny
-    _vz = (zM-zm)/nz
+    info_dict = dict( filter( lambda x: len(x)==2,
+                              (inf.split(':') for inf in info_str.split("\n"))
+                              ) )
+    for k,v in info_dict.iteritems():
+        info_dict[k] = v.strip()
 
-    #LSM datas   
+    # -- getting the voxelsizes from the tiff image: sometimes
+    # there is a BoundingBox attribute, sometimes there are
+    # XResolution, YResolution, ZResolution or spacing. --
+    if "BoundingBox" in info_dict:
+        bbox = info_dict["BoundingBox"]
+        xm, xM, ym, yM, zm, zM = map(float,bbox.split())
+        _vx = (xM-xm)/nx
+        _vy = (yM-ym)/ny
+        _vz = (zM-zm)/nz
+    else:
+        if "XResolution" in info_dict:
+            # --resolution is stored in a [(values, precision)] list-of-one-tuple --
+            xres_str = eval(info_dict["XResolution"])[0]
+            _vx = float(xres_str[0])/xres_str[1]
+        else:
+            _vx = 1.0 # dumb fallback, maybe we will find something smarter later on
+        if "YResolution" in info_dict:
+            # --resolution is stored in a [(values, precision)] list-of-one-tuple --
+            yres_str = eval(info_dict["YResolution"])[0]
+            _vy = float(yres_str[0])/yres_str[1]
+        else:
+            _vy = 1.0 # dumb fallback, maybe we will find something smarter later on
+
+        if "ZResolution" in info_dict:
+            # --resolution is stored in a [(values, precision)] list-of-one-tuple --
+            zres_str = eval(info_dict["ZResolution"])[0]
+            _vz = float(zres_str[0])/zres_str[1]
+        else:
+            if "spacing" in info_dict:
+                _vz = eval(info_dict["spacing"])
+            else:
+                _vz = 1.0 # dumb fallback, maybe we will find something smarter later on
+
+
+    # -- Return a SpatialImage please! --
     im = SpatialImage(_data)
     im.resolution = _vx,_vy,_vz
 
