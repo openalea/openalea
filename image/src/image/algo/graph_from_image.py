d929e77f16c6586fd1501b1b5c71e4d5ad426d1e
boudon <boudon@04ea39c6-a500-0410-8508-e77fe2a861cf>
2012-02-02 13:10:33 +0000

    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3482 04ea39c6-a500-0410-8508-e77fe2a861cf

--- image/src/image/algo/graph_from_image.py
+++ image/src/image/algo/graph_from_image.py
@@ -29,6 +29,30 @@ def graph_from_image(image,
                      background = 1, 
                      default_properties = default_properties,
                      default_real_property = True):
+    """ 
+        Construct a PropertyGraph from a SpatialImage (or equivalent) representing a segmented image.
+
+        :Parameters:
+         - `labels` (list) - sequence of label numbers of the objects to be measured.
+            If labels is None, all labels are used.
+         - `background` (int) - label representing background.
+         - `default_properties` (list) - the list of name of properties to create. It should be in default_properties.
+         - `default_real_property` (bool) - If real = True, center of mass is in real-world units else in voxels.
+
+        :rtype: PropertyGraph
+
+        :Examples:
+
+        >>> import numpy as np
+        >>> image = np.array([[1, 2, 7, 7, 1, 1],
+                          [1, 6, 5, 7, 3, 3],
+                          [2, 2, 1, 7, 3, 3],
+                          [1, 1, 1, 4, 1, 1]])
+
+        >>> from openalea.image.algo.graph_from_image import graph_from_image
+        >>> graph = graph_from_image(image)
+
+    """
     
     analysis = SpatialImageAnalysis(image)
     
@@ -67,16 +91,28 @@ def graph_from_image(image,
     return graph       
 
 def label2vertex(graph):
+    """
+        Compute a dictionary that map label to vertex id.
+        It requires the existence of a 'label' property
+        
+        :rtype: dict
+    """
     return dict([(j,i) for i,j in graph.property('label')])
 
 def labelpair2edge(graph):
+    """
+        Compute a dictionary that map pair of labels to edge id.
+        It requires the existence of a 'label' property
+        
+        :rtype: dict
+    """
     mlabel2vertex = label2vertex(graph)
     return dict([((mlabel2vertex[graph.source(eid)],mlabel2vertex[graph.target(eid)]),eid) for eid in graph.edges()])
 
 
 def add_vertex_property_from_label_and_value(graph, name, labels, property_values, mlabel2vertex = None):
     """ 
-        Add a property with name 'name' to the graph build from an image. 
+        Add a vertex property with name 'name' to the graph build from an image. 
         The values of the property are given as two lists. 
         First one gives the label in the image and second gives the value of the property.
         Labels are first translated in id of the graph and values are assigned to these ids in the graph
@@ -90,7 +126,7 @@ def add_vertex_property_from_label_and_value(graph, name, labels, property_value
 
 def add_vertex_property_from_label_property(graph, name, label_property, mlabel2vertex = None):
     """ 
-        Add a property with name 'name' to the graph build from an image. 
+        Add a vertex property with name 'name' to the graph build from an image. 
         The values of the property are given as a dictionnary associating a label and a value. 
         Labels are first translated in id of the graph and values are assigned to these ids in the graph
     """
@@ -102,10 +138,10 @@ def add_vertex_property_from_label_property(graph, name, label_property, mlabel2
 
 def add_edge_property_from_label_and_value(graph, name, label_pairs, property_values, mlabelpair2edge = None):
     """ 
-        Add a property with name 'name' to the graph build from an image. 
+        Add an edge property with name 'name' to the graph build from an image. 
         The values of the property are given as two lists. 
-        First one gives the label in the image and second gives the value of the property.
-        Labels are first translated in id of the graph and values are assigned to these ids in the graph
+        First one gives the pair of labels in the image that are connected and the second list gives the value of the property.
+        Pairs of labels are first translated in edge ids of the graph and values are assigned to these ids in the graph
     """
         
     if mlabelpair2edge is None:
@@ -116,9 +152,9 @@ def add_edge_property_from_label_and_value(graph, name, label_pairs, property_va
 
 def add_edge_property_from_label_property(graph, name, labelpair_property, mlabelpair2edge = None):
     """ 
-        Add a property with name 'name' to the graph build from an image. 
-        The values of the property are given as a dictionnary associating a label and a value. 
-        Labels are first translated in id of the graph and values are assigned to these ids in the graph
+        Add an edge property with name 'name' to the graph build from an image. 
+        The values of the property are given as a dictionnary associating a pair of label and a value. 
+        Pairs of labels are first translated in edge ids of the graph and values are assigned to these ids in the graph
     """
     if mlabelpair2edge is None:
         mlabelpair2edge = labelpair2edge(graph)
