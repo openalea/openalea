38e4b2493311282538c16a8ac75213ce28592855
agpr <agpr@04ea39c6-a500-0410-8508-e77fe2a861cf>
2012-03-23 12:11:13 +0000

    now it places each cell in it's own position, forever they were fused because of the find objects function that dit put all in the same referential
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3506 04ea39c6-a500-0410-8508-e77fe2a861cf

--- image/src/image/gui/stack_view3D.py
+++ image/src/image/gui/stack_view3D.py
@@ -19,63 +19,78 @@ __license__= "Cecill-C"
 __revision__=" $Id$ "
 
 # import libraries
+import sys
 import numpy as np
 from scipy import ndimage
 import math
+from openalea.image.all import imread, display
+#compatibility
+try:
+	from tvtk.tools import ivtk
+	from tvtk.api import tvtk
+except ImportError:
+	from enthought.tvtk.tools import ivtk
+	from enthought.tvtk.api import tvtk
+except:
+	print "import impossible dans stack_view3D"
+	sys.exit(0)
 
 
 def img2polydata(image):
-    """
-    Convert a |SpatialImage| to a PolyData object with cells surface
-
-    : Parameters :
-
-    """
-
-    labels = list(np.unique(image))
+	"""
+	Convert a |SpatialImage| to a PolyData object with cells surface
+	
+	: Parameters :
+	
+	"""
+	
+	labels = list(np.unique(image))
+	
+	try:      labels.remove(0)
+	except:   pass
+	try:      labels.remove(1)
+	except:   pass
+	
+	print image.shape
+	
+	xyz = {}
+	bbox = ndimage.find_objects(image)
+	print labels
+	for label in xrange(2,max(labels)+1):
+		if not label in labels: continue
+		print label
+		slices = bbox[label-1]
+		label_image = (image[slices] == label)
+		# compute the indices of voxel with adequate label
+		a = np.array(label_image.nonzero()).T
+		a+=[slices[0].start, slices[1].start, slices[2].start ]
+		print a.shape
+		if a.shape[1] == 4:
+			print a
+		else:
+			xyz[label] = a
     
-    try:      labels.remove(0)
-    except:   pass
-    try:      labels.remove(1)
-    except:   pass
-    
-    print image.shape
-    
-    xyz = {}
-    bbox = ndimage.find_objects(image)
-    print labels
-    for label in xrange(2,max(labels)+1):
-        if not label in labels: continue
-        print label
-        slices = bbox[label-1]
-        label_image = (image[slices] == label)
-        # compute the indices of voxel with adequate label
-        a = np.array(label_image.nonzero()).T
-        print a.shape
-        if a.shape[1] == 4: print a
-        else:               xyz[label] = a
-    
 
-    vx,vy,vz = image.resolution
+	vx,vy,vz = image.resolution
+	
+	polydata = tvtk.AppendPolyData()
+	polys = {}
+	for c in xyz:
+		p=xyz[c]
+		p=p.astype(np.float)
+		pd = tvtk.PolyData(points=xyz[c].astype(np.float))
+		pd.point_data.scalars = [c for i in xrange(len(xyz[c]))]
+	
+		f=tvtk.VertexGlyphFilter(input=pd)
+		f2=tvtk.PointDataToCellData(input=f.output)
+		polys[c]=f2.output
+		polydata.add_input(polys[c])
+		polydata.set_input_array_to_process(0,0,0,0,0)
+	
+	
+	return polydata
+	
 
-    polydata = tvtk.AppendPolyData()
-    polys = {}
-    for c in xyz:
-        pd = tvtk.PolyData(points=xyz[c])
-        pd.point_data.scalars = [c for i in xrange(len(xyz[c]))]
-
-        f=tvtk.VertexGlyphFilter(input=pd)
-        f2=tvtk.PointDataToCellData(input=f.output)
-        polys[c]=f2.output
-        polydata.add_input(polys[c])
-        polydata.set_input_array_to_process(0,0,0,0,0)
-
-
-    return polydata
-
-
-from tvtk.tools import ivtk
-from tvtk.api import tvtk
 
 def display3D(img, colormap = 'jet'):
     polydata = img2polydata(img)
@@ -93,8 +108,6 @@ def display3D(img, colormap = 'jet'):
 
 
 if __name__ == '__main__':
-    from openalea.image import *
     im1 = imread('../../../test/segmentation.inr.gz')
-
     display3D(im1)
 
