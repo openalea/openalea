1b66fed107ed18ca1b84aedc79b2c0a6746377ea
Christophe Pradal <christophe.pradal@inria.fr>
2012-06-18 16:59:49 +0000

    svn merge branch to trunk
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@3629 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/metainfo.py
+++ visualea/src/visualea/metainfo.py
@@ -14,4 +14,4 @@ url = "http://openalea.gforge.inria.fr"
 
 def get_copyright():
 
-    return u"Copyright \xa9 2006-2010 INRIA/CIRAD/INRA\n"
+    return u"Copyright \xa9 2006-2012 INRIA/CIRAD/INRA\n"
