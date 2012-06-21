59873c41d107202cae82362504ab35dcd726e4b2
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2012-06-21 16:18:51 +0000

    Extend get_shared_data
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3633 04ea39c6-a500-0410-8508-e77fe2a861cf

--- deploy/src/openalea/deploy/shared_data.py
+++ deploy/src/openalea/deploy/shared_data.py
@@ -16,7 +16,10 @@
 """INRIA GForge SOAP python API wrappers (based on SOAPpy)
 
 """
+import types
 from os.path import join as pj
+from os.path import realpath, isdir, isfile
+from openalea.core.path import path
 
 __license__ = "Cecill-C"
 __revision__ = " $Id: gforge.py 2243 2010-02-08 17:08:47Z cokelaer $ "
@@ -57,9 +60,10 @@ def get_shared_data_path(package_path, filename=None, share_path=share_path):
     >>> get_shared_data_path(['/home/user/mypackage'], share_path='share/databases')
     '/home/user/mypackage/share/databases'
     """
-    from os.path import realpath, isdir, isfile
-    from openalea.core.path import path
     
+    if isinstance(package_path, types.ModuleType):
+        package_path = package_path.__path__
+
     if isinstance(package_path, list):
         if len(package_path) == 0:
             return None
