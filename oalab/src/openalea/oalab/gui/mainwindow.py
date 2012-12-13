5b72d200c3da2392dba2b6725f1d24f9c6da4042
jcoste <jcoste@04ea39c6-a500-0410-8508-e77fe2a861cf>
2012-12-13 16:29:26 +0000

    Run python code from text editor in shell
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/openalea_2@3679 04ea39c6-a500-0410-8508-e77fe2a861cf

--- oalab/src/openalea/oalab/gui/mainwindow.py
+++ oalab/src/openalea/oalab/gui/mainwindow.py
@@ -1,10 +1,15 @@
+#---------------------------------------------
+# Main Window class
+# 
+# OALab start here with the 'main' function
+#---------------------------------------------
+
 import sys
 
 import qt
 from openalea.oalab.editor.text_editor import PythonCodeEditor as Editor
 from openalea.oalab.shell.shell import ShellWidget
 from openalea.oalab.shell.interpreter import Interpreter
-
 
 class MainWindow(qt.QMainWindow):
     def __init__(self, parent=None):
@@ -14,17 +19,36 @@ class MainWindow(qt.QMainWindow):
         self.centralWidget = Editor()
         self.setCentralWidget(self.centralWidget)
         
-        # interpreter
-        interpreter = Interpreter()
-        
         # dock widget => Shell IPython
+        self.interpreter = Interpreter()# interpreter
         shellDockWidget = qt.QDockWidget("IPython Shell", self)
         shellDockWidget.setObjectName("Shell")
         shellDockWidget.setAllowedAreas(qt.Qt.BottomDockWidgetArea | qt.Qt.TopDockWidgetArea)
         self.addDockWidget(qt.Qt.BottomDockWidgetArea, shellDockWidget)
         
-        shellwdgt = ShellWidget(interpreter)
-        shellDockWidget.setWidget(shellwdgt)
+        self.shellwdgt = ShellWidget(self.interpreter)
+        shellDockWidget.setWidget(self.shellwdgt)
+        
+        # top buttons
+        
+        self.CodeBar = qt.QToolBar(self)
+        # self.CodeBar.setWindowTitle(qt.QApplication.translate("MainWindow", "Code Bar", None, qt.QApplication.UnicodeUTF8))
+        self.CodeBar.setToolButtonStyle(qt.Qt.ToolButtonTextBesideIcon)
+        # self.CodeBar.setObjectName(_fromUtf8("CodeBar"))
+        self.addToolBar(qt.Qt.TopToolBarArea, self.CodeBar)
+        
+        self.actionRun = qt.QAction(self)
+        # icon8 = QtGui.QIcon()
+        # icon8.addPixmap(QtGui.QPixmap(_fromUtf8(":/images/icons/run.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        # self.actionRun.setIcon(icon8)
+        self.actionRun.setText(qt.QApplication.translate("MainWindow", "Run", None, qt.QApplication.UnicodeUTF8))
+        self.actionRun.setShortcut(qt.QApplication.translate("MainWindow", "Ctrl+R", None, qt.QApplication.UnicodeUTF8))
+        self.actionRun.setObjectName("actionRun")
+        
+                   
+        qt.QObject.connect(self.actionRun, qt.SIGNAL('triggered(bool)'),self.run)     
+
+        self.CodeBar.addAction(self.actionRun)
         
         # status bar
         self.sizeLabel = qt.QLabel()     
@@ -36,6 +60,14 @@ class MainWindow(qt.QMainWindow):
         
         # window title    
         self.setWindowTitle("Open Alea Virtual Laboratory")
+        
+    def run(self):
+        code = self.centralWidget.text()
+        interp = self.get_interpreter()
+        interp.runsource(code)
+
+    def get_interpreter(self):
+        return self.interpreter
 
 
 def main():
--- oalab/src/openalea/oalab/gui/qt.py
+++ oalab/src/openalea/oalab/gui/qt.py
@@ -1,3 +1,11 @@
+#---------------------------------------------
+# qt class
+# 
+# Usefull to use the same version of Qt
+# (PyQt, Pyside...)
+#---------------------------------------------
+
+# Set Api
 import sip
 sip.setapi("QString", 2)
 sip.setapi("QVariant", 2)
@@ -7,6 +15,7 @@ sip.setapi("QTextStream", 2)
 sip.setapi("QTime", 2)
 sip.setapi("QUrl", 2)
 
+# Import PyQt
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.Qt import *
