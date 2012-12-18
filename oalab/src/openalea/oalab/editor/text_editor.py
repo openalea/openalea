65696e6735704b481432c998ce9e9e7480bfee5e
jcoste <jcoste@04ea39c6-a500-0410-8508-e77fe2a861cf>
2012-12-18 16:07:23 +0000

    add open and save buttons
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/openalea_2@3682 04ea39c6-a500-0410-8508-e77fe2a861cf

--- oalab/src/openalea/oalab/editor/text_editor.py
+++ oalab/src/openalea/oalab/editor/text_editor.py
@@ -5,35 +5,87 @@ from openalea.oalab.gui import qt
 #   2. Retrieve the type of editor from settings
 #   3. from pkg_resources:entry_point 
 
-EDITOR = "LPY"
-EDITOR = "SPYDER"
-EDITOR = "SPYDER2"
 EDITOR = "VISUALEA"
 EDITOR = "SCINTILLA"
+EDITOR = "LPY"
+EDITOR = "SPYDER"
 
 PythonCodeEditor = None
-
-if EDITOR == "SCINTILLA":
-    from openalea.visualea.scintilla_editor import ScintillaCodeEditor
-    PythonCodeEditor = ScintillaCodeEditor
-
-
-elif EDITOR == "VISUALEA":
-    from openalea.visualea.code_editor import PythonCodeEditor    
     
 
-elif EDITOR == "SPYDER":
+if EDITOR == "SPYDER":
     from spyderlib.widgets.sourcecode.codeeditor import CodeEditor      
-    PythonCodeEditor = CodeEditor
+    class PythonCodeEditor(CodeEditor):
+        def __init__(self, parent=None):
+            super(PythonCodeEditor, self).__init__()
+            self.rehighlight()
+            
+        def get_full_text(self):
+            self.rehighlight()
+            return self.get_text('sof', 'eof')
+    '''
+    ----QPlainTextEdit----
+    doc.qt.digia.com: QPlainText uses very much the same technology and concepts as QTextEdit,
+    but is optimized for plain text handling.
+    - multi-language
+    - syntax coloring
+    - code analysis (pyflakes + pulint)
+    - code completion
+    - calltips
+    - go-to-definition (rope)
+    - function/class browser
+    '''
+    
+    
+elif EDITOR == "SCINTILLA":
+    from openalea.visualea.scintilla_editor import ScintillaCodeEditor
+    class PythonCodeEditor(ScintillaCodeEditor):
+        def __init__(self, parent=None):
+            super(PythonCodeEditor, self).__init__()
+            
+        def get_full_text(self):
+            return self.text()
+    '''
+    ----QSciScintilla----
+    The QsciScintilla class implements a higher level, 
+    more Qt-like, API to the Scintilla editor widget (e.g. Notepad++).
+    - multi-language
+    - syntax coloring
+    - find and replace
+    - clear
+    '''
 
     
-# elif EDITOR == "SPYDER2":
-    # from spyderlib.widgets.editor import EditorPluginExample      
-    # PythonCodeEditor = EditorPluginExample
+elif EDITOR == "VISUALEA":
+    from openalea.visualea.code_editor import PythonCodeEditor as PythonCodeEditor0
+    class PythonCodeEditor(PythonCodeEditor0):
+        def __init__(self, parent=None):
+            super(PythonCodeEditor, self).__init__()
+            
+        def get_full_text(self):
+            return self.text()
+    '''
+    ----QWidget----
+    QWidget is a base class... Not optimal?
+    - syntax coloring
+    - "apply" and "save" buttons
+    '''
     
     
 elif EDITOR == "LPY":
     from openalea.lpy.gui.lpycodeeditor import LpyCodeEditor      
     class PythonCodeEditor(LpyCodeEditor):
         def __init__(self, parent=None):
-            super(PythonCodeEditor, self).__init__(parent)  
+            super(PythonCodeEditor, self).__init__(parent)
+        
+        def get_full_text(self):
+            return self.toPlainText()
+    '''
+    ----QTextEdit----
+    - syntax coloring
+    - (un)comment
+    - (un)tab
+    - go-to-line
+    - (un)zoom
+    - find and replace
+    '''        
--- oalab/src/openalea/oalab/gui/mainwindow.py
+++ oalab/src/openalea/oalab/gui/mainwindow.py
@@ -14,11 +14,23 @@ from openalea.oalab.shell.interpreter import Interpreter
 class MainWindow(qt.QMainWindow):
     def __init__(self, parent=None):
         super(qt.QMainWindow, self).__init__(parent)
+
+        self.current_path = None
         
+        self.set_text_editor()
+        self.set_shell()
+        self.set_top_buttons()
+        self.set_status_bar()
+        
+        # window title    
+        self.setWindowTitle("Open Alea Virtual Laboratory")
+    
+    def set_text_editor(self):
         # central widget => Editor
         self.centralWidget = Editor()
         self.setCentralWidget(self.centralWidget)
-        
+    
+    def set_shell(self):
         # dock widget => Shell IPython
         self.interpreter = Interpreter()# interpreter
         shellDockWidget = qt.QDockWidget("IPython Shell", self)
@@ -28,41 +40,91 @@ class MainWindow(qt.QMainWindow):
         
         self.shellwdgt = ShellWidget(self.interpreter)
         shellDockWidget.setWidget(self.shellwdgt)
-        
-        # top buttons
-        
+    
+    def set_top_buttons(self):
+        # set top buttons
         self.CodeBar = qt.QToolBar(self)
         # self.CodeBar.setWindowTitle(qt.QApplication.translate("MainWindow", "Code Bar", None, qt.QApplication.UnicodeUTF8))
         self.CodeBar.setToolButtonStyle(qt.Qt.ToolButtonTextBesideIcon)
         # self.CodeBar.setObjectName(_fromUtf8("CodeBar"))
         self.addToolBar(qt.Qt.TopToolBarArea, self.CodeBar)
-        
+        # Create button
+        self.actionOpen = qt.QAction(self)
+        self.actionSave = qt.QAction(self)
+        self.actionClose = qt.QAction(self)
         self.actionRun = qt.QAction(self)
+        # Set title of buttons
+        self.actionOpen.setText(qt.QApplication.translate("MainWindow", "Open", None, qt.QApplication.UnicodeUTF8))
+        self.actionSave.setText(qt.QApplication.translate("MainWindow", "Save", None, qt.QApplication.UnicodeUTF8))
+        self.actionClose.setText(qt.QApplication.translate("MainWindow", "Close", None, qt.QApplication.UnicodeUTF8))
+        self.actionRun.setText(qt.QApplication.translate("MainWindow", "Run", None, qt.QApplication.UnicodeUTF8))
+        # Shortcuts
+        self.actionOpen.setShortcut(qt.QApplication.translate("MainWindow", "Ctrl+O", None, qt.QApplication.UnicodeUTF8))
+        self.actionSave.setShortcut(qt.QApplication.translate("MainWindow", "Ctrl+S", None, qt.QApplication.UnicodeUTF8))
+        self.actionClose.setShortcut(qt.QApplication.translate("MainWindow", "Ctrl+W", None, qt.QApplication.UnicodeUTF8))
+        self.actionRun.setShortcut(qt.QApplication.translate("MainWindow", "Ctrl+R", None, qt.QApplication.UnicodeUTF8))
         # icon8 = QtGui.QIcon()
         # icon8.addPixmap(QtGui.QPixmap(_fromUtf8(":/images/icons/run.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         # self.actionRun.setIcon(icon8)
-        self.actionRun.setText(qt.QApplication.translate("MainWindow", "Run", None, qt.QApplication.UnicodeUTF8))
-        self.actionRun.setShortcut(qt.QApplication.translate("MainWindow", "Ctrl+R", None, qt.QApplication.UnicodeUTF8))
+        # Set names
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave.setObjectName("actionSave")
+        self.actionClose.setObjectName("actionClose")
         self.actionRun.setObjectName("actionRun")
-        
-                   
+        # connect actions to buttons
+        qt.QObject.connect(self.actionOpen, qt.SIGNAL('triggered(bool)'),self.open)
+        qt.QObject.connect(self.actionSave, qt.SIGNAL('triggered(bool)'),self.save)     
+        qt.QObject.connect(self.actionClose, qt.SIGNAL('triggered(bool)'),self.close) 
         qt.QObject.connect(self.actionRun, qt.SIGNAL('triggered(bool)'),self.run)     
-
+        self.CodeBar.addAction(self.actionOpen)
+        self.CodeBar.addAction(self.actionSave)
+        self.CodeBar.addAction(self.actionClose)        
         self.CodeBar.addAction(self.actionRun)
-        
+   
+    def set_status_bar(self):   
         # status bar
         self.sizeLabel = qt.QLabel()     
         self.sizeLabel.setFrameStyle(qt.QFrame.StyledPanel|qt.QFrame.Sunken)
         status = self.statusBar()     
         status.setSizeGripEnabled(False)
         status.addPermanentWidget(self.sizeLabel)     
-        status.showMessage("OALab is ready!", 10000)
+        status.showMessage("OALab is ready!", 10000)    
         
-        # window title    
-        self.setWindowTitle("Open Alea Virtual Laboratory")
+    def open(self, fname = None):
+        # TODO
+        try:
+            fname = qt.QFileDialog.getOpenFileName(self, 'Open file', self.current_path, "Python or L-Py File (*.py *.lpy);;Any file(*.*)")
+            f = open(fname, 'r')
+            data = f.read() #.rstrip("\n\r")
+            # self.current_path = f.filePath()
+            f.close()
+            # strdata = str(data)
+            self.centralWidget.set_text(data)
+        except:
+            pass
+        
+    def save(self):
+        # TODO
+        fname = qt.QFileDialog.getSaveFileName(self, 'Save file', self.current_path, "Python File(*.py)")
+        code = self.centralWidget.get_full_text() # type(code) = unicode
+        # writeList = []
+        # for line in code.rstrip("\n\r"):
+            # writeList.append("%s" %line.encode("iso-8859-1","ignore")) #\u240D
+        # f.writelines(writeList)
+        # strcode = code.encode("utf8","ignore")
+        code_enc = code.encode("iso-8859-1","ignore")
+        
+        f = open(fname, "w")
+        # f.writelines("%s" %strcode)
+        f.writelines(code_enc)
+        f.close()
+
+    def close(self):
+        # TODO
+        pass
         
     def run(self):
-        code = self.centralWidget.text()
+        code = self.centralWidget.get_full_text()
         interp = self.get_interpreter()
         interp.runsource(code)
 
