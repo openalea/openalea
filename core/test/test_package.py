9a640c33f19c73806d429bb0c05c68b386f76863
pradal <pradal@04ea39c6-a500-0410-8508-e77fe2a861cf>
2012-12-21 08:42:23 +0000

    Remove global variables for X
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3687 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/test/test_package.py
+++ core/test/test_package.py
@@ -3,7 +3,7 @@ __revision__ = " $Id$ "
 
 from openalea.core.package import *
 from openalea.core.node import gen_port_list
-import os
+import os, shutil
 
 
 def test_package():
@@ -19,66 +19,62 @@ def test_package():
     assert package != None
 
 
-def test_userpackage():
-    metainfo = {'version': '0.0.1',
-              'license': 'CECILL-C',
-              'authors': 'OpenAlea Consortium',
-              'institutes': 'INRIA/CIRAD',
-              'description': 'Base library.',
-              'url': 'http://openalea.gforge.inria.fr',
-              'icon': ''}
-    try:
-        import shutil
-        shutil.rmtree("tstpkg")
-    except:
-        pass
-
-    assert not os.path.exists("tstpkg")
-
-    try:
+class TestUserPackage():
+    def setUp(self):
         os.mkdir("tstpkg")
-    except:
-        pass
 
-    path = os.path.join(os.path.curdir, "tstpkg")
-    mypackage = UserPackage("DummyPkg", metainfo, path)
+    def tearDown(self):
+        shutil.rmtree("tstpkg")
 
+    def test_case_1(self):
 
-    factory = mypackage.create_user_node("TestFact",\
-                                         "category test",
-                                         "this is a test",
-                                         gen_port_list(3),
-                                         gen_port_list(2))
-    assert path in factory.search_path
-    assert len(factory.inputs)==3
-    assert len(factory.outputs)==2
+        metainfo = {'version': '0.0.1',
+                  'license': 'CECILL-C',
+                  'authors': 'OpenAlea Consortium',
+                  'institutes': 'INRIA/CIRAD',
+                  'description': 'Base library.',
+                  'url': 'http://openalea.gforge.inria.fr',
+                  'icon': ''}
 
-    assert os.path.exists("tstpkg/TestFact.py")
-    execfile("tstpkg/TestFact.py")
-
-    mypackage.write()
-    assert os.path.exists("tstpkg/__wralea__.py")
-    assert os.path.exists("tstpkg/__init__.py")
-    execfile("tstpkg/__wralea__.py")
-
-    # Test_clone_package
-    path = os.path.join(os.path.curdir, "clonepkg")
-    pkg2 = UserPackage("ClonePkg", metainfo, path)
-    print pkg2.wralea_path
+        path = os.path.join(os.path.curdir, "tstpkg")
+        mypackage = UserPackage("DummyPkg", metainfo, path)
 
 
-    # todo this is not working !!
-    from openalea.core.pkgmanager import PackageManager
-    pm = PackageManager()
-    pm.add_wralea_path(path, pm.temporary_wralea_paths)
-    pm.init()
-    pkg2.clone_from_package(mypackage)
-    pkg2.write()
+        factory = mypackage.create_user_node("TestFact",
+                                             "category test",
+                                             "this is a test",
+                                             gen_port_list(3),
+                                             gen_port_list(2))
+        assert path in factory.search_path
+        assert len(factory.inputs)==3
+        assert len(factory.outputs)==2
 
-    assert len(pkg2) == 1
-    assert len(pkg2["TestFact"].inputs) == 3
-    assert id(pkg2["TestFact"]) != id(mypackage["TestFact"])
-    assert os.path.exists(path)
-    assert os.path.exists(os.path.join(path, '__wralea__.py'))
-    assert os.path.exists(os.path.join(path, '__init__.py'))
-    assert os.path.exists(os.path.join(path, 'TestFact.py'))
+        assert os.path.exists("tstpkg/TestFact.py")
+        execfile("tstpkg/TestFact.py")
+
+        mypackage.write()
+        assert os.path.exists("tstpkg/__wralea__.py")
+        assert os.path.exists("tstpkg/__init__.py")
+        execfile("tstpkg/__wralea__.py")
+
+        # Test_clone_package
+        path = os.path.join(os.path.curdir, "clonepkg")
+        pkg2 = UserPackage("ClonePkg", metainfo, path)
+        print pkg2.wralea_path
+
+
+        # todo this is not working !!
+        from openalea.core.pkgmanager import PackageManager
+        pm = PackageManager()
+        pm.add_wralea_path(path, pm.temporary_wralea_paths)
+        pm.init()
+        pkg2.clone_from_package(mypackage)
+        pkg2.write()
+
+        assert len(pkg2) == 1
+        assert len(pkg2["TestFact"].inputs) == 3
+        assert id(pkg2["TestFact"]) != id(mypackage["TestFact"])
+        assert os.path.exists(path)
+        assert os.path.exists(os.path.join(path, '__wralea__.py'))
+        assert os.path.exists(os.path.join(path, '__init__.py'))
+        assert os.path.exists(os.path.join(path, 'TestFact.py'))
