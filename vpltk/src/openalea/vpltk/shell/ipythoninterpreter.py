638be9269be614c20df645fb3e3af6ec7adb1385
jcoste <jcoste@04ea39c6-a500-0410-8508-e77fe2a861cf>
2013-02-28 16:29:10 +0000

    'print' pb with ipython not solve
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/openalea_2@3736 04ea39c6-a500-0410-8508-e77fe2a861cf

--- vpltk/src/openalea/vpltk/shell/ipythoninterpreter.py
+++ vpltk/src/openalea/vpltk/shell/ipythoninterpreter.py
@@ -9,20 +9,26 @@ class Interpreter(InProcessKernel):
     """
     
     def __init__(self, gui="qt4", locals=None):
-        super(Interpreter, self).__init__(gui=gui)
+        super(Interpreter, self).__init__(gui=gui)# cf. get_ipython for ipython singleton problem
         self.locals = self.shell.user_ns
         if locals is not None:
             for l in locals:
                 self.locals += l
+        self.locals['shell'] = self        
         
-    def runsource(self, text):
+    def runsource(self, text, store_history=True):
         """
         Run code in IPython Interpreter
         
         :param text: code text to run
         :return: nothing
+        
+        :warning: "print" problem: sometimes, print is displayed later
         """
-        self.shell.run_cell(text)
+        try:
+            self.shell.run_cell(text, store_history=store_history)
+        except:
+            print("Can't run the code %s" %str(text))
         
     runcode = runsource    
         
