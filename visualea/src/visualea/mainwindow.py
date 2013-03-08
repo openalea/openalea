4208b38ea18e333a2b6f2582fb56ddfe83c2fea4
jcoste <jcoste@04ea39c6-a500-0410-8508-e77fe2a861cf>
2013-03-08 14:27:49 +0000

    Add "compile" as in Revision 3629
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/openalea_2@3743 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/mainwindow.py
+++ visualea/src/visualea/mainwindow.py
@@ -655,17 +655,25 @@ class MainWindow(qt.QtGui.QMainWindow,
 
         filename = qt.QtGui.QFileDialog.getOpenFileName(
             self, "Python Script", "Python script (*.py)")
+            
         filename = str(filename)
         if(not filename) : return
 
+        import code
         file = open(filename, 'r')
+        
         sources = ''
         compiled = None
-        for line in file:
-            sources += line
-            
-        try: self.interpreterWidget.get_interpreter().runcode(sources, hidden=False)
-        except: self.interpreterWidget.get_interpreter().runcode(sources)
+        
+        # Try if IPython    
+        try: self.interpreterWidget.get_interpreter().runcode(file, hidden=False)
+        except:
+            for line in file:
+                sources += line
+                compiled = code.compile_command(sources, filename)
+                if(compiled):
+                    self.interpreterWidget.get_interpreter().runcode(compiled)
+                    sources = ''
         
         sources = ''
 
