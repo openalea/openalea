3367b6d07f511eda50638b7caf7785805966f7ce
jcoste <jcoste@04ea39c6-a500-0410-8508-e77fe2a861cf>
2013-04-02 15:13:30 +0000

    add tests
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/openalea_2@3758 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ oalab/test/test_history.py
@@ -0,0 +1,21 @@
+from openalea.oalab.history.history import History
+
+def test_create_and_reset():
+    a = 1
+    b = 2
+    c = 3
+    d = "Viva Virtual Plants Lab!!!"
+
+    h = History()
+    h.add("a",a)
+    h.add("b",b)
+    h.add("the_answer",42)
+    h.reset()
+    h.add("c",c)
+    h.add("d",d)
+    hist = h.getHistory()
+    
+    assert len(hist) == 2
+
+    
+
