32098a1eae39314e91d54baecfec610ab55767ee
jcoste <jcoste@04ea39c6-a500-0410-8508-e77fe2a861cf>
2013-04-05 07:23:21 +0000

    add tests for project
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/branches/openalea_2@3762 04ea39c6-a500-0410-8508-e77fe2a861cf

--- vpltk/test/test_project_and_lpy.py
+++ vpltk/test/test_project_and_lpy.py
@@ -34,6 +34,46 @@ def test_load_project_2():
     
     assert len(tree) == len(tree2)
 
+
+def test_load_project_3():
+    fn = "data/noise_branch-2d.lpy"
+    tree, lsys = run_lpy(fn)
+
+    pm = ProjectManager()
+    current_path = path.abspath(path('.'))
+    proj = pm.load('test_project_lpy',current_path/'data') # load in globals context and python as startup
+    
+    for s in proj.scripts:
+        script_filename = proj.path/proj.name/'scripts'/s
+    tree2, lsys2 = run_lpy(str(script_filename), parameters=proj.ns)
+
+    assert len(tree) == len(tree2)
+    
+    
+def test_load_and_open_project():
+    fn = "data/noise_branch-2d.lpy"
+    tree, lsys = run_lpy(fn)
+
+    pm = ProjectManager()
+    current_path = path.abspath(path('.'))
+    proj = pm.load('test_project_lpy',current_path/'data')
+    
+    for s in proj.scripts:
+        script_filename = proj.path/proj.name/'scripts'/s
+        
+    code = open(script_filename).read()
+    
+    file = open('mytemp.lpy', "w")
+    file.write(code)
+    file.close()    
+
+    tree2, lsys2 = run_lpy('mytemp.lpy', parameters=proj.ns)
+    
+    os.remove('mytemp.lpy')
+    
+
+    assert len(tree) == len(tree2)    
+
     
 def test_create_project():
     pm = ProjectManager()
@@ -42,4 +82,4 @@ def test_create_project():
     
     assert len(listdir) == 3
     
-    shutil.rmtree(proj.path/proj.name)
+    shutil.rmtree(proj.path/proj.name)     
