e149a22ec33f9478c7eb8f88f460a6b3440df5d9
Frederic Boudon <frederic.boudon@cirad.fr>
2013-04-18 20:16:06 +0000

    fix bug with QtCore import
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@3785 04ea39c6-a500-0410-8508-e77fe2a861cf

--- vpltk/src/openalea/vpltk/shell/scishell.py
+++ vpltk/src/openalea/vpltk/shell/scishell.py
@@ -21,7 +21,8 @@ __revision__ = " $Id: scishell.py 3672 2012-12-05 12:28:19Z jcoste $"
 
 import os, sys
 from openalea.vpltk.qt import QtCore, QtGui
-from openalea.vpltk.qt.QtCore import Qt
+#from openalea.vpltk.qt.QtCore import Qt
+Qt = QtCore.Qt
 
 from PyQt4.Qsci import QsciScintilla, QsciLexerPython, QsciAPIs
 from streamredirection import *
