e10395c8d22a90d9e4c9d5f30f302ac09651d9e4
jcoste <jcoste@04ea39c6-a500-0410-8508-e77fe2a861cf>
2013-05-16 12:57:17 +0000

    add plugin test
    git-svn-id: https://scm.gforge.inria.fr/svn/openalea/trunk@3801 04ea39c6-a500-0410-8508-e77fe2a861cf

--- vpltk/test/test_plugin.py
+++ vpltk/test/test_plugin.py
@@ -1,5 +1,12 @@
 from openalea.vpltk import plugin
 
-def test1()
+def test1():
     plugins = plugin.discover('wralea')
     assert 'vpltk' in plugins
+    
+def test2():
+    plugins = plugin.discover('wralea')
+    myentrypoint = plugins['vpltk']
+    myplugin = plugin.Plugin(myentrypoint)
+    assert myplugin.name == myentrypoint.name
+
