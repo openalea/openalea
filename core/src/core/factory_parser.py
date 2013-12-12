5742f948e9a24e044d56157e5e2ea846ed3891b0
Julien Diener <julien.diener@inria.fr>
2013-12-12 13:41:08 +0000

    factory_parser module added
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@3978 04ea39c6-a500-0410-8508-e77fe2a861cf

---
+++ core/src/core/factory_parser.py
@@ -0,0 +1,12 @@
+"""
+Automatical extraction of openalea factories for python modules and packages  
+"""
+
+def parse_module(module_name):
+    """
+    Create an openalea package from a python module
+    
+    :Inputs::
+      `module_name`: the *full* name of the module to parse (i.e. 'pck.module')
+    """
+    pass
