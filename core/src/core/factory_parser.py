38a8fe1b1c164b4a233cfc86af369ca739dfce23
Julien Diener <julien.diener@inria.fr>
2013-12-12 14:24:11 +0000

    factory_parser header
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@3981 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/factory_parser.py
+++ core/src/core/factory_parser.py
@@ -1,7 +1,23 @@
+# -*- python -*-
+#
+#       OpenAlea.Core
+#
+#       Copyright 2006-2009 INRIA - CIRAD - INRA
+#
+#       File author(s): Julien Diener  <julien.diener@inria.fr>
+#                       Julien Coste   <julien.coste@inria.fr>
+#                       Guillaume baty <guillaume.baty@inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+###############################################################################
 """
 Automatical extraction of openalea factories for python modules and packages  
 """
-
 """
 TODO:
   - remove all ##
