32983252770ad8b3dfc2ee106d61d063d4601d71
Camille Chambon <cchambon@grignon.inra.fr>
2013-12-12 10:44:00 +0000

    Add decorators inputs and outputs.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@3970 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/factory_decorator.py
+++ core/src/core/factory_decorator.py
@@ -6,9 +6,37 @@ def factory(f):
     defines f, then add f to __factories__, else, create the list __factories__ 
     which contains f.
     '''
+    f.__factory__ = True
     mod = sys.modules[f.__module__]
     if not hasattr(f, '__factories__'):
         mod.__factories__ = [f]
     else:
         mod.__factories__.append(f)
     return f
+
+
+class inputs(object):
+    '''
+    Add inputs.
+    '''
+    def __init__(self, *args):
+        self.args = args
+        
+    def __call__(self, f):
+        f.__inputs__ = self.args
+        return f
+
+
+class outputs(object):
+    '''
+    Add outputs.
+    '''
+    def __init__(self, *args):
+        self.args = args
+        
+    def __call__(self, f):
+        f.__outputs__ = self.args
+        return f
+
+
+
