e650122de619fecb94a21d4b2dd7287d4eaf6431
Christophe Pradal <christophe.pradal@inria.fr>
2013-12-13 14:16:58 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@4002 04ea39c6-a500-0410-8508-e77fe2a861cf

--- core/src/core/signature.py
+++ core/src/core/signature.py
@@ -122,6 +122,13 @@ class Signature(object):
         else:
             return self.parameters[:]
 
+    def get_returns(self):
+        """ Return the outputs of a functor based on a predifened contract.
+        TO BE DEFINED
+        TODO
+        """
+        return dict(name="out", interface=None), 
+
     def get_varargs(self):
         return self.varargs
 
@@ -207,44 +214,3 @@ class Signature(object):
         return args, defaults, varargs, keywords
 
 
-#def get_parameters(f):
-#    """ Return the list of
-#    dict(name='name', interface=None, value=None) corresponding
-#    to the function parameters """
-#
-#    try:
-#        # if f is a class
-#        if (not isinstance(f, types.FunctionType)):
-#            specs = inspect.getargspec(f.__call__)
-#            varnames = specs[0]
-#            varnames = varnames[1:]
-#            defaults = specs[3]
-#
-#        # f is a function
-#        else:
-#            specs = inspect.getargspec(f)
-#            varnames = specs[0]
-#            defaults = f.func_defaults
-#            defaults = specs[3]
-#
-#    except Exception, e:
-#        print e
-#        return ()
-#
-#    if (defaults == None): defaults = []
-#    if (varnames == None): varnames = []
-#
-#    args= []
-#    nv = len(varnames)
-#    nd = len(defaults)
-#    # parse args without default value
-#    for i,name in enumerate(varnames[:nv - nd]):
-#        args.append(dict(name=name, interface=None, value=None))
-#
-#    # parse args with default value
-#    for i,name in enumerate(varnames[nv - nd:]):
-#        v = defaults[i]
-#        interface = TypeInterfaceMap().get(type(v),None)
-#        args.append(dict(name=name, interface=interface, value=v))
-#
-#    return args
