f028df633c2f76f8a682e2d632e521eee4c12e8a
Julien Coste <julien.coste@inria.fr>
2014-01-31 16:41:57 +0000

    fix test import
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@15638 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/test/test_import.py
+++ oalab/test/test_import.py
@@ -8,8 +8,8 @@ def test_import():
     from openalea.oalab.gui.menu import PanedMenu
     from openalea.vpltk.shell.shell import get_interpreter_class, get_shell_class
     from openalea.oalab.applets.container import AppletContainer
-    from openalea.oalab.scene.vplscene import SceneWidget
-    from openalea.oalab.project.widgets import ProjectWidget
+    from openalea.oalab.scene.view3d import Viewer
+    from openalea.oalab.project.manager import ProjectManager
     from openalea.oalab.project.treeview import ProjectTreeView, ProjectLayoutWidget
     from openalea.oalab.package import PackageViewWidget, PackageCategorieViewWidget, PackageSearchWidget
     from openalea.oalab.gui.store import Store
