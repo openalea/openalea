023f3a2fa1f0bc04179810c4d2a9acfa75571b50
Julien Coste <julien.coste@inria.fr>
2014-02-05 17:06:16 +0000

    resize soil in demo mango
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@15699 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/share/data/mango/scripts/soil.py
+++ oalab/share/data/mango/scripts/soil.py
@@ -3,7 +3,7 @@
 from openalea.plantgl.all import *
 import numpy as np
 
-size = 1000.
+size = 100.
 
 heights=(np.random.random_sample((size,size))*.1).tolist()
 grid = Shape(Translated((-size/2.,-size/2.,0), ElevationGrid(heights)), Material(Color3(149,109,0)))
