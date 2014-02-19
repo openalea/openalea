acf2a56b724a3bc86b2d12899043271e6db028c1
Julien Coste <julien.coste@inria.fr>
2014-02-19 09:04:58 +0000

    re-order windows once more
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@15822 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/gui/mainwindow.py
+++ oalab/src/openalea/oalab/gui/mainwindow.py
@@ -257,15 +257,18 @@ class MainWindow(QtGui.QMainWindow):
 
         # Docks
         self._dockWidget("Viewer3D", controller.applets["Viewer3D"], name="3D Viewer", position=QtCore.Qt.RightDockWidgetArea)
-        self._dockWidget("Logger", controller.applets["Logger"], position=QtCore.Qt.RightDockWidgetArea)
         self._dockWidget("Store", controller.applets["Store"], name="OpenAlea Store", position=QtCore.Qt.RightDockWidgetArea)
-        self._dockWidget("Shell", controller.shell, name="IPython Shell", position=QtCore.Qt.RightDockWidgetArea)
+        
+        self._dockWidget("HelpWidget", controller.applets["HelpWidget"], position=QtCore.Qt.RightDockWidgetArea, alias="Help")
         self._dockWidget("Project", controller.applets["Project"],position=QtCore.Qt.RightDockWidgetArea) # Project Manager
+        
+        self._dockWidget("Shell", controller.shell, name="IPython Shell", position=QtCore.Qt.RightDockWidgetArea)
+        self._dockWidget("Logger", controller.applets["Logger"], position=QtCore.Qt.RightDockWidgetArea)
+        
         self._dockWidget("Packages", controller.applets["Packages"], position=QtCore.Qt.BottomDockWidgetArea)
         self._dockWidget("PackageCategories", controller.applets["PackageCategories"], name="Package Categories", position=QtCore.Qt.BottomDockWidgetArea)
         self._dockWidget("PackageSearch", controller.applets["PackageSearch"], name="Package Search", position=QtCore.Qt.BottomDockWidgetArea)
         self._dockWidget("ControlPanel", controller.applets["ControlPanel"], name="Control Panel", position=QtCore.Qt.BottomDockWidgetArea)
-        self._dockWidget("HelpWidget", controller.applets["HelpWidget"], position=QtCore.Qt.RightDockWidgetArea, alias="Help")
         
         controller.applets['ControlPanel'].geometry_editor.setStatusBar(self.statusBar())
         self._dockwidgets['Store'].hide()
