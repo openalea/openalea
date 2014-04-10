05654e9d6a77b98bf1fa44781fe38120855624e6
Julien Coste <julien.coste@inria.fr>
2014-04-10 11:45:23 +0000

    add doc and small refactor of project
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@4196 04ea39c6-a500-0410-8508-e77fe2a861cf

--- vpltk/test/test_project_manager.py
+++ vpltk/test/test_project_manager.py
@@ -1,29 +1,33 @@
 from openalea.vpltk.project.manager import ProjectManager
 
+
 def test_project_manager_discover():
     pm = ProjectManager()
     pm.discover()
-    
+
     assert len(pm.projects) > 0
+
 
 def test_project_manager_discover_not_add_twice_by_discover_twice():
     pm = ProjectManager()
     pm.discover()
-    
+
     projects_nb = len(pm.projects)
     pm.discover()
-    
+
     assert len(pm.projects) == projects_nb
-  
+
+
 def test_project_manager_discover_not_add_twice():
     pm = ProjectManager()
     pm.discover()
-    
+
     nb = len(pm.projects)
-    nb2 = int(nb/2)
+    nb2 = int(nb / 2)
     assert nb > 1
     assert nb2 > 0
-    assert str(pm.projects[nb-1].name) != str(pm.projects[nb2-1].name)
+    assert str(pm.projects[nb - 1].name) != str(pm.projects[nb2 - 1].name)
+
 
 def test_project_manager_add_path():
     pm = ProjectManager()
