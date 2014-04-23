084b132c0e3b98c5e10db9b9f2242dd9d8df43f8
Guillaume Baty <guillaume.baty@inria.fr>
2014-04-23 13:52:47 +0000

    Add minilab extension. Load widgets depending on config. Define config in extension plugin
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16424 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/plugins/tissuelab.py
+++ oalab/src/openalea/oalab/plugins/tissuelab.py
@@ -1,18 +1,77 @@
+
+import copy
 from openalea.vpltk.plugin import iter_plugins
+from openalea.oalab.plugins.minilab import OALabExtensionMini
 
-class OALabExtensionTissue(object):
+config_template = """
+# Configuration file for application.
 
-    data = {
-        'extension_name': 'tissue',
-        'implements' : ['IOAExtension']
-    }
+c = get_config()
 
-    def __call__(self, mainwin):
-        for widget_factory_class in iter_plugins('oalab.widget'):
+#------------------------------------------------------------------------------
+# MainConfig configuration
+#------------------------------------------------------------------------------
 
-            # Select appropriate widget.
-            # Currently, widget defines himself extensions it belong to.
-            if self.data['extension_name'] in widget_factory_class.data['extensions']:
-                widget_factory = widget_factory_class()
-                widget_factory(mainwin)
+# This is an application.
 
+# MainConfig will inherit config from: Application
+
+# Set the log level by value or name.
+# c.MainConfig.log_level = 30
+
+# The Logging format template
+# c.MainConfig.log_format = '[%(name)s]%(highlevel)s %(message)s'
+
+# Load this config file
+# c.MainConfig.config_file = u''
+
+# The date format used by logging formatters for %(asctime)s
+# c.MainConfig.log_datefmt = '%Y-%m-%d %H:%M:%S'
+
+#------------------------------------------------------------------------------
+# MainWindowConfig configuration
+#------------------------------------------------------------------------------
+
+# Display package manager
+c.MainWindowConfig.packages = True
+
+# Display control panel
+c.MainWindowConfig.controlpanel = False
+
+# Display Help widget
+c.MainWindowConfig.helpwidget = True
+
+# Display 3D Viewer
+c.MainWindowConfig.viewer3d = True
+
+# Display menu bar
+c.MainWindowConfig.menu = True
+
+# Display project tree view
+c.MainWindowConfig.project = True
+
+# Display graphical Python interpreter
+c.MainWindowConfig.shell = True
+
+# Display search widget for package manager
+c.MainWindowConfig.packagesearch = True
+
+# Display logger (usefull to debug)
+c.MainWindowConfig.logger = True
+
+# List of graphical Python interpreters, sorted by preference
+c.MainWindowConfig.shell_priority = ['oalab:IPythonShell', 'oalab:BuiltinShell']
+
+# Display alea_install_gui
+c.MainWindowConfig.store = False
+
+# Display package manager sorted by categories
+c.MainWindowConfig.packagecategories = True
+"""
+
+
+class OALabExtensionTissue(OALabExtensionMini):
+
+    data = copy.deepcopy(OALabExtensionMini.data)
+    data['extension_name'] = 'tissue'
+    data['config_template'] = config_template
