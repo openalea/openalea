b22d74cae1c62c04059a25d4a174c3d0bea99cbc
Julien Coste <julien.coste@inria.fr>
2014-04-29 13:18:28 +0000

    remove empty widget
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16477 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/plugins/applets/projectmanager.py
+++ oalab/src/openalea/oalab/plugins/applets/projectmanager.py
@@ -11,5 +11,3 @@ class ProjectManager(PluginApplet):
 
         self._applet = ProjectManagerWidget(mainwindow.session, mainwindow)
         self._fill_menu(mainwindow, self._applet)
-
-        mainwindow.add_applet(self._applet, self.alias, area='inputs')
