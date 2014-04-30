fda34d5f1af9006245d8d4b6691ae12c4b9c862e
Julien Coste <julien.coste@inria.fr>
2014-04-30 15:45:17 +0000

    tabbify the 3 package manager views
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16508 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/plugins/applets/packagemanager.py
+++ oalab/src/openalea/oalab/plugins/applets/packagemanager.py
@@ -8,20 +8,8 @@ class PkgManagerWidget(PluginApplet):
 
     def __call__(self, mainwindow):
         # Load and instantiate graphical component that actually provide feature
-        from openalea.oalab.package.widgets import (PackageViewWidget,
-                                                    PackageCategorieViewWidget,
-                                                    PackageSearchWidget)
+        from openalea.oalab.package.widgets import PackageManagerTreeView
+        self._applet = PackageManagerTreeView(session=mainwindow.session, controller=mainwindow)
+        mainwindow.add_applet(self._applet, self.alias, area='inputs')
 
-        factories = []
-        factories.append(('Packages', PackageViewWidget))
-        factories.append(('PackageCategories', PackageCategorieViewWidget))
-        factories.append(('PackageSearch', PackageSearchWidget))
-
-        applets = []
-        for name, klass in factories:
-            applet = klass(session=mainwindow.session, controller=mainwindow)
-            self._fill_menu(mainwindow, applet)
-            mainwindow.add_applet(applet, name, area='inputs')
-            applets.append(applet)
-        self._applet = applets
 
