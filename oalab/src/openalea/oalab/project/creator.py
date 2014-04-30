27f9ca6ef99831658f6885961f46a54d567a0b24
Julien Coste <julien.coste@inria.fr>
2014-04-30 12:56:15 +0000

    permit to edit metadata of a project
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16499 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/project/creator.py
+++ oalab/src/openalea/oalab/project/creator.py
@@ -28,12 +28,12 @@ class CreateProjectWidget(QtGui.QWidget):
     """
     Object which permit to create projects.
     """
-    def __init__(self, parent=None):
+    def __init__(self, proj=None, parent=None):
         super(CreateProjectWidget, self).__init__(parent)
         self.project = None
         layout = QtGui.QGridLayout(self)
 
-        layout.addWidget(QtGui.QLabel("Fill this to create your project"), 0, 0)
+        layout.addWidget(QtGui.QLabel("Fill this to set metadata of your project"), 0, 0)
         categories = ["name", "author", "author_email", "description", "long_description", "citation", "url", "icon",
                       "dependencies", "license", "version"]
         i = 1
@@ -42,43 +42,79 @@ class CreateProjectWidget(QtGui.QWidget):
             layout.addWidget(wid, i, 0)
             i += 1
 
-        date = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
-        self.name_lineedit = QtGui.QLineEdit('project_%s' % date)
-        self.name_lineedit.setMinimumWidth(300)
-        layout.addWidget(self.name_lineedit, 1, 1)
-        self.author_lineedit = QtGui.QLineEdit('OpenAlea Consortium')
-        layout.addWidget(self.author_lineedit, 2, 1)
-        self.author_email_lineedit = QtGui.QLineEdit('')
-        layout.addWidget(self.author_email_lineedit, 3, 1)
-        self.description_lineedit = QtGui.QLineEdit('')
-        layout.addWidget(self.description_lineedit, 4, 1)
-        self.long_description_lineedit = QtGui.QLineEdit('')
-        layout.addWidget(self.long_description_lineedit, 5, 1)
-        self.citation_lineedit = QtGui.QLineEdit('')
-        layout.addWidget(self.citation_lineedit, 6, 1)
-        self.url_lineedit = QtGui.QLineEdit('')
-        layout.addWidget(self.url_lineedit, 7, 1)
-        self.icon_lineedit = QtGui.QLineEdit('')
-        layout.addWidget(self.icon_lineedit, 8, 1)
-        self.dependencies_lineedit = QtGui.QLineEdit('[]')
-        layout.addWidget(self.dependencies_lineedit, 9, 1)
-        self.license_lineedit = QtGui.QLineEdit('CeCILL-C')
-        layout.addWidget(self.license_lineedit, 10, 1)
-        self.version_lineedit = QtGui.QLineEdit('0.1')
-        layout.addWidget(self.version_lineedit, 11, 1)
+        if not proj:
+            date = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+            self.name_lineedit = QtGui.QLineEdit('project_%s' % date)
+            self.name_lineedit.setMinimumWidth(300)
+            layout.addWidget(self.name_lineedit, 1, 1)
+            self.author_lineedit = QtGui.QLineEdit('OpenAlea Consortium')
+            layout.addWidget(self.author_lineedit, 2, 1)
+            self.author_email_lineedit = QtGui.QLineEdit('')
+            layout.addWidget(self.author_email_lineedit, 3, 1)
+            self.description_lineedit = QtGui.QLineEdit('')
+            layout.addWidget(self.description_lineedit, 4, 1)
+            self.long_description_lineedit = QtGui.QLineEdit('')
+            layout.addWidget(self.long_description_lineedit, 5, 1)
+            self.citation_lineedit = QtGui.QLineEdit('')
+            layout.addWidget(self.citation_lineedit, 6, 1)
+            self.url_lineedit = QtGui.QLineEdit('')
+            layout.addWidget(self.url_lineedit, 7, 1)
+            self.icon_lineedit = QtGui.QLineEdit('')
+            layout.addWidget(self.icon_lineedit, 8, 1)
+            self.dependencies_lineedit = QtGui.QLineEdit('[]')
+            layout.addWidget(self.dependencies_lineedit, 9, 1)
+            self.license_lineedit = QtGui.QLineEdit('CeCILL-C')
+            layout.addWidget(self.license_lineedit, 10, 1)
+            self.version_lineedit = QtGui.QLineEdit('0.1')
+            layout.addWidget(self.version_lineedit, 11, 1)
 
-        layout.addWidget(QtGui.QLabel("path"), 12, 0)
-        path_ = path(settings.get_project_dir())
-        self.path_lineedit =  QtGui.QLineEdit(str(path_))
-        layout.addWidget(self.path_lineedit, 12, 1)
-        self.btn_path = QtGui.QPushButton("   ...   ")
-        layout.addWidget(self.btn_path, 12, 2)
-        self.connect(self.btn_path, QtCore.SIGNAL('clicked()'), self.select_path)
+            layout.addWidget(QtGui.QLabel("path"), 12, 0)
+            path_ = path(settings.get_project_dir())
+            self.path_lineedit =  QtGui.QLineEdit(str(path_))
+            layout.addWidget(self.path_lineedit, 12, 1)
+            self.btn_path = QtGui.QPushButton("   ...   ")
+            layout.addWidget(self.btn_path, 12, 2)
+            self.connect(self.btn_path, QtCore.SIGNAL('clicked()'), self.select_path)
 
-        self.ok_btn = QtGui.QPushButton("Create Project")
-        layout.addWidget(self.ok_btn, 13, 0, 1, 3)
-        self.connect(self.ok_btn, QtCore.SIGNAL('clicked()'), self.ok_clicked)
+            self.ok_btn = QtGui.QPushButton("Create metadata set")
+            layout.addWidget(self.ok_btn, 13, 0, 1, 3)
+            self.connect(self.ok_btn, QtCore.SIGNAL('clicked()'), self.ok_clicked)
+        else:
+            self.name_lineedit = QtGui.QLineEdit(proj.name)
+            self.name_lineedit.setMinimumWidth(300)
+            layout.addWidget(self.name_lineedit, 1, 1)
+            self.author_lineedit = QtGui.QLineEdit(proj.author)
+            layout.addWidget(self.author_lineedit, 2, 1)
+            self.author_email_lineedit = QtGui.QLineEdit(proj.author_email)
+            layout.addWidget(self.author_email_lineedit, 3, 1)
+            self.description_lineedit = QtGui.QLineEdit(proj.description)
+            layout.addWidget(self.description_lineedit, 4, 1)
+            self.long_description_lineedit = QtGui.QLineEdit(proj.long_description)
+            layout.addWidget(self.long_description_lineedit, 5, 1)
+            self.citation_lineedit = QtGui.QLineEdit(proj.citation)
+            layout.addWidget(self.citation_lineedit, 6, 1)
+            self.url_lineedit = QtGui.QLineEdit(proj.url)
+            layout.addWidget(self.url_lineedit, 7, 1)
+            self.icon_lineedit = QtGui.QLineEdit(proj.icon)
+            layout.addWidget(self.icon_lineedit, 8, 1)
+            self.dependencies_lineedit = QtGui.QLineEdit(str(proj.dependencies))
+            layout.addWidget(self.dependencies_lineedit, 9, 1)
+            self.license_lineedit = QtGui.QLineEdit(proj.license)
+            layout.addWidget(self.license_lineedit, 10, 1)
+            self.version_lineedit = QtGui.QLineEdit(proj.version)
+            layout.addWidget(self.version_lineedit, 11, 1)
 
+            layout.addWidget(QtGui.QLabel("path"), 12, 0)
+            path_ = path(settings.get_project_dir())
+            self.path_lineedit =  QtGui.QLineEdit(str(proj.path))
+            layout.addWidget(self.path_lineedit, 12, 1)
+            self.btn_path = QtGui.QPushButton("   ...   ")
+            layout.addWidget(self.btn_path, 12, 2)
+            self.connect(self.btn_path, QtCore.SIGNAL('clicked()'), self.select_path)
+
+            self.ok_btn = QtGui.QPushButton("Set metadata")
+            layout.addWidget(self.ok_btn, 13, 0, 1, 3)
+            self.connect(self.ok_btn, QtCore.SIGNAL('clicked()'), self.ok_clicked)
         # self.connect(self, QtCore.SIGNAL('ProjectOpened(PyQt_PyObject)'), self.catch_signal)
 
     def ok_clicked(self):
@@ -95,7 +131,7 @@ class CreateProjectWidget(QtGui.QWidget):
                        license=self.license_lineedit.text(),
                        version=self.version_lineedit.text())
         self.project = proj
-        self.emit(QtCore.SIGNAL('ProjectOpened(PyQt_PyObject)'), proj)
+        self.emit(QtCore.SIGNAL('ProjectMetadataSet(PyQt_PyObject)'), proj)
         self.hide()
 
     def select_path(self):
