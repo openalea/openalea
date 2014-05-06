27ab47e44daf9020d1c6912321fca89a04c1d94d
Julien Coste <julien.coste@inria.fr>
2014-05-06 08:34:08 +0000

    first stub for world
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16524 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/world/world.py
+++ oalab/src/openalea/oalab/world/world.py
@@ -2,7 +2,7 @@
 #
 #       OpenAlea.OALab: Multi-Paradigm GUI
 #
-#       Copyright 2013 INRIA - CIRAD - INRA
+#       Copyright 2014 INRIA - CIRAD - INRA
 #
 #       File author(s): Julien Coste <julien.coste@inria.fr>
 #
@@ -17,9 +17,47 @@
 ###############################################################################
 __revision__ = ""
 
+from collections import OrderedDict
 
-class World(object):
-    pass
+
+class World(OrderedDict):
+    """
+    Contain objects of the world.
+    """
+    # TODO: How to manage time?
+    def __init__(self):
+        super(World, self).__init__()
+
+    def get_scene(self):
+        """
+        return only object stock in self that are part of the scene
+        """
+        return_dict = OrderedDict()
+        for obj in self:
+            if self[obj].in_scene:
+                return_dict[obj] = self[obj]
+        return return_dict
+
 
 class WorldObject(object):
-    pass
+    """
+    Object of the world.
+    """
+    def __init__(self, obj, model_id, output_id, in_scene=False):
+        """
+        :param obj: object to store
+        :param model_id: identifier of the model used to create this object
+        :param output_id: identifier of output of the model used to create this object
+        :param in_scene: set to True if it is a part of the scene (so it is viewable)
+        """
+        self.obj = obj
+        self.model_id = model_id
+        self.output_id = output_id
+        self.in_scene = in_scene
+
+    def _repr_qglviewer_(self):
+        """
+        Return a 3d representation used in the viewer (PyQGLViewer)
+        """
+        # TODO: use services
+        pass
