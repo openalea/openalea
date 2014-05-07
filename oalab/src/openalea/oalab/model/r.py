ee18dfa6a6a8dfe22af3c11d3042d21eb64480bb
Julien Coste <julien.coste@inria.fr>
2014-05-07 09:15:39 +0000

    add models r and workflow
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16538 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ oalab/src/openalea/oalab/model/r.py
@@ -0,0 +1,99 @@
+# -*- python -*-
+#
+#       OpenAlea.OALab: Multi-Paradigm GUI
+#
+#       Copyright 2014 INRIA - CIRAD - INRA
+#
+#       File author(s): Julien Coste <julien.coste@inria.fr>
+#
+#       File contributor(s):
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+###############################################################################
+from openalea.oalab.model.model import Model
+
+
+class RModel(Model):
+    default_name = "R"
+    default_file_name = "script.r"
+    pattern = "*.r"
+    extension = "r"
+    icon = ":/images/resources/RLogo.png"
+
+    def __init__(self, name="script.r", code="", inputs=[], outputs=[]):
+        super(RModel, self).__init__()
+        self._step = None
+        self._animate = None
+        self._init = None
+
+    def repr_code(self):
+        """
+        :return: a string representation of model to save it on disk
+        """
+        return self.code
+
+    def run(self, interpreter):
+        """
+        execute model thanks to interpreter
+        """
+        user_ns = interpreter.user_ns
+        code = """%load_ext rmagic
+%%R
+
+""" + self.code
+        interpreter.runcode(code)
+
+        ## Hack to store methods init, step and animate
+        self._init = user_ns.get("init")
+        if not callable(self._init):
+            self._init = None
+
+        self._step = user_ns.get("step")
+        if not callable(self._step):
+            self._step = None
+
+        self._animate = user_ns.get("animate")
+        if not callable(self._animate):
+            self._animate = None
+            if self._step :
+                def animate():
+                    for i in range(5):
+                        self._step()
+                self._animate = animate
+
+    def reset(self, interpreter):
+        """
+        go back to initial step
+        """
+        # TODO : get function from the current widget
+        if self._init:
+            self._init()
+
+    def step(self, interpreter):
+        """
+        execute only one step of the model
+        """
+        # TODO : get function from the current widget
+        if self._step:
+            self._step()
+
+    def stop(self, interpreter):
+        """
+        stop execution
+        """
+        # TODO : to implement
+        pass
+
+    def animate(self, interpreter):
+        """
+        run model step by step
+        """
+        # TODO : get function from the current widget
+        if self._animate:
+            self._animate()
+
---
+++ oalab/src/openalea/oalab/model/visualea.py
@@ -0,0 +1,89 @@
+# -*- python -*-
+#
+#       OpenAlea.OALab: Multi-Paradigm GUI
+#
+#       Copyright 2014 INRIA - CIRAD - INRA
+#
+#       File author(s): Julien Coste <julien.coste@inria.fr>
+#
+#       File contributor(s):
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+###############################################################################
+from openalea.oalab.model.model import Model
+from openalea.core.compositenode import CompositeNodeFactory
+
+
+class VisualeaModel(Model):
+    default_name = "Workflow"
+    default_file_name = "workflow.wpy"
+    pattern = "*.wpy"
+    extension = "wpy"
+    icon = ":/images/resources/openalealogo.png"
+
+    def __init__(self, name="workflow.wpy", code="", inputs=[], outputs=[]):
+        super(VisualeaModel, self).__init__()
+        if (code is None) or (code is ""):
+            self._workflow = CompositeNodeFactory(name).instantiate()
+        elif isinstance(code, CompositeNodeFactory):
+            self._workflow = code.instantiate()
+        else:
+            cnf = eval(code, globals(), locals())
+            self._workflow = cnf.instantiate()
+
+    def repr_code(self):
+        """
+        :return: a string representation of model to save it on disk
+        """
+        name = self.name
+
+        if name[-3:] in '.py':
+            name = name[-3:]
+        elif name[-4:] in '.wpy':
+            name = name[-4:]
+        cn = self.applet._workflow
+        cnf = CompositeNodeFactory(name)
+        cn.to_factory(cnf)
+
+        repr_wf = repr(cnf.get_writer())
+        # hack to allow eval rather than exec...
+        # TODO: change the writer
+
+        repr_wf = (' = ').join(repr_wf.split(' = ')[1:])
+        return repr_wf
+
+    def run(self, interpreter):
+        """
+        execute model thanks to interpreter
+        """
+        self._workflow.eval()
+
+    def reset(self, interpreter):
+        """
+        go back to initial step
+        """
+        self._workflow.reset()
+
+    def step(self, interpreter):
+        """
+        execute only one step of the model
+        """
+        self._workflow.eval_as_expression(step=True)
+
+    def stop(self, interpreter):
+        """
+        stop execution
+        """
+        # TODO : to implement
+        pass
+
+    def animate(self, interpreter):
+        """
+        run model step by step
+        """
+        self._workflow.eval()
