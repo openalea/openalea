d6d98c15848c5b7e648acc6125c3e862610b0a92
Guillaume Baty <guillaume.baty@inria.fr>
2014-05-15 14:39:58 +0000

    Fix logger bug and side effect (import openalea.vpltk had as consequence to import almost all vplant modules)
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@4234 04ea39c6-a500-0410-8508-e77fe2a861cf

--- vpltk/src/openalea/vpltk/project/__init__.py
+++ vpltk/src/openalea/vpltk/project/__init__.py
@@ -77,3 +77,5 @@ To search projects that are not located inside default directories:
     print project_manager.projects
 
 """
+from openalea.vpltk.project.project import Project
+from openalea.vpltk.project.manager import ProjectManager
