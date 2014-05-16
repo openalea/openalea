1522e0157d7ee14378838b96abcf99e90d21d137
Julien Coste <julien.coste@inria.fr>
2014-05-16 12:02:57 +0000

    change oalab2 into oalab
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16602 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/main.py
+++ oalab/src/openalea/oalab/main.py
@@ -25,23 +25,6 @@ from openalea.oalab.session.all import Session
 
 def main():
     """
-    OpenAleaLaboratory starts here
-    """
-    # Create shortcut in project dir to oalab.share dir (only if necessary)
-    create_project_shortcut()
-    # Launch app
-
-    session = Session()
-    cli = CommandLineParser(sys.argv, session)
-
-    if session.gui:
-        from openalea.oalab.gui.app import OALab
-        app = OALab(sys.argv, session)
-        app.exec_()
-
-
-def main2():
-    """
     1. Parse command line arguments.
     2. If GUI enabled (session.gui), launch QApplication
     3. Search an extension in "oalab.extension" plugins.
@@ -88,70 +71,6 @@ def main2():
             print 'Extension %r not found' % session.extension
             print 'Please choose a valid \033[94mextension\033[0m:'
             print '\n'.join(available_extensions)
-
-def main_plantlab():
-    """
-    OpenAleaLaboratory starts here
-    """
-    # Create shortcut in project dir to oalab.share dir (only if necessary)
-    create_project_shortcut()
-
-    session = Session()
-    session.extension = 'plant'
-
-    # Launch app
-    if session.gui:
-        from openalea.oalab.gui.app import OALab
-        app = OALab(sys.argv, session)
-        app.exec_()
-
-def main_tissuelab():
-    """
-    OpenAleaLaboratory starts here
-    """
-    # Create shortcut in project dir to oalab.share dir (only if necessary)
-    create_project_shortcut()
-
-    session = Session()
-    session.extension = 'tissue'
-
-    # Launch app
-    if session.gui:
-        from openalea.oalab.gui.app import OALab
-        app = OALab(sys.argv, session)
-        app.exec_()
-
-def main_3dlab():
-    """
-    OpenAleaLaboratory starts here
-    """
-    # Create shortcut in project dir to oalab.share dir (only if necessary)
-    create_project_shortcut()
-
-    session = Session()
-    session.extension = '3d'
-
-    # Launch app
-    if session.gui:
-        from openalea.oalab.gui.app import OALab
-        app = OALab(sys.argv, session)
-        app.exec_()
-
-def main_minilab():
-    """
-    OpenAleaLaboratory starts here
-    """
-    # Create shortcut in project dir to oalab.share dir (only if necessary)
-    create_project_shortcut()
-
-    session = Session()
-    session.extension = 'mini'
-
-    if session.gui:
-        from openalea.oalab.gui.app import OALab
-        # Launch app
-        app = OALab(sys.argv, session)
-        app.exec_()
 
 
 if(__name__ == "__main__"):
