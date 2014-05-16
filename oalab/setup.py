afcae7e2eec515ca2d6c6dc7fd28c4decc896f1c
Julien Coste <julien.coste@inria.fr>
2014-05-16 12:02:57 +0000

    change oalab2 into oalab
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16602 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/setup.py
+++ oalab/setup.py
@@ -85,12 +85,7 @@ setup(
     # Declare src and wralea as entry_points (extensions) of your package
     entry_points={
         'gui_scripts': ['oalab = openalea.oalab.main:main',
-                        'oalab2 = openalea.oalab.main:main2',
-                        'openalealab = openalea.oalab.main:main',
-                        'plantlab = openalea.oalab.main:main_plantlab',
-                        'tissuelab = openalea.oalab.main:main_tissuelab',
-                        '3dlab = openalea.oalab.main:main_3dlab',
-                        'minilab = openalea.oalab.main:main_minilab'],
+                        'openalealab = openalea.oalab.main:main'],
         'oalab.control': ['IntControl = openalea.oalab.control.stdcontrol:IntControl',
                           'BoolControl = openalea.oalab.control.stdcontrol:BoolControl',
                           'FloatControl = openalea.oalab.control.stdcontrol:FloatControl'],
