4be246171f3eaeed7692d53227d56fced06d0db4
Christophe Pradal <christophe.pradal@inria.fr>
2014-05-27 08:59:08 +0000

    control code review
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16672 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/control/control.py
+++ oalab/src/openalea/oalab/control/control.py
@@ -1,4 +1,7 @@
+# Header
+""" Control
 
+"""
 from openalea.core.observer import Observed
 from openalea.oalab.service.interface import get_interface
 
@@ -6,23 +9,27 @@ import copy
 import inspect
 
 class Control(Observed):
+    """ TODO
+
+    """
     def __init__(self, name, interface, value=None, widget=None, constraints=None):
+        """
+
+        """
         Observed.__init__(self)
-        if isinstance(interface, basestring):
-            interface = get_interface(interface)
-        if constraints is None:
-            constraints = {}
-        if inspect.isclass(interface):
-            interface = interface(**constraints)
-        self._interface = interface
+
         self.name = name
         self.widget = widget
-        self.default()
-        if value:
-            self._value = value
+
+        self._interface = get_interface(interface, constraints)
+
+        self._value = value 
+        if value is None: 
+            self._value = self.default_value(self._interface)  
 
     def __repr__(self):
-        return 'Control(%r, name=%r)' % (self._interface, self.name)
+        # TODO: CPL : Update the function
+        return 'Control(name=%r, %r)' % (self._interface, self.name)
 
     def notify_change(self):
         self.notify_listeners(('ValueChanged', self._value))
@@ -30,8 +37,9 @@ class Control(Observed):
     def rename(self, name):
         self.name = name
 
-    def default(self):
-        self._value = self._interface.default()
+    @staticmethod
+    def default_value(interface):
+        return interface.default()
 
     def value(self):
         return self._value
@@ -41,11 +49,15 @@ class Control(Observed):
         A deep copy of value must be saved in _value.
         Original one is stored in _user_value.
         """
-        self._value = copy.deepcopy(value)
-        self._user_value = value
+        # TODO: CPL: To discuss !!!!
+        #self._value = copy.deepcopy(value)
+        #self._user_value = value
         self.notify_change()
 
     def check(self, value):
         pass
 
-    interface = property(fget=lambda self:self._interface)
+    @property
+    def interface(self):
+        return self._interface
+ 
