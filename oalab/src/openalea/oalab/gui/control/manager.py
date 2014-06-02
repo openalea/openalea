6ef73b7cc0c9ea797b960b4f7297493e7e1192f2
Guillaume Baty <guillaume.baty@inria.fr>
2014-06-02 15:41:53 +0000

    Separate globals and local controls
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16730 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/gui/control/manager.py
+++ oalab/src/openalea/oalab/gui/control/manager.py
@@ -2,45 +2,55 @@
 from openalea.vpltk.qt import QtGui, QtCore
 from openalea.core.observer import AbstractListener
 
-from openalea.oalab.gui.control.model_view import ControlModel, ControlDelegate
-from openalea.oalab.gui.control.editor import ControlEditorDialog
+from openalea.oalab.gui.control.model_view import ControlModel, ControlView
 from openalea.oalab.control.manager import ControlManager
 
 class ControlManagerWidget(QtGui.QWidget, AbstractListener):
     def __init__(self):
         AbstractListener.__init__(self)
         QtGui.QWidget.__init__(self)
-        self._layout = QtGui.QHBoxLayout(self)
+        self._layout = QtGui.QVBoxLayout(self)
 
         self._manager = ControlManager()
 
-        self.model = ControlModel(self._manager)
+        self.model_global = ControlModel(self._manager)
+        self.model_local = ControlModel(self._manager)
 
-        self.view = QtGui.QTreeView()
-        self.view.setModel(self.model)
-        self.view.pressed.connect(self.on_control_selected)
-        self.view.setEditTriggers(self.view.AllEditTriggers)
-        self.view.setDragEnabled(True)
-        self.view.setDragDropMode(self.view.DragOnly)
-        self.view.setSortingEnabled(True)
-        self.view.setItemDelegateForColumn(1, ControlDelegate())
+        self.view_global = ControlView()
+        self.view_global.setModel(self.model_global)
+        self.view_global.pressed.connect(self.on_global_control_selected)
 
-        self._layout.addWidget(self.view)
+        self.view_local = ControlView()
+        self.view_local.setModel(self.model_local)
+        self.view_local.pressed.connect(self.on_local_control_selected)
+
+        self.l_local_control_name = QtGui.QLabel()
+        self.setModel(None)
+
+        self._layout.addWidget(QtGui.QLabel("Global controls"))
+        self._layout.addWidget(self.view_global)
+        self._layout.addWidget(self.l_local_control_name)
+        self._layout.addWidget(self.view_local)
+        self._layout.setSpacing(0)
+        self._layout.setContentsMargins(0, 0, 0, 0)
 
         self._i = 1
 
-    def on_control_selected(self, index):
-        print self.model.control(index)
+    def on_global_control_selected(self, index):
+        print 'global:',
+        self.on_control_selected(index, self.model_global)
 
-    def contextMenuEvent(self, event):
-        menu = QtGui.QMenu(self)
-        action = QtGui.QAction("New control", menu)
-        action.triggered.connect(self.new_control)
-        menu.addAction(action)
-        menu.exec_(event.globalPos())
+    def on_local_control_selected(self, index):
+        print 'local:',
+        self.on_control_selected(index, self.model_local)
 
-    def new_control(self):
-        dial = ControlEditorDialog('control_%d' % self._i)
-        dial.exec_()
-        self._manager.add_control(dial.control())
-        self._i += 1
+    def on_control_selected(self, index, model):
+        print model.control(index)
+
+    def setModel(self, model_uid=None):
+        if model_uid is None :
+            self.l_local_control_name.setText("Local controls")
+            self.model_local.set(False)
+        else :
+            self.l_local_control_name.setText("%s controls" % model_uid)
+            self.model_local.set(model_uid)
