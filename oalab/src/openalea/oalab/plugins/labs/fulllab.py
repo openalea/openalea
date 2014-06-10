41347da2ce35ff21c3fd739618baa1dcaaff55ad
Julien Coste <julien.coste@inria.fr>
2014-06-10 12:03:20 +0000

    fix *add model to project*
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16799 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/plugins/labs/fulllab.py
+++ oalab/src/openalea/oalab/plugins/labs/fulllab.py
@@ -1,17 +1,29 @@
 from openalea.vpltk.plugin import iter_plugins
 
-class FullLab(object):
 
+class FullLab(object):
     name = 'full'
 
     def __call__(self, mainwin):
-        plugins = []
-        for plugin in iter_plugins('oalab.applet'):
-            plug = plugin()
-            plugins.append(plug)
-            mainwin.add_plugin(plug)
+        plugins_used = []
+        plugins_to_use = [plug for plug in iter_plugins('oalab.applet')]
 
-        for plugin in plugins:
+        # Set editor manager, because it is used by other plugins
+        for plugin in plugins_to_use:
+            if plugin.name == "EditorManager":
+                plug = plugin()
+                plugins_used.append(plug)
+                mainwin.add_plugin(plug)
+
+        # Set other plugins
+        for plugin in plugins_to_use:
+            plug = plugin()
+            if plugin.name not in [plu.name for plu in plugins_used]:
+                plugins_used.append(plug)
+                mainwin.add_plugin(plug)
+
+        # Initialize all plugins
+        for plugin in plugins_used:
             instance = plugin.instance()
             if hasattr(instance, "initialize"):
                 instance.initialize()
