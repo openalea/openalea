c61987679111438616302462875507c16adaed26
Guillaume Baty <guillaume.baty@inria.fr>
2014-06-23 14:46:01 +0000

    Reorder tab in plantlab
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16944 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/plugins/labs/plantlab.py
+++ oalab/src/openalea/oalab/plugins/labs/plantlab.py
@@ -2,22 +2,29 @@
 class PlantLab(object):
     name = 'plant'
     applets = [
-        'ControlManager',
-        'EditorManager',
-        'HelpWidget',
-        'HistoryWidget',
-        'Logger',
-        'PkgManagerWidget',
         'ProjectManager',
         'ProjectWidget',
+        'ControlManager',
+        'PkgManagerWidget',
+        'EditorManager',
         'Viewer3D',
+        'HelpWidget',
+        'Logger',
+        'HistoryWidget',
         ]
 
 
     def __call__(self, mainwin):
         from openalea.vpltk.plugin import iter_plugins
         session = mainwin.session
+
+        plugins = {}
         for plugin in iter_plugins('oalab.applet', debug=session.debug_plugins):
             if plugin.name in self.applets:
-                mainwin.add_plugin(plugin())
+                plugins[plugin.name] = plugin()
+
+        # Keep order, this is important to order tabs correctly
+        for name in self.applets:
+            if name in plugins:
+                mainwin.add_plugin(plugins[name])
 
