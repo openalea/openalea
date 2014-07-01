9b14d1c3026dd7604d6f5575cd3aecc18b19364a
Christophe Godin <christophe.godin@inria.fr>
2014-07-01 17:02:16 +0000

    first integrated demo for OpenAleaLab
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@17057 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ oalab/share/data/Fractals/lib/algo.py
@@ -0,0 +1,20 @@
+from openalea.mtg.io import lpy2mtg, mtg2lpy, axialtree2mtg, mtg2axialtree
+
+def to_mtg(model):
+    scales = {}
+    scales['P'] = 1
+    scales['A'] =1
+    scales['I'] = 1
+    scales['B'] = 1
+
+    params ={}
+    params['P'] = ['age']
+    params['A'] = ['age']
+    params['I'] = ['age']
+
+    g = axialtree2mtg(model.axialtree, scales, None, params)
+    return g
+
+def count(g, label):
+    return len([v for v in g if g.label(v) == label])
+
