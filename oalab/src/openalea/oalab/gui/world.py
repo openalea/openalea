922836802c6cb0df8863f66517a0122864f840c0
Julien Coste <julien.coste@inria.fr>
2014-07-01 13:59:02 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@17054 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/gui/world.py
+++ oalab/src/openalea/oalab/gui/world.py
@@ -48,8 +48,7 @@ class WorldBrowser(GenericWorldBrowser, AbstractListener):
 
         actionClearWorld = QtGui.QAction(QtGui.QIcon(":/images/resources/plant.png"), "Clear World", self)
         actionClearWorld.triggered.connect(self.world.clear)
-        self._actions = [["Viewer", "World", actionClearWorld, 0],
-                         ["Project", "World", actionClearWorld, 0]]
+        self._actions = [["Project", "World", actionClearWorld, 0]]
 
     def actions(self):
         return self._actions
