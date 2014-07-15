9f407a907848722195ccc2fe6373645d821f1a21
Julien Diener <julien.diener@inria.fr>
2014-07-15 13:02:26 +0000

    plot2d applet (tries to) enforce qt4agg backend
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@17150 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/plugins/applets/plot2d.py
+++ oalab/src/openalea/oalab/plugins/applets/plot2d.py
@@ -4,13 +4,16 @@ from openalea.oalab.plugins.applets import PluginApplet
 class Plot2dWidget(PluginApplet):                     
 
     name = 'Plot2dWidget'
-    alias = 'Plot2D'
+    alias = 'Plot2d'
 
     def __call__(self, mainwindow):
         # Load and instantiate graphical component that actually provide feature
         from openalea.oalab.plot2d import activate_in_pyplot
         from openalea.oalab.plot2d.widget import MplTabWidget
         from matplotlib import pyplot as plt                     
+        
+        # work with qt4agg backend
+        plt.switch_backend('qt4agg')
 
         self._applet = MplTabWidget.get_singleton()
         activate_in_pyplot()
