a0cd3642b48e1465774c6923aa0e8604542f9a78
Julien Diener <julien.diener@inria.fr>
2014-07-15 11:16:26 +0000

    simple doc added to matplotlib.__init__
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@17146 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- oalab/src/openalea/oalab/matplotlib/__init__.py
+++ oalab/src/openalea/oalab/matplotlib/__init__.py
@@ -1,20 +1,33 @@
-# -*- python -*-
-#
-#       OpenAlea.OALab.matplotlib: matplotlib in OpenAleaLab
-#
-#       Copyright 2013 INRIA - CIRAD - INRA
-#
-#       File author(s): Julien Diener <julien.diener@inria.fr>
-#
-#       File contributor(s):
-#
-#       Distributed under the Cecill-C License.
-#       See accompanying file LICENSE.txt or copy at
-#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
-#
-#       OpenAlea WebSite : http://openalea.gforge.inria.fr
-#
-###############################################################################
+"""
+Package that provides a unique qt widget for matplotlib figure
+
+To get pyplot actions redirected to this widget, use::
+
+    from openalea.oalab import matplotlib as oampl
+    oampl.activate_in_pyplot()
+    
+To disable it, do::
+
+    from openalea.oalab import matplotlib as oampl
+    oampl.desactivate_in_pyplot()
+
+
+#########################################################
+#   OpenAlea.OALab.matplotlib: matplotlib in OpenAleaLab
+#   
+#   Copyright 2013 INRIA - CIRAD - INRA
+#   
+#   File author(s): Julien Diener <julien.diener@inria.fr>
+#   
+#   File contributor(s):
+#   
+#   Distributed under the Cecill-C License.
+#   See accompanying file LICENSE.txt or copy at
+#       http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#   
+#   OpenAlea WebSite : http://openalea.gforge.inria.fr
+#########################################################
+"""
 __revision__ = "0.1"
 
 # to save mpl function to replace
