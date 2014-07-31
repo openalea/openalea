544a9fad753331def0ab0dd3c2e3330f0982df65
Julien Coste <julien.coste@inria.fr>
2014-07-31 14:28:49 +0000

    add test for python and lpy for inputs kwargs
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@17297 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ oalab/test/test_run_lpy_model.py
@@ -0,0 +1,62 @@
+# -*- python -*-
+#
+#       OpenAlea.OALab: Multi-Paradigm GUI
+#
+#       Copyright 2014 INRIA - CIRAD - INRA
+#
+#       File author(s): Julien Coste <julien.coste@inria.fr>
+#
+#       File contributor(s):
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+###############################################################################
+from openalea.oalab.model.lpy import LPyModel
+
+
+def test_run():
+    model_src = '''"""
+input = lstring="_(0.01)-(90)F(1)", N=2
+output = lstring
+"""
+
+derivation length: N
+
+production:
+
+F(x) :
+  produce  F(x/3.0)+F(x/3.0)--F(x/3.0)+F(x/3.0)
+
+endlsystem
+'''
+    model = LPyModel(code=model_src)
+    assert model is not None
+    assert model.outputs == []
+    result = model()
+
+    true_result = "_(0.01)-(90)F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)+F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)+F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)"
+    assert str(model.outputs) == true_result
+    assert str(result) == true_result
+
+    result = model("_(0.01)-(90)F(1)", 0)
+    assert result == "_(0.01)-(90)F(1)"
+
+    result = model(N=0)
+    assert result == "_(0.01)-(90)F(1)"
+
+    true_result2 = "F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)+F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)+F(0.111111111111)+F(0.111111111111)--F(0.111111111111)+F(0.111111111111)"
+    result = model.run("F(1)")
+    assert str(result) == true_result2
+
+    result = model(N=0, lstring="F(1)")
+    assert result == "F(1)"
+
+    result = model(lstring="F(5)", N=0)
+    assert result == "F(5)"
+
+
+
