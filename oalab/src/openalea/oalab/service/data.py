b5f6be489b2f9fe1ef78ec03222f7578837ff098
Guillaume Baty <guillaume.baty@inria.fr>
2014-08-11 17:30:42 +0200

    Improve data service and tests

--- oalab/src/openalea/oalab/service/data.py
+++ oalab/src/openalea/oalab/service/data.py
@@ -7,15 +7,39 @@ class Data(object):
         self.path = Path(path)
         self.datatype = datatype
 
-def data(category, path, **kwargs):
+def datatype(path):
+    return path.ext
+
+def dataname(path):
+    return path.namebase
+
+def dataclass(dtype):
+    """
+    Return class wich match dtype.
+    For example, for 'python' dtype it return PythonModel class.
+
+    Matching can be extended with plugins.
+    """
+    return Data
+
+def arrange_data_args(name, path, dtype):
+    if name is None:
+        name = dataname(path)
+    if dtype is None:
+        dtype = datatype(path)
+    return name, path, dtype
+
+def data(path, name=None, dtype=None, **kwargs):
     content = kwargs['content'] if 'content' in kwargs else None
 
     path = Path(path)
     if path.isfile():
-        if content:
-            raise ValueError, 'err'
+        if content is not None:
+            raise ValueError, "got multiple values for content (parameter and '%s')" % path.name
         else:
-            return Data(path.namebase, path, path.ext)
+            name, path, dtype = arrange_data_args(name, path, dtype)
+            DataClass = dataclass(dtype)
+            return DataClass(name, path, dtype)
     elif path.exists() :
         raise ValueError, '%s exists but is not a file'
     elif not path.exists():
@@ -24,4 +48,6 @@ def data(category, path, **kwargs):
         f = path.open('wb')
         f.write(content)
         f.close()
-        return Data(path.namebase, path, path.ext)
+        name, path, dtype = arrange_data_args(name, path, dtype)
+        DataClass = dataclass(dtype)
+        return DataClass(name, path, dtype)
--- vpltk/test/test_service_new_data.py
+++ vpltk/test/test_service_new_data.py
@@ -21,7 +21,7 @@ class TestProject(unittest.TestCase):
         filepath = self.tmpdir / 'new_image.tiff'
         content = b'blob'
 
-        d1 = data('data', path=filepath, content=content)
+        d1 = data(path=filepath, content=content)
         assert d1.path.name == 'new_image.tiff'
         assert d1.name == 'new_image'
         assert filepath.isfile()
@@ -32,9 +32,14 @@ class TestProject(unittest.TestCase):
         assert content == content2
 
         # Embed existing data  (image.tiff exists)
-        d2 = data('data', path=get_data('image.tiff'))
+        d2 = data(path=get_data('image.tiff'))
         assert d2.path.name == 'image.tiff'
         assert d2.name == 'image'
 
         # Cannot set content because data yet exists
-        d3 = data('data', path='image.tiff', content=b'')
+        # Case datatype is not defined
+        with self.assertRaises(ValueError) as cm:
+            d3 = data(path=get_data('image.tiff'), content=b'')
+        msg = "got multiple values for content (parameter and 'image.tiff')"
+        self.assertEqual(cm.exception.message, msg)
+
