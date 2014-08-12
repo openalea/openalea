6ced148ff90e8ff98dfd3b4a0b69207704e96873
Julien Coste <julien.coste@inria.fr>
2014-08-12 16:20:40 +0200

    remove unused tests

--- vpltk/test/test_project_manager_2.py
+++ vpltk/test/test_project_manager_2.py
@@ -1,8 +1,15 @@
 import unittest
 
+from openalea.core.path import path as Path
 from openalea.vpltk.project import ProjectManager
+
 pm = ProjectManager()
 
+def get_data_dir():
+    return Path(__file__).parent.abspath() / 'data'
+
+def get_data(filename):
+    return get_data_dir() / filename
 
 class TestSequenceFunctions(unittest.TestCase):
 
@@ -10,14 +17,15 @@ class TestSequenceFunctions(unittest.TestCase):
         # Create tmp dir
         pass
 
-    def test_load_project(self):
-        project = pm.load('test_project_lpy', projectdir=get_test_data_dir())
+    # def test_load_project(self):
+    #     project = pm.load('test_project_lpy', projectdir=get_data_dir())
+    #
+    #     with self.assertRaises(ValueError) as cm:
+    #         project = pm.load('unexisting_project')
+    #
+    #     msg = "Project 'unexisting_project' doesn't exist"
+    #     self.assertEqual(cm.exception.message, msg)
 
-        with self.assertRaises(ValueError) as cm:
-            project = pm.load('unexisting_project')
-
-        msg = "Project 'unexisting_project' doesn't exist"
-        self.assertEqual(cm.exception.message, msg)
-
-    def tearDown():
+    def tearDown(self):
+        pass
         # Delete tmp dir
