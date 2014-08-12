b4993a3bcae304f5872266e3da84f4d72372a342
Julien Coste <julien.coste@inria.fr>
2014-08-12 16:20:40 +0200

    remove unused tests

--- vpltk/test/test_project.py
+++ vpltk/test/test_project.py
@@ -1,139 +1,177 @@
-from openalea.vpltk.project.manager import ProjectManager
-from openalea.vpltk.project.project import Project
-from openalea.core.path import path, tempdir
-import shutil
+
+import unittest
+from openalea.core.path import tempdir, path
+from openalea.vpltk.project.project2 import Project
+from openalea.oalab.service.data import data
 
 
-def test_import_project():
-    from openalea.vpltk.project import Project, ProjectManager
-    assert Project is not None
-    assert ProjectManager is not None
+def get_data(filename):
+    return path(__file__).parent.abspath() / 'data' / filename
 
 
-def test_load():
-    pm = ProjectManager()
-    current_path = path('.')
-    proj = pm.load(name='test_project_lpy', path=current_path / 'data') # load in globals context and python as startup
+class TestProject(unittest.TestCase):
 
-    for category in ["src", "cache", "scene", "startup"]:
-        assert hasattr(proj, category)
+    def setUp(self):
+        self.tmpdir = tempdir()
+        self.project = Project('test', projectdir=self.tmpdir)
 
-    for metadata in ["name", "icon", "author", "description", "version", "license", "dependencies"]:
-        assert metadata in proj.metadata
+    def tearDown(self):
+        self.project = None
+        self.tmpdir.rmtree()
 
-    assert len(proj.src.keys()) == 1
-    assert len(proj.cache.keys()) == 4
-    assert len(proj.startup.keys()) == 1
-
-
-def test_manifest():
-    pm = ProjectManager()
-    proj = pm.load('test_project_lpy', 'data')
-    proj.load_manifest()
-    assert len(proj.src) == 1
-    assert len(proj.cache) == 4
-    assert len(proj.startup) == 1
+    def test_path_incompatibility(self):
+        model = data(path=get_data('model.py'))
+        model2 = self.project.add('model', model)
+        assert model is not model2
+        assert model2.path == self.project.path / 'model' / 'model.py'
 
 
-def test_save_project():
-    pm = ProjectManager()
+    def test_add_data(self):
 
-    # Work in temporary directory
-    tmpprojectdir = tempdir()
+        # Create new data from scratch
+        d1 = self.project.add('data', filename='image_1.tiff', content=b'')
+        assert d1.path.name == 'image_1.tiff'
 
-    proj = pm.create('my_new_temp_project', tmpprojectdir)
-    proj.add("model", "plop.py", "print 'hello world'")
-    # proj.control["my_integer"] = 42
-    # proj.control["my_float"] = 3.14
-    proj.save()
+        # Create new data and get content and name from existing file
+        d2 = self.project.add('data', path=get_data('image_2.tiff'))
+        assert d2.path.name == 'image_2.tiff'
 
-    assert len(proj.src) == 1
-    # assert len(proj.control) == 2
+        # Create new data with explicit filename and get content from existing file
+        # Warning: in this test, extension is changed
+        d3 = self.project.add('data', path=get_data('image.jpg'), filename='image_4.jpeg')
+        assert d3.path.name == 'image_4.jpeg'
 
-    pm.close('my_new_temp_project')
-    proj2 = pm.load('my_new_temp_project', tmpprojectdir)
+        for data in [d1, d2, d3]:
+            assert data.path.parent == self.project.path / 'data'
 
-    assert len(proj2.src) == 1
-    # assert len(proj2.control) == 2
-    # assert proj2.control["my_integer"] == 42
-    # assert proj2.control["my_float"] == 3.14
-    assert proj2.src["plop"].code == "print 'hello world'"
+        assert len(self.project.data) == 3
 
-    pm.close('my_new_temp_project')
+    def test_add_exceptions(self):
 
-    # Delete temporary dir
-    tmpprojectdir.rmtree()
+        # Case of path passed doesn't exist
+        with self.assertRaises(ValueError) as cm:
+            self.project.add('data', path='/do/not/exist.err')
 
-
-def test_add_script():
-    pm = ProjectManager()
-    proj = pm.create('my_new_temp_project', path("data"))
-    proj.add("model", "1.py", "blablabla")
-    proj.add("model", "2.py", "blablabla2")
-    proj.add("model", "3.py", "blablabla3")
-    proj.add("model", "4.py", "blablabla4")
-    assert len(proj.models()) == 4
-    assert proj.is_project() is True
-    assert proj.is_script() is False
+        msg = "path '/do/not/exist.err' doesn't exists"
+        self.assertEqual(cm.exception.message, msg)
+        assert(len(self.project.data) == 0)
 
 
-def test_rename():
-    tmpprojectdir = tempdir()
+        # Case datatype is not defined
+        with self.assertRaises(ValueError) as cm:
+            self.project.add('data')
 
-    pm = ProjectManager()
-    proj = pm.create('my_new_temp_project', tmpprojectdir)
-    proj.add("model", "1.py", "blablabla")
+        msg = "path or filename required"
+        self.assertEqual(cm.exception.message, msg)
+        assert(len(self.project.data) == 0)
 
-    model1_path = proj.path / 'model' / '1.py'
-    model2_path = proj.path / 'model' / '2.py'
+        # Case user give an existing path and a content
+        with self.assertRaises(ValueError) as cm:
+            self.project.add('data', path=get_data('image.jpg'), content=b'')
 
-    assert model1_path.isfile()
+        msg = "got multiple values for content (parameter and 'image.jpg')"
+        self.assertEqual(cm.exception.message, msg)
+        assert(len(self.project.data) == 0)
 
-    proj.rename("model", "1", "2")
-    assert len(proj.src) == 1
-    assert proj.src["2"].repr_code() == "blablabla"
+        # Add an object twice
+        self.project.add('data', filename='image.png')
+        with self.assertRaises(ValueError) as cm:
+            self.project.add('data', filename='image.png')
 
-    # Old bug, path lost extension at rename
-    model2_badpath = proj.path / 'model' / '2'
-    assert model2_badpath.exists() is False
-    assert model2_path.isfile()
-
-
-    tmpprojectdir.rmtree()
-
-def test_rename_project():
-    pm = ProjectManager()
-    proj = pm.create('my_new_temp_project', path("data"))
-    proj.add("model", "1.py", "blablabla")
-    proj.rename("project", "my_new_temp_project", "new_name")
-    assert proj.name == "new_name"
-
-#######################################################
-# New API
-#######################################################
+        msg = "data 'image.png' already exists in project 'test'"
+        self.assertEqual(cm.exception.message, msg)
 
 
 
-def test_create_project():
-    proj = Project('my_new_temp_project', path("data"))
+    def test_add_model(self):
 
-    for category in ["name", "icon", "author", "description", "version", "license", "dependencies"]:
-        assert proj.metadata.has_key(category)
+        m1 = self.project.add('model', filename='model_1.py', datatype='python', content='print 1')
+        self.assertEqual(m1.read(), 'print 1')
+        assert str(m1.filename) == 'model_1.py'
+        assert m1.path == self.project.path / 'model' / 'model_1.py'
 
-
-def test_add():
-    proj = Project('my_new_temp_project', path("data"))
-    proj.add("fake_category", "fake_name", "fake_value")
-
-    assert hasattr(proj, "fake_category")
-    assert proj.fake_category["fake_name"] == "fake_value"
+        m2 = self.project.add('model', filename='model_2.py', content='print 2')
+        assert m2.read() == 'print 2'
+        assert str(m2.filename) == 'model_2.py'
+        assert m2.path == self.project.path / 'model' / 'model_2.py'
 
 
-def test_get():
-    proj = Project('my_new_temp_project', path("data"))
+        sample = get_data('model.py')
+        f = open(sample)
+        code = f.read()
+        f.close()
 
-#     proj.add(category="src", name="test_name", value="test_value")
-#     proj.add(category="answer", name="the Ultimate Question of Life, the Universe, and Everything", value=42)
+        m3 = self.project.add('model', path=sample)
+        assert str(m3.filename) == 'model.py'
+        assert m3.path == self.project.path / 'model' / 'model.py'
+        self.assertEqual(m3.read(), code)
 
-#     assert proj.get(category="src", name="test_name") == "test_value"
-#     assert proj.get(category="answer", name="the Ultimate Question of Life, the Universe, and Everything") == 42
+        m4 = self.project.add('model', filename='model_4.py', datatype='py', content='print 4')
+        assert m4.read() == 'print 4'
+        assert str(m4.filename) == 'model_4.py'
+        assert m4.path == self.project.path / 'model' / 'model_4.py'
+
+
+        # Check object is a valid model
+        # for model in [m1, m2, m3, m4]:
+        #     assert hasattr(model, 'run')
+        #     assert model.dtype == 'python'
+
+
+    def test_load(self):
+        project = Project('test_project_lpy', projectdir='data')
+        assert len(project.model) == 1
+        assert len(project.cache) == 4
+        assert len(project.startup) == 1
+        assert 'noise_branch-2d.lpy' in project.model
+
+
+#     def test_save(self):
+#
+#         proj = self.project
+#         proj.add("model", filename="plop.py", content="print 'hello world'")
+#         proj.save()
+#
+#         assert len(proj.model) == 1
+#
+#         proj2 = Project('test', self.tmpdir)
+#
+#         assert len(proj2.model) == 1
+#         # assert len(proj2.control) == 2
+#         # assert proj2.control["my_integer"] == 42
+#         # assert proj2.control["my_float"] == 3.14
+#         assert proj2.model["plop"].read() == "print 'hello world'"
+#
+#         pm.close('test')
+
+
+    def test_add_script(self):
+        self.project.add("model", filename="1.py", content="blablabla")
+        self.project.add("model", filename="2.py", content="blablabla2")
+#         self.project.add("model", filename="2.py", content="blablabla2")
+        assert len(self.project.model) == 2
+
+
+    # def test_rename(self):
+    #     self.project.add("model", filename="1.py", content="blablabla")
+    #
+    #     model1_path = self.project.path / 'model' / '1.py'
+    #     model2_path = self.project.path / 'model' / '2.py'
+    #
+    #     assert model1_path.isfile()
+    #
+    #     self.project.rename("model", "1", "2")
+    #     assert len(self.project.src) == 1
+    #     assert self.project.model["2"].read() == "blablabla"
+    #
+    #     # Old bug, path lost extension at rename
+    #     model2_badpath = self.project.path / 'model' / '2'
+    #     assert model2_badpath.exists() is False
+    #     assert model2_path.isfile()
+
+
+    # def test_rename_project(self):
+    #     self.project.add("model", filename="1.py", content="blablabla")
+    #     self.project.rename("project", "test", "test2")
+    #     assert self.project.name == "test2"
+
