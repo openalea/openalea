de37ff8a788754e860b9559a87e914e33de541cb
Julien Coste <julien.coste@inria.fr>
2014-08-20 13:09:43 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/openalea/trunk@4355 04ea39c6-a500-0410-8508-e77fe2a861cf

--- visualea/src/visualea/provenance.py
+++ visualea/src/visualea/provenance.py
@@ -1,5 +1,61 @@
 from openalea.vpltk.qt import QtGui, QtCore
+import sqlite3
+from openalea.core.path import path
+from openalea.core import settings
+from openalea.core.provenance import get_database_name, db_connexion
+import subprocess
 
+#db_conn2 = None
+# def get_database_name():
+    # db_fn = path(settings.get_openalea_home_dir())/'provenance.sq3'
+    # return db_fn
+
+# def db_connexion():
+    # print 'Dans db connexion 1'
+    # db_fn = ""+get_database_name()+""
+    # db_fn ="C:/Users/Yattara/.openalea/provenance.sq3"
+    # print db_fn
+    # conn=sqlite3.connect(fichierDonnees)
+    # dbconn=sqlite3.connect(db_fn)
+    # cu = dbconn.cursor()
+    # print 'ok1'
+    # cu.execute("SELECT * FROM Node")
+    # print 'ok2'
+    # for l in cu:
+      # print 'value'
+      # print(l)
+    # cu.execute("SELECT COUNT(*) from Node")
+    # print "le nombre de ligne est",cu.fetchone()
+    # return cur
+    # """ Return a curso on the database.
+    # If the database does not exists, create it.
+    # """
+    #global db_conn2
+    #if db_conn2 is None:
+    #    db_fn = get_database_name()
+    #    if not db_fn.exists():
+    #        db_conn=sqlite3.connect(db_fn)
+    #        cur = db_conn.cursor()
+    #        cur = db_create(cur)
+    #        print 'hhhhhhhhohhoo'
+    #        return cur
+        #   print 'coucou'
+        #   cur.execute("INSERT INTO CompositeNode(CompositeNodeid,creatime,name,description,userid) VALUES(NULL,start_time,pname,pname,1)")
+            #Affichage a partir de la bd
+        #   cur.execute("SELECT * FROM CompositeNode")
+        #   for l in cur:
+        #       print(l)
+        #   print 'kaka'
+    #else:
+    #    cur = db_conn2.cursor()
+    #    return cur
+    #    print 'Dans la bd'
+        #Insertion dans la bd
+        #cur.execute("INSERT INTO CompositeNode(CompositeNodeid,creatime,name,description,userid) VALUES(NULL,start_time,pname,pname,1)")
+        #Affichage a partir de la bd
+        #cur.execute("SELECT * FROM CompositeNode")
+    #    for l in cur:
+    #        print(l)
 
 def search_trace(composite_node, package, workspace, parent=None):
     """
@@ -11,27 +67,35 @@ def search_trace(composite_node, package, workspace, parent=None):
     """
     # @Moussa : This is here that you interrogate the database
     # print composite_node, package, workspace
-    
-    
-    
-    
-    
+    #cur = db_connexion()
     # Fake results:
+	#seach in database specification and all trace for this Composite_node, package and workspace.
+    nameval=workspace+"."+package+"."+composite_node
+    # cur = db_connexion()
+    #cur.execute("SELECT * FROM CompositeNode where name=? ",nameval)
+    # cur.execute("SELECT * FROM CompositeNode")
+    # for l in cur:
+       # print(l)
     value1 = 0
     value2 = 1
-    value3 = 5
-    value4 = 10
-    value5 = 42
-    result = [["spec1", "date1", value1], ["exec1", "date2", value2], ["exec2", "date3", value3], ["spec2", "date4", value4], ["exec3", "date5", value5]]
+    value3 = 10
+    value4 = 100
+    value5 = 1000
+	# Faire la recherche dans la base de donnees pour rechercher le composite_node le package et le workfspace
+	# Concatener le composite_node, le package et le workspace pour avoir le name du workflow
+	# select * from 
+    result = [["specification", "date", value1], ["Execution 1", "date", value2], ["Execution 2", "date", value3], ["Execution 3", "date", value4], ["Execution 4", "date", value5]]
     
     prov_widget = ProvenanceViewerWidget(composite_node, package, workspace, result, parent=parent)
-    
+    print composite_node
+    print package
+    print workspace
+    print 'helo'
     dialog = ModalDialog(prov_widget)
     dialog.show()
     dialog.raise_()
     dialog.exec_()
-
-
+	
 class ProvenanceViewerWidget(QtGui.QWidget):
     def __init__(self, composite_node, package, workspace, traces, parent=None):
         super(ProvenanceViewerWidget, self).__init__(parent)
@@ -47,13 +111,23 @@ class ProvenanceViewerWidget(QtGui.QWidget):
             layout.addWidget(QtGui.QLabel(date), i, 1)
             btn = QtGui.QPushButton("show", parent=self)
             btn.prov = value
+            btn.category = exe
             btn.clicked.connect(self.show_provenance)
             layout.addWidget(btn, i, 2)
             i += 1
     
     def show_provenance(self):
         sender = self.sender()
-        print sender.prov
+        category = sender.category
+        value = sender.prov
+        
+        if "spec" in category:
+            # Launch Visualea
+            pass
+        else:
+            # Launch Graphviz
+            cmd = "notepad"
+            subprocess.call(cmd)
         
 class ProvenanceSelectorWidget(QtGui.QWidget):
     def __init__(self, parent=None):
