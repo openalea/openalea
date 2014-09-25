37c06462e40fbf2631a293d04b86e158c00a10c7
Guillaume Baty <guillaume.baty@inria.fr>
2014-09-25 14:59:59 +0200

    oalab.legacy is now deprecated

--- oalab/src/openalea/oalab/legacy/__init__.py
+++ oalab/src/openalea/oalab/legacy/__init__.py
@@ -3,3 +3,6 @@ This module contains old packages.
 Main part of these packages are no more used and will be deleted soon
 """
 
+from openalea.core.util import warn_deprecated
+warn_deprecated(__name__, 'new modules', (2014, 9, 25))
+
