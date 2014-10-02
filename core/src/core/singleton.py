9269388f4a0bb5378b5c8235166165478b2e7b26
Guillaume Baty <guillaume.baty@inria.fr>
2014-10-02 11:33:23 +0200

    Singleton instance attribute is now private (use call to get it)

--- core/src/core/singleton.py
+++ core/src/core/singleton.py
@@ -26,12 +26,12 @@ class Singleton(type):
 
     def __init__(cls, name, bases, dic):
         super(Singleton, cls).__init__(name, bases, dic)
-        cls.instance=None
+        cls._instance = None
 
     def __call__(cls, *args, **kw):
-        if cls.instance is None:
-            cls.instance=super(Singleton, cls).__call__(*args, **kw)
-        return cls.instance
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls._instance
 
 
 import weakref
