103d82bf4fc1688058686d70bc7be8b8cca4ad5f
Guillaume Baty <guillaume.baty@inria.fr>
2015-04-03 17:04:53 +0200

    First unittest to test events

--- core/src/core/unittest_tools.py
+++ core/src/core/unittest_tools.py
@@ -2,10 +2,12 @@
 import unittest
 from openalea.core.observer import AbstractListener
 
+
 class EventTracker(AbstractListener):
+
     def __init__(self, debug=False):
         AbstractListener.__init__(self)
-        self.debug=debug
+        self.debug = debug
         self._events = []
 
     def notify(self, sender, event=None):
@@ -22,7 +24,15 @@ class EventTracker(AbstractListener):
 
 class TestCase(unittest.TestCase):
 
-    def check_events(self, events, names=None):
+    def check_events(self, events, names=None, values=None):
+        """
+        values:
+        dict {idx:[val, val2]}
+        """
         ev_names = [event[1][0] for event in events]
+        ev_args = [event[1] for event in events]
         self.assertListEqual(ev_names, names)
-
+        if values:
+            for event_idx, event in values.items():
+                for arg_idx, arg in event.items():
+                    self.assertEqual(ev_args[event_idx][arg_idx], arg)
