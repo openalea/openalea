210a9b72178007c4b204a5ef0dcfc926a180bc27
Guillaume Baty <guillaume.baty@inria.fr>
2015-05-11 14:56:51 +0200

    Use PyQt4 generated file by default

--- visualea/src/visualea/ui_listedit.py
+++ visualea/src/visualea/ui_listedit.py
@@ -2,36 +2,47 @@
 
 # Form implementation generated from reading ui file 'listedit.ui'
 #
-# Created: Tue Oct  2 13:56:54 2007
-#      by: PyQt4 UI code generator 4.2
+# Created: Mon May 11 14:54:41 2015
+#      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
 
-from openalea.vpltk.qt import qt
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_ListEdit(object):
     def setupUi(self, ListEdit):
-        ListEdit.setObjectName("ListEdit")
-        ListEdit.resize(qt.QtCore.QSize(qt.QtCore.QRect(0,0,400,300).size()).expandedTo(ListEdit.minimumSizeHint()))
-
-        self.vboxlayout = qt.QtGui.QVBoxLayout(ListEdit)
-        self.vboxlayout.setObjectName("vboxlayout")
-
-        self.listWidget = qt.QtGui.QListWidget(ListEdit)
-        self.listWidget.setObjectName("listWidget")
+        ListEdit.setObjectName(_fromUtf8("ListEdit"))
+        ListEdit.resize(400, 300)
+        self.vboxlayout = QtGui.QVBoxLayout(ListEdit)
+        self.vboxlayout.setObjectName(_fromUtf8("vboxlayout"))
+        self.listWidget = QtGui.QListWidget(ListEdit)
+        self.listWidget.setObjectName(_fromUtf8("listWidget"))
         self.vboxlayout.addWidget(self.listWidget)
-
-        self.buttonBox = qt.QtGui.QDialogButtonBox(ListEdit)
-        self.buttonBox.setOrientation(qt.QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(qt.QtGui.QDialogButtonBox.Cancel|qt.QtGui.QDialogButtonBox.NoButton|qt.QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
+        self.buttonBox = QtGui.QDialogButtonBox(ListEdit)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.NoButton|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
         self.vboxlayout.addWidget(self.buttonBox)
 
         self.retranslateUi(ListEdit)
-        qt.QtCore.QObject.connect(self.buttonBox,qt.QtCore.SIGNAL("accepted()"),ListEdit.accept)
-        qt.QtCore.QObject.connect(self.buttonBox,qt.QtCore.SIGNAL("rejected()"),ListEdit.reject)
-        qt.QtCore.QMetaObject.connectSlotsByName(ListEdit)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), ListEdit.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), ListEdit.reject)
+        QtCore.QMetaObject.connectSlotsByName(ListEdit)
 
     def retranslateUi(self, ListEdit):
-        ListEdit.setWindowTitle(qt.QtGui.QApplication.translate("ListEdit", "Dialog", None, qt.QtGui.QApplication.UnicodeUTF8))
+        ListEdit.setWindowTitle(_translate("ListEdit", "Dialog", None))
 
