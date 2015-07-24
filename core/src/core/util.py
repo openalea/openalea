b1f0660c6752c00120ddbeeeed732d8007b6cb7a
Pierre Fernique <pfernique@gmail.com>
2015-07-24 16:30:51 +0200

    Add camel_case_to_lower function

--- core/src/core/util.py
+++ core/src/core/util.py
@@ -3,6 +3,42 @@ import datetime
 import traceback
 import os
 
+def camel_case_to_lower(name):
+    """
+
+    :Examples:
+
+    >>> camel_case_to_lower('squareRoot')
+    'square_root'
+
+    >>> camel_case_to_lower('SquareRoot')
+    'square_root'
+
+    >>> camel_case_to_lower('ComputeSQRT')
+    'compute_sqrt'
+    """
+    lowername = '_'
+    index = 0
+    while index < len(name):
+        if name[index].islower():
+            lowername += name[index]
+            index += 1
+        else:
+            if lowername[-1] == '_':
+                if not name[index] == '_':
+                    lowername += name[index].lower()
+            else:
+                if not name[index] == '_':
+                    lowername += '_' + name[index].lower()
+                else:
+                    lowername += '_'
+            index += 1
+            while index < len(name) and not name[index].islower():
+                lowername += name[index].lower()
+                index += 1
+    lowername = lowername.lstrip('_')
+    return lowername
+
 def warn_deprecated(old_name, new_name, date=None):
     """
     :param date: (yyyy, mm, dd) ex: (2014, 9, 25) for 2014, 25th of september
