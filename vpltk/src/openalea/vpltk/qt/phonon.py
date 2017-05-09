f9340c97ec393bea87ed50e3ec0c8d1a2f1636c2
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2017-05-09 13:51:02 +0200

    disabling phonon import on mac sierra (conda package broken)

--- vpltk/src/openalea/vpltk/qt/phonon.py
+++ vpltk/src/openalea/vpltk/qt/phonon.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+import platform
 
 if os.environ['QT_API'] == 'pyqt':
-    from PyQt4.phonon import *
+    if platform.system() == 'Darwin' and platform.mac_ver()[0].split('.')[:2] == ['10', '12']:
+        # illegal instruction 4 on Mac Sierra using conda packages, so temporary disabling phonon
+        pass
+    else:
+        from PyQt4.phonon import *
 else:
     from PySide.phonon import *
