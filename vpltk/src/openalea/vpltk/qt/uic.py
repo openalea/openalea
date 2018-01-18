cf4f379718426367a5c04bd91bcfa7cf087880a5
pradal <christophe.pradal@inria.fr>
2018-01-18 15:34:13 +0100

    update uic

--- vpltk/src/openalea/vpltk/qt/uic.py
+++ vpltk/src/openalea/vpltk/qt/uic.py
@@ -8,15 +8,20 @@ from openalea.vpltk.qt import PYQT4_API
 from openalea.vpltk.qt import PYSIDE_API
 
 try:
-    if os.environ[QT_API] in PYQT5_API:
+    _QT_API = os.environ.get(QT_API)
+    if _QT_API in PYQT5_API:
         from PyQt5.uic import compileUi
         compile_args = dict(execute=False, indent=4)
-    elif os.environ[QT_API] in PYQT4_API:
+    elif _QT_API in PYQT4_API:
         from PyQt4.uic import compileUi
         compile_args = dict(execute=False, indent=4)
-    elif os.environ[QT_API] in PYSIDE_API:
+    elif _QT_API in PYSIDE_API:
         from pysideuic import compileUi
         compile_args = dict(execute=False, indent=4, from_imports=False)
+    elif _QT_API is None:
+        # default
+        from PyQt4.uic import compileUi
+        compile_args = dict(execute=False, indent=4)
     else :
         raise NotImplementedError
 except ImportError, e:
