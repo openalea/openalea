f5274facf09dfca88ef1803e7948dbc228bc64d4
pradal <christophe.pradal@inria.fr>
2018-02-09 18:46:16 +0100

    misc as a submodule

--- misc/src/openalea/misc_wralea/alinea_packages.py
+++
@@ -1,9 +0,0 @@
-def alinea_packages(in1):
-    '''    List of packages to release from alinea project 
-    '''
-    alinea_packages = """
-
-        """
-
-    # return outputs
-    return alinea_packages
